6dd0acc24e17bce12768e660aa5a4cefab41665b
gforney <gforney@gmail.com>
2016-09-08 22:27:49 -0400

    update_repos: fix a typo

--- Scripts/update_repos.sh
+++ Scripts/update_repos.sh
@@ -19,7 +19,7 @@ case $OPTION  in
    usage;
    ;;
   p)
-   PUSH=1
+   PUSH="1"
    ;;
 esac
 done
@@ -59,5 +59,5 @@ do
      echo "$repo does not exist, not updating"
   fi
 done
-echo repo creation complated
+echo repo updating complete
 cd $CURDIR
