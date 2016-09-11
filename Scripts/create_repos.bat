848cc9f5d52b6efc1671436fd7d5699c3d52de0d
gforney <gforney@gmail.com>
2016-09-11 14:39:34 -0400

    scripts: initial corrections for the windows create repo batch file

--- Scripts/create_repos.bat
+++ Scripts/create_repos.bat
@@ -1,81 +1,105 @@
-#!/bin/bash
-GITUSER=`git remote -v | head -1 | awk -F ':' '{print $2}' | awk -F\/ '{print $1}'`
-fdsrepos="exp fds out smv"
-smvrepos="cfast fds smv"
-cfastrepos="cfast exp smv"
-allrepos="cfast cor exp fds out radcal smv"
-repos=$fdsrepos
+@echo off
+git remote -v | grep origin | head -1 | gawk -F ":" "{print $2}" | gawk -F\\/ "{print $1}" > gituser.out
+set /p GITUSER=<gituser.out
 
-function usage {
-echo "Create repos used by cfast, fds and/or smokview"
-echo ""
-echo "Options:"
-echo "-a - setup all repos: "
-echo "    $allrepos"
-echo "-c - setup repos used by cfastbot: "
-echo "    $cfastrepos"
-echo "-f - setup repos used by firebot: "
-echo "    $fdsrepos"
-echo "-s - setup repos used by smokebot: "
-echo "    $smvrepos"
-echo "-h - display this message"
-exit
-}
+set fdsrepos=exp fds out smv
+set smvrepos=cfast fds smv
+set cfastrepos=cfast exp smv
+set allrepos=cfast cor exp fds out radcal smv
+set repos=%fdsrepos%
 
-while getopts 'acfsh' OPTION
-do
-case $OPTION  in
-  a)
-   repos=$allrepos;
-   ;;
-  c)
-   repos=$cfastrepos;
-   ;;
-  f)
-   repos=$fdsrepos;
-   ;;
-  h)
-   usage;
-   ;;
-  s)
-   repos=$smvrepos;
-   ;;
-esac
-done
-shift $(($OPTIND-1))
+call :getopts %*
+pause
+if %stopscript% == 1 (
+  exit /b
+)
 
-echo "You are about to clone the repos:"
-echo "$repos "
-echo "from git@github.com:$GITUSER"
-echo ""
-echo "Press any key to continue or <CTRL> c to abort."
-echo "Type $0 -h for other options"
-read val
+echo You are about to clone the repos: %repos%
+echo from git@github.com:%GITUSER%
+echo.
+echo Press any key to continue or CTRL c to abort.
+echo create_repos -hfor other options
+pause >Nul
 
-CURDIR=`pwd`
-cd ../..
-FIREMODELS=`pwd`
-for repo in $repos
-do 
+
+set CURDIR=%CD%
+cd ..\..
+set FIREMODELS=%CD%
+
+for %%repo in ("%repos%") do
   echo
-  repodir=$FIREMODELS/$repo
+  set repodir=%FIREMODELS%\%%repo
   echo "-------------------------------"
-  if [ -e $repodir ]; then
-     echo Skipping $repo.  The directory $repodir already exists.
+  if exit %repodir% (
+     echo Skipping %repo%.  The directory %repodir% already exists.
+  ) else (
   else
-     cd $FIREMODELS
-     if [ "$repo" == "exp" ]; then
-        git clone  --recursive git@github.com\:$GITUSER/$repo.git
-     else
-        git clone git@github.com\:$GITUSER/$repo.git
-     fi
-     if [ "$GITUSER" != "firemodels" ]; then
+     cd %FIREMODELS%
+     if "%repo%" == "exp" (
+        git clone  --recursive git@github.com\:%GITUSER%/%%repo.git
+     )  else (
+        git clone git@github.com\:%GITUSER%/%%repo.git
+     )
+     if "%GITUSER% != "firemodels"  (
         echo setting up remote tracking
-        cd $repodir
-        git remote add firemodels git@github.com\:firemodels/$repo.git
+        cd %repodir%
+        git remote add firemodels git@github.com\:firemodels/%%repo.git
         git remote update
      fi
   fi
-done
+)
 echo repo creation complated
-cd $CURDIR
+cd %CURDIR%
+
+goto eof
+
+:getopts
+ if (%1)==() exit /b
+ set valid=0
+ set arg=%1
+ if /I "%1" EQU "-h" (
+   call :usage
+   set stopscript=1
+   exit /b
+ )
+ if /I "%1" EQU "-a" (
+   set valid=1
+   repos=%allrepos%;
+ )
+ if /I "%1" EQU "-c" (
+   set valid=1
+   repos=%cfastrepos%;
+ )
+ if /I "%1" EQU "-f" (
+   set valid=1
+   repos=%fdsrepos%;
+ )
+ if /I "%1" EQU "-s" (
+   set valid=1
+   repos=%smvrepos%;
+ )
+ shift
+ if %valid% == 0 (
+   echo.
+   echo ***Error: the input argument %arg% is invalid
+   echo.
+   echo Usage:
+   call :usage
+   set stopscript=1
+   exit /b
+ )
+if not (%1)==() goto getopts
+exit /b
+
+:usage
+echo Create repos used by cfast, fds and/or smokview
+echo.
+echo Options:
+echo -a - setup all repos: %allrepos%
+echo -c - setup repos used by cfastbot: %cfastrepos%
+echo -f - setup repos used by firebot: %fdsrepos%
+echo -s - setup repos used by smokebot: %smvrepos%
+echo -h - display this message%
+exit /b
+
+:eof
