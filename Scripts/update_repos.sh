03c2b33c39d28c946ea28478c9099c4f5c50316e
gforney <gforney@gmail.com>
2016-09-12 21:11:10 -0400

    update create and  update repos scripts

--- Scripts/update_repos.sh
+++ Scripts/update_repos.sh
@@ -17,12 +17,12 @@ FMROOT=
 if [ -e ../.gitbot ]; then
    cd ../..
    FMROOT=`pwd`
-fi
-if [ x"$FMROOT" == "x" ]; then
-   FMROOT=$FIREMODELS
+else
+   echo "***error: the update_repos.sh script must be run from the bot/Scripts directory"
+   exit
 fi
 
-while getopts 'hpr:' OPTION
+while getopts 'hp' OPTION
 do
 case $OPTION  in
   h)
@@ -31,55 +31,39 @@ case $OPTION  in
   p)
    PUSH="1";
    ;;
-  r)
-   FMROOT=$OPTARG;
-   ;;
 esac
 done
 shift $(($OPTIND-1))
-
-if [ "x$FMROOT" == "x" ]; then
-   echo "***Error: repo location not defined."
-   echo "          Rerun in the bot/Scripts directory, or:
-   echo "          use the -r option or define the FIREMODELS:
-   echo "          environment variable to define a repo location"
-   exit
-fi
-if [ ! -e $FMROOT ]; then
-   echo "***Error: The directory $FMROOT does not exist"
-   echo "          You need to cd to $FMROOT and clone the bot directory from github"
-   exit
-fi
 
 for repo in $allrepos
 do 
   echo
   repodir=$FMROOT/$repo
   echo "---------------------------------------------------------------"
-  if [ -e $repodir ]; then
-     cd $repodir
-     CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
-     if [ "$BRANCH" == "$CURRENT_BRANCH" ]; then
-       echo "updating $repo from origin"
-       git fetch origin
-       git merge origin/$BRANCH
-       have_central=`git remote -v | awk '{print $1}' | grep firemodels | wc  -l`
-       if [ "$have_central" -gt "0" ]; then
-          echo "updating $repo from firemodels"
-          git fetch firemodels
-          git merge firemodels/$BRANCH
-          if [ "$PUSH" == "1" ]; then
-             echo "pushing changes in $repo to origin"
-             git push origin $BRANCH
-          fi
-       fi
-     else
-       echo $BRANCH branch not checkout out in $repo
-       echo update skipped
-     fi
-  else
+  if [ ! -e $repodir ]; then
      echo "$repo does not exist, not updating"
+     continue;
   fi
+  cd $repodir
+  CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
+  if [ "$BRANCH" != "$CURRENT_BRANCH" ]; then
+    echo $BRANCH branch not checked out in $repo
+    echo update skipped
+    continue;
+  fi
+  echo "updating $repo from origin"
+  git fetch origin
+  git merge origin/$BRANCH
+  have_central=`git remote -v | awk '{print $1}' | grep firemodels | wc  -l`
+  if [ "$have_central" -gt "0" ]; then
+     echo "updating $repo from firemodels"
+     git fetch firemodels
+     git merge firemodels/$BRANCH
+     if [ "$PUSH" == "1" ]; then
+         echo "pushing changes in $repo to origin"
+         git push origin $BRANCH
+     fi
+   fi
 done
 echo
 echo repo updating complete
