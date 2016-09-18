f9507becb7e1eada2a4677e1b7fcd79a45795c2b
gforney <gforney@gmail.com>
2016-09-18 10:49:39 -0400

    scripts: add option to show_remotes command to disable push access to firemodels

--- Scripts/show_remotes.bat
+++ Scripts/show_remotes.bat
@@ -13,6 +13,12 @@ if not exist ..\.gitbot goto skip1
    exit /b
 :endif1
 
+set wc=%FMROOT%\bot\Scripts\wc
+set grep=%FMROOT%\bot\Scripts\grep
+set gawk=%FMROOT%\bot\Scripts\gawk
+set head=%FMROOT%\bot\Scripts\head
+set disable_push=0
+
 call :getopts %*
 if %stopscript% == 1 (
   exit /b
@@ -31,16 +37,26 @@ goto eof
 
 :show_remotes
   set repo=%1
-  echo
+  echo.
   set repodir=%FMROOT%\%repo%
-  echo -----------------------------------------------------------
+  echo -------------------%repo%-------------------------------------
   if not exist %repodir% (
-     echo %repo% does not exist, skipping
      exit /b
   )   
   cd %repodir%
-  echo %repo% remotes:
   git remote -v
+  git remote -v | %grep% firemodels | %grep% push | %gawk% "{print $2}" | %grep% github | %grep% firemodels | %wc% -l > %CURDIR%\nbadpush.out
+  set /p nbadpush=<%CURDIR%\nbadpush.out
+  if %nbadpush% GTR 0 (
+    if %disable_push% EQU 1 (
+      git remote set-url --push firemodels DISABLE
+    ) else (
+      echo push access to firemodels is enabled
+      echo to disable, rerun show_remotes with the -d option
+    )
+    
+  )
+  erase %CURDIR%\nbadpush.out
   exit /b
 
 goto eof
@@ -53,6 +69,11 @@ goto eof
  if /I "%1" EQU "-h" (
    call :usage
    set stopscript=1
+   exit /b
+ )
+ if /I "%1" EQU "-d" (
+   set disable_push=1   
+   set valid=1
    exit /b
  )
  shift
@@ -72,6 +93,7 @@ exit /b
 echo Show remotes for repos %allrepos% if they exist
 echo.
 echo Options:
+echo -d - disable push access to firemodels for %allrepos%
 echo -h - display this message
 exit /b
 
