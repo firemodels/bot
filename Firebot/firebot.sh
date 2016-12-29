9fa1482157e7d4185482ccea34963b358c42626b
gforney <gforney@gmail.com>
2016-12-28 20:41:53 -0500

    firebot: add some debugging output

--- Firebot/firebot.sh
+++ Firebot/firebot.sh
@@ -46,12 +46,14 @@ PROCESS()
   nfds=-1
   nsuccess=-1
   cd $case
+  sleep 60
   if [ -d Current_Results ]; then
     cd Current_Results
     nout=`ls -l *.out | wc -l`
     nfds=`ls -l *.fds | wc -l`
     nsuccess=`tail *.out |& grep successfully | wc -l`
     ls
+    echo current_dir=`pwd`
   fi
   echo nfds=$nfds nout=$nout nsuccess=$nsuccess
   if [ $nfds -gt 0 ] && [ $nfds -gt $nout ]; then
