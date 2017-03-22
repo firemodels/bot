99b7c160233a4da34d119000f1f976afa6d6c73c
gforney <gforney@gmail.com>
2017-03-22 13:36:33 -0400

    bot scripts: update email lists

--- Smokebot/firebot_email_list.bat
+++ Smokebot/firebot_email_list.bat
@@ -4,8 +4,8 @@
 set mailToSMV="gforney@gmail.com"
 
 :: Mailing lists for Firebot status report
-set    mailToFDS="mcgratta@gmail.com, randy.mcdermott@gmail.com, gforney@gmail.com, jfloyd@jensenhughes.com, milac@uw.edu"
+set    mailToFDS="mcgratta@gmail.com, randy.mcdermott@gmail.com, gforney@gmail.com, jfloyd@jensenhughes.com"
 
-set mailToFDSAll="mcgratta@gmail.com, randy.mcdermott@gmail.com, gforney@gmail.com, jfloyd@jensenhugues.com, milac@uw.edu"
+set mailToFDSAll="mcgratta@gmail.com, randy.mcdermott@gmail.com, gforney@gmail.com, jfloyd@jensenhugues.com"
 
 set mailToFDSDebug="gforney@gmail.com"
--- Smokebot/smokebot_email_list.sh
+++ Smokebot/smokebot_email_list.sh
@@ -7,10 +7,10 @@ mailToSMV="gforney@gmail.com"
 mailToCFAST="gforney@gmail.com"
 
 # General mailing list for Firebot status report
-        mailToFDS="mcgratta@gmail.com, kevin.mcgrattan@nist.gov, randy.mcdermott@gmail.com, randall.mcdermott@nist.gov, drjfloyd@gmail.com, gforney@gmail.com, marcos.vanella@nist.gov, miilac@uw.edu"
+        mailToFDS="mcgratta@gmail.com, kevin.mcgrattan@nist.gov, randy.mcdermott@gmail.com, randall.mcdermott@nist.gov, jfloyd@jensenhughes.com, gforney@gmail.com, marcos.vanella@nist.gov, miilac@uw.edu"
 
 # Verbose mailing list for Validationbot, Macbot, etc.
-mailToFDS_verbose="mcgratta@gmail.com, kevin.mcgrattan@nist.gov, randy.mcdermott@gmail.com, randall.mcdermott@nist.gov, drjfloyd@gmail.com, gforney@gmail.com, marcos.vanella@nist.gov, milac@uw.edu"
+mailToFDS_verbose="mcgratta@gmail.com, kevin.mcgrattan@nist.gov, randy.mcdermott@gmail.com, randall.mcdermott@nist.gov, jfloyd@jensenhughes.com, gforney@gmail.com, marcos.vanella@nist.gov"
 
 # mailing list when debugging firebot
 mailToFDS_debug="gforney@gmail.com, randy.mcdermott@gmail.com, randall.mcdermott@nist.gov"
