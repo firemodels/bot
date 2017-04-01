a0b3f8823739c115272ec69d7e506d75df7b2806
gforney <gforney@gmail.com>
2017-04-01 13:41:56 -0400

    smokebot: when building libraries , call script that builds in foreground

--- Smokebot/smokebot.bat
+++ Smokebot/smokebot.bat
@@ -285,7 +285,7 @@ echo  Building Smokeview
 echo    libs
 
 cd %smvrepo%\Build\LIBS\intel_win%size%
-call makelibs bot 1>> %OUTDIR%\stage2a.txt 2>&1
+call makelibs_bot bot 1>> %OUTDIR%\stage2a.txt 2>&1
 
 echo    debug
 
