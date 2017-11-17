5d73f9261a54bed0c85c44a6398d6a6d4015a1e8
gforney <gforney@gmail.com>
2017-11-17 14:57:46 -0500

    cfastbot: update email dist list

--- Cfastbot/cfastbot.sh
+++ Cfastbot/cfastbot.sh
@@ -1270,7 +1270,7 @@ size2=
 #  = Input variables =
 #  ===================
 
-mailTo="gforney@gmail.com, rpeacoc@gmail.com"
+mailTo="gforney@gmail.com, rpeacoc@gmail.com, waicheong.tam@nist.gov, paul.reneke@nist.gov"
 
 cfastbotdir="`pwd`"
 
