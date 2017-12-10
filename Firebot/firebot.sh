c6c6ccfe0e04f9300b09385949210ef9e265d97a
Forney <gforney@gmail.com>
2017-12-10 11:01:11 -0500

    firebot: remove extra quote

--- Firebot/firebot.sh
+++ Firebot/firebot.sh
@@ -1685,7 +1685,7 @@ if [ "QUEUE" == "none"]; then
    notfound=`background -v 2>&1 | tail -1 | grep "not found" | wc -l`
    if [ $notfound == 1 ]; then
       echo "Error: The program background was not found.  firebot aborted"
-      echo "       Add the directory containing fds and smokeview to your path""
+      echo "       Add the directory containing fds and smokeview to your path"
       echo "       (same directory containing fds and smokeview)"
       echo "Error: background not found. firebot aborted." >> $OUTPUT_DIR/stage1 2>&1
       exit
