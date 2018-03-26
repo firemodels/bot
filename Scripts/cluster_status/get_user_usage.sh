6e72cb602cc4140376430a801d501b2255f81a80
gforney <gforney@gmail.com>
2018-03-26 13:10:10 -0400

    add batch queues to summary web page

--- Scripts/cluster_status/get_user_usage.sh
+++ Scripts/cluster_status/get_user_usage.sh
@@ -6,11 +6,24 @@ njobs=`cat $qstatout | wc -l`
 if [ $njobs -gt 0 ]; then
 awk '{a[$1] += $2} END{for (i in a) print i,a[i]}' $qstatout | sort > $infoout
 cat << EOF
+<table>
+<tr>
+<td>
 <table border=on>
 <tr><th>User</th><th>cores</th></tr>
 EOF
   cat $infoout | awk '{print "<tr><td>",$1,"</td>", "<td align=right>",$2,"</td></tr>"}' 
 cat << EOF
+</table>
+</td>
+<td></td>
+<td>
+EOF
+if [ -e queues.html ]; then
+cat queues.html 
+fi
+cat << EOF
+</td></tr>
 </table>
 EOF
 rm -f $infoout
