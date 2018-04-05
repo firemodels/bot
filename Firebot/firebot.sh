ac7ea65eaec59b87df945dc4c4e4699c2d9cb6f3
gforney <gforney@gmail.com>
2018-04-05 08:46:29 -0400

    firebot: treat matlab errors as errors not warnings in firebot summary email

--- Firebot/firebot.sh
+++ Firebot/firebot.sh
@@ -1111,9 +1111,9 @@ check_matlab_verification()
    then
       matlab_verification_success=true
    else
-      echo "Warnings from Stage 7a - Matlab plotting and statistics (verification):" >> $WARNING_LOG
-      grep -B 5 -A 50 "Error" $OUTPUT_DIR/stage7a_verification | tr -cd '\11\12\15\40-\176' >> $WARNING_LOG
-      echo "" >> $WARNING_LOG
+      echo "Errors from Stage 7a - Matlab plotting and statistics (verification):" >> $ERROR_LOG
+      grep -B 5 -A 50 "Error" $OUTPUT_DIR/stage7a_verification | tr -cd '\11\12\15\40-\176' >> $ERROR_LOG
+      echo "" >> $ERROR_LOG
    fi
 }
 
@@ -1187,9 +1187,9 @@ check_matlab_validation()
    then
       matlab_validation_succcess=true
    else
-      echo "Warnings from Stage 7b - Matlab plotting and statistics (validation):" >> $WARNING_LOG
-      grep -B 5 -A 50 "Error" $OUTPUT_DIR/stage7b_validation | tr -cd '\11\12\15\40-\176' >> $WARNING_LOG
-      echo "" >> $WARNING_LOG
+      echo "Errors from Stage 7b - Matlab plotting and statistics (validation):" >> $ERROR_LOG
+      grep -B 5 -A 50 "Error" $OUTPUT_DIR/stage7b_validation | tr -cd '\11\12\15\40-\176' >> $ERROR_LOG
+      echo "" >> $ERROR_LOG
    fi
 }
 
