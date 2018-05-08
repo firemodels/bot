7ad848830bb856622106abfc0c2472e4bb18b99c
gforney <gforney@gmail.com>
2018-05-08 13:52:05 -0400

    bots: update comments in email list files

--- Cfastbot/cfastbot_email_list.sh
+++ Cfastbot/cfastbot_email_list.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# cfastbot by default sends emails to the email address set up for the
+# bot repo ( git config user.email ) .  If you wish to use a different
+# email address use the -m option when running run_cfastbot.sh or copy
+# the file cfastbot_email_list.sh to
+#    $HOME/.cfastbot/cfastbot_email_list 
+# and change the following line to the list of email addresses you wish.
+
 mailTo=""
--- Smokebot/smokebot_email_list.sh
+++ Smokebot/smokebot_email_list.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-# Mailing list for Smokebot status report
+# smokebot by default sends emails to the email address set up for the
+# bot repo ( git config user.email ) .  If you wish to use a different
+# email address use the -m option when running run_smokebot.sh or copy
+# the file smokebot_email_list.sh to
+#    $HOME/.smokebot/smokebot_email_list 
+# and change the following line to the list of email addresses you wish.
+
+
 mailToSMV=""
 
-# General mailing list for Firebot status report
-        mailToFDS=""
+# email list for fds errors
+mailToFDS=""
