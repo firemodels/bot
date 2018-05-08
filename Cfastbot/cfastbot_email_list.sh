966fbc76b5d18c07931abdbcfee9ee536b54f0fb
gforney <gforney@gmail.com>
2018-05-08 15:13:28 -0400

    bots: fix a typo and make minor editing changes

--- Cfastbot/cfastbot_email_list.sh
+++ Cfastbot/cfastbot_email_list.sh
@@ -3,8 +3,8 @@
 # cfastbot by default sends emails to the email address set up for the
 # bot repo ( git config user.email ) .  If you wish to use a different
 # email address use the -m option when running run_cfastbot.sh or copy
-# the file cfastbot_email_list.sh to
-#    $HOME/.cfastbot/cfastbot_email_list 
+# this file (cfastbot_email_list.sh) to
+#    $HOME/.cfastbot/cfastbot_email_list.sh
 # and change the following line to the list of email addresses you wish.
 
 mailTo=""
--- Firebot/firebot_email_list.sh
+++ Firebot/firebot_email_list.sh
@@ -3,8 +3,8 @@
 # firebot by default sends emails to the email address set up for the
 # bot repo ( git config user.email ) .  If you wish to use a different
 # email address use the -m option when running run_firebot.sh or copy
-# the file firebot_email_list.sh to
-#    $HOME/.firebot/firebot_email_list 
+# this file (firebot_email_list.sh) to
+#    $HOME/.firebot/firebot_email_list.sh
 # and change the following line to the list of email addresses you wish.
 
         mailToFDS=""
