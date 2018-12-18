af8fbb48f9355a04aac087411e4745f5579bb9aa
gforney <gforney@gmail.com>
2018-12-18 16:37:23 -0500

    script update

--- Bundle/fds/osx/make_bundle.sh
+++ Bundle/fds/osx/make_bundle.sh
@@ -11,4 +11,4 @@ export MPI_VERSION=$osx_mpi_version
 
 export INSTALLDIR=FDS/$FDSEDITION
 
-~/$fds_smvroot/fds/Build/Bundle/scripts/bundle_generic.sh
+~/$fds_smvroot/bot/Bundle/fds/scripts/bundle_generic.sh
