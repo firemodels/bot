35733f5e8165f92c530150ae62c0886ccd7f9429
gforney <gforney@gmail.com>
2019-01-06 13:11:23 -0500

    bundle scripts: add -np 1 to windows fds alias

--- Bundle/fds/for_bundle/fdsinit.bat
+++ Bundle/fds/for_bundle/fdsinit.bat
@@ -1,6 +1,6 @@
 @echo off
 set mpibindir="%~dp0\mpi\intel64\bin"
 call %mpibindir%\mpivars
-doskey fds=mpiexec -localonly fdsmpi.exe $* 
+doskey fds=mpiexec -localonly -np 1 fdsmpi.exe $* 
 title FDS
 
