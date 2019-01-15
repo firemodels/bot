0a9665ac214f56b10c36d2058410a44f3db087ff
gforney <gforney@gmail.com>
2019-01-15 18:18:13 -0500

    bundle scripts: add a helpfds command to give asummary on how to use fds

--- Bundle/fds/for_bundle/fdsinit.bat
+++ Bundle/fds/for_bundle/fdsinit.bat
@@ -4,9 +4,6 @@ SET PATH=%I_MPI_ROOT%;%PATH%
 doskey fdss=mpiexec -localonly -np  1 fds.exe $1 
 doskey fdsm=mpiexec -localonly -np $1 fds.exe $2 
 title FDS
-echo OMP_NUM_THREADS=%OMP_NUM_THREADS%
-echo fdss casename.fds
-echo fdsm xx casename.fds
-echo mpiexec -np xx fds casename.fds
 echo.
+echo type helpfds for help on running fds
 
