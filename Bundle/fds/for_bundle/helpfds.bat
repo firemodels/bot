ccf0370c37f2fdaf87f69200423fb39ca6aa614f
gforney <gforney@gmail.com>
2019-01-15 19:24:15 -0500

    bundle scripts: updates to fdshelp file

--- Bundle/fds/for_bundle/helpfds.bat
+++ Bundle/fds/for_bundle/helpfds.bat
@@ -1,8 +1,13 @@
 @echo off
 echo.
+echo fdss casename.fds   - shortcut for mpiexec -localonly -np 1 fds casename.fds
+echo fdsm n casename.fds - shortcut for mpiexec -localonly -np n fds casename.fds
+echo.
+echo Number of OpenMP threads
 echo OMP_NUM_THREADS=%OMP_NUM_THREADS%
-echo fdss casename.fds
-echo fdsm xx casename.fds
-echo mpiexec -np xx fds casename.fds
+echo.
+echo When using fdsm or mpiexec with n greater than one, it is 
+echo recommended to set the number of OpenMP threads to 1 using 
+echo set OMP_NUM_THREADS=1
 echo.
 
