255f9cdef4543415f9fedd77817c9d60201c277a
gforney <gforney@gmail.com>
2019-01-24 20:39:31 -0500

    bundle scripts: if fdspath is run a 2nd time it restores the path to the original setting

--- Bundle/fds/for_bundle/fdspath.bat
+++ Bundle/fds/for_bundle/fdspath.bat
@@ -1,6 +1,21 @@
 @echo off
 set CURDIR=%CD%
 
+if "x%FDS_SAVE_PATH%" == "x" goto set_fds_path
+:: restore original path
+
+echo restoring original path
+set PATH=%FDS_SAVE_PATH%
+echo.
+echo PATH=%PATH%
+set FDS_SAVE_PATH=
+
+goto eof
+
+:set_fds_path
+
+:: set path containing fds, smokeview, intel mpi and windows entries
+
 SET I_MPI_ROOT=%~dp0\mpi
 SET FDS_PATH=%~dp0
 SET SMV_PATH=%~dp0\..\..\SMV6
@@ -14,7 +29,13 @@ SET FDS_PATH=%CD%
 cd %SMV_PATH%
 SET SMV_PATH=%CD%
 
-SET PATH=%SMV_PATH%;%FDS_PATH%;%I_MPI_ROOT%
+SET FDS_SAVE_PATH=%PATH%
+echo setting path to contain only fds, smokeview, intel mpi and windows entries
+echo type fdspath again to restore original path
+SET PATH=%SMV_PATH%;%FDS_PATH%;%I_MPI_ROOT%;%WINDIR%;%WINDIR%\system32
+echo.
+echo PATH=%PATH%
+
+:eof
+
 cd %CURDIR%
-
-
