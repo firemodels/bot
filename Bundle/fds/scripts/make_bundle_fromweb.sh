bc29e38301c8eca44732a27fa31960a49f239baf
gforney <gforney@gmail.com>
2019-02-21 08:56:21 -0500

    bundle scripts: consolidate make bundle scripts

---
+++ Bundle/fds/scripts/make_bundle_fromweb.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+fds_version=$1
+smv_version=$2
+mpi_version=$3
+
+export NOPAUSE=1
+args=$0
+DIR=$(dirname "${args}")
+cd $DIR
+
+./bundle_generic.sh $fds_version $smv_version $mpi_version
