c9a50ca6751ac569f6a3a652cb2498b41e69fae3
gforney <gforney@gmail.com>
2019-05-29 08:50:20 -0400

    Create setup_ubuntu.sh

---
+++ Scripts/setup_ubuntu.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# install make, gnu compilers etc
+sudo apt-get -y install build-essential
+
+# install openmpi
+sudo apt-get -y install openmpi-bin openmpi-common openssh-client openssh-server libopenmpi-dbg libopenmpi-dev
+
+# install gfotran
+sudo apt-get -y install gfortran
