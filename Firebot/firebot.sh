ded86ab53515d5ef07ed8f35cb0ba6c4ce6f7d02
gforney <gforney@gmail.com>
2019-07-09 07:24:43 -0400

    firebot: uncomment line that was checking whether cases were run

--- Firebot/firebot.sh
+++ Firebot/firebot.sh
@@ -477,7 +477,7 @@ run_verification_cases_debug()
    wait_cases_debug_end 'verification'
 
 #  check whether cases have run
-#   ./Run_FDS_Cases.sh -C >> $OUTPUT_DIR/stage4 2>&1
+   ./Run_FDS_Cases.sh -C >> $OUTPUT_DIR/stage4 2>&1
 
    # Remove all .stop files from Verification directories (recursively)
    cd $fdsrepo/Verification
@@ -761,7 +761,7 @@ run_verification_cases_release()
 #   wait_cases_release_end 'verification'
 
 #  check whether cases have run 
-#   ./Run_FDS_Cases.sh -C  >> $OUTPUT_DIR/stage5 2>&1
+   ./Run_FDS_Cases.sh -C  >> $OUTPUT_DIR/stage5 2>&1
 }
 
 #---------------------------------------------
