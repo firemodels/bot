f22fb02a628aa437d549bcd43c50de336872f008
gforney <gforney@gmail.com>
2019-08-15 22:19:46 -0400

    repo scripts: UPDATE_thishost.sh moved from fds repo to bot repo

---
+++ Scripts/UPDATE_thishost.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+directory=$1
+
+cd ~/$directory
+git checkout master
+git remote update
+git merge origin/master
+git merge firemodels/master
+git push origin master
+git describe --dirty --long
