04f88d4275cacf36dfb2ab56fd5c85032d0bd7e8
gforney <gforney@gmail.com>
2019-10-18 22:42:01 -0400

    bundle scripts: remove unused script

--- Bundle/fds/scripts/bundle_setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export smokebot_home=/home2/smokevis2/smokebot
-export smokebot_host=blaze.el.nist.gov
-export firebot_home=/home2/smokevis2/firebot
-export firebot_host=blaze.el.nist.gov
