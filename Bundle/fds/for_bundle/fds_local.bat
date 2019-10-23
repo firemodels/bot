d1d831ba24818aed6273f19dfe0386f59a8736ea
gforney <gforney@gmail.com>
2019-10-23 13:30:28 -0400

    bundle script: correction to fds_local script

--- Bundle/fds/for_bundle/fds_local.bat
+++ Bundle/fds/for_bundle/fds_local.bat
@@ -18,7 +18,7 @@ if "%stop_script%" == "1" exit /b
 call :set_openmp_defaults
 
 if "%show_version%" == "0" goto skip_showversion
-  mpiexec -localonly fds
+  mpiexec -localonly -n 1 fds
 goto eof
 :skip_showversion
 
