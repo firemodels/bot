363fe56b91635d4c8bcda4a0ac6d8527be587fca
gforney <gforney@gmail.com>
2019-10-29 22:44:12 -0400

    bundle scripts: upload both bundle and sha files

--- Bundle/fds/scripts/upload_bundles.sh
+++ Bundle/fds/scripts/upload_bundles.sh
@@ -10,23 +10,49 @@ BUNDLE_PARENT_ID=1X-gRYGPGtcewgnNiNBuho3U8zDFVqFsC
 
 UPLOAD_BUNDLE ()
 {
-  ext=$1
+  platform=$1
+  if [ "$platform" == "win64" ]; then
+    ext=.exe
+  else
+    ext=.sh
+  fi
 
-  file=${BUNDLE_BASE}_$ext
-  if [ -e $BUNDLE_DIR/$file ]; then
-    $GDRIVE list  | grep $ext | awk '{ system("~/bin/gdrive delete -i " $1)} '
+  file=${BUNDLE_BASE}_$platform$ext
+  shafile=${BUNDLE_BASE}_${platform}.sha1
+
+  upload=1
+  if [ ! -e $BUNDLE_DIR/$file ]; then
+    echo "file doesn't exist"
+    upload=
+  fi
+  if [ ! -e $BUNDLE_DIR/$shafile ]; then
+    echo "shafile doesn't exist"
+    upload=
+  fi
+  if [ "$upload" == "1" ]; then
+    $GDRIVE list  | grep $platform | awk '{ system("~/bin/gdrive delete -i " $1)} '
     $GDRIVE upload -p $BUNDLE_PARENT_ID -f $BUNDLE_DIR/$file
     nfiles=`$GDRIVE list  | grep $file | wc -l`
     if [ $nfiles -eq 0 ]; then
       echo "*** warning: The bundle file $file failed to upload to google drive"
     fi
+    $GDRIVE upload -p $BUNDLE_PARENT_ID -f $BUNDLE_DIR/$shafile
+    nfiles=`$GDRIVE list  | grep $shafile | wc -l`
+    if [ $nfiles -eq 0 ]; then
+      echo "*** warning: The sha1 file $sha1file failed to upload to google drive"
+    fi
   else
-    echo "*** warning: The bundle file $BUNDLE_DIR/$file  does not exist"
+    if [ ! -e $BUNDLE_DIR/$file ]; then
+      echo "*** warning: The bundle file $BUNDLE_DIR/$file  does not exist"
+    fi
+    if [ ! -e $BUNDLE_DIR/$shafile ]; then
+      echo "*** warning: The sha1 file $BUNDLE_DIR/$shafile  does not exist"
+    fi
   fi
 }
 
 if [ -e $GDRIVE ] ; then
-  UPLOAD_BUNDLE linux64.sh
-  UPLOAD_BUNDLE osx64.sh
-  UPLOAD_BUNDLE win64.exe
+  UPLOAD_BUNDLE linux64
+#  UPLOAD_BUNDLE osx64
+#  UPLOAD_BUNDLE win64
 fi
