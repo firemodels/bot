d1f3aae8b09caf392e6134535ff722905d322244
gforney <gforney@gmail.com>
2019-11-01 13:57:16 -0400

    bundle scripts: synch script now copies hashes needed to do the synch

--- Scripts/synch_fdssmv.sh
+++ Scripts/synch_fdssmv.sh
@@ -3,6 +3,8 @@ acct=$1
 host=$2
 branch=$3
 
+eval acct=$acct
+
 #---------------------------------------------
 #                   CP
 #---------------------------------------------
@@ -11,20 +13,19 @@ CP ()
 {
   local FROMDIR=$1
   local FROMFILE=$2
-  local TODIR=$3
-  local TOFILE=$4
+  local TOFILE=$3
 
-  rm -f $TODIR/$TOFILE
+  rm -f $TOFILE
   if [[ "$host" != "" ]] && [[ "$host" != "`hostname`" ]]; then
-    scp -q $host:$FROMDIR/$FROMFILE $TODIR/$TOFILE
+    scp -q $host:$FROMDIR/$FROMFILE $TOFILE
   else
     if [ -e $FROMDIR/$FROMFILE ]; then
-      cp $FROMDIR/$FROMFILE $TODIR/TOFILE 
-      if [ -e $TODIR/$TOFILE ]; then
-        echo $FROMFILE copied to $TODIR
+      cp $FROMDIR/$FROMFILE $TOFILE 
+      if [ -e $TOFILE ]; then
+        echo $FROMFILE copied to $TOFILE
       else
         echo "" 
-        echo ***error: $FROMFILE failed to copy to $TODIR
+        echo ***error: $FROMFILE failed to copy to $TOFILE
         return_code=1
       fi
     else
@@ -46,6 +47,17 @@ cd $curdir
 cd $scriptdir/../../smv
 smvrepo=`pwd`
 
+FDS_HASH_FILE=/tmp/FDS_HASH.$$
+SMV_HASH_FILE=/tmp/SMV_HASH.$$
 
+eval CP \$acct/.firebot/apps FDS_HASH $FDS_HASH_FILE
+eval CP \$acct/.firebot/apps SMV_HASH $SMV_HASH_FILE
 
-eval ls $acct
+FDS_HASH=`cat $FDS_HASH_FILE`
+SMV_HASH=`cat $SMV_HASH_FILE`
+
+echo FDS_HASH=$FDS_HASH
+echo SMV_HASH=$SMV_HASH
+
+rm -f $FDS_HASH_FILE
+rm -f $SMV_HASH_FILE
