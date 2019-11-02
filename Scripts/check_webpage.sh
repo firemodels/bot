c72d26cc100a8a3f4b49f3147e979647159de84b
gforney <gforney@gmail.com>
2019-11-02 18:06:05 -0400

    scripts: ignore 301 Moved Permanently message when checking web links

--- Scripts/check_webpage.sh
+++ Scripts/check_webpage.sh
@@ -3,16 +3,19 @@ webpage=$1
 
 # get directory this (and checkurl.sh) script is run in
 args=$0
+
+curdir=`pwd`
 DIR=$(dirname "${args}")
 cd $DIR
 DIR=`pwd`
+cd $curdir
 
 
 links=/tmp/links.$$
 results=/tmp/results.$$
 
 sed -n 's/.*href="\([^"]*\).*/\1/p' $webpage  | grep http > $links
-$DIR/checkurl.sh $links | grep -v 200 | grep -v 302 > $results
+$DIR/check_url.sh $links | grep -v 200 | grep -v 302 | grep -v '301 Moved Permanently' > $results
 nresults=`cat $results | wc -l`
 if [ "$nresults" == "0" ]; then
   echo all links in $webpage are good
