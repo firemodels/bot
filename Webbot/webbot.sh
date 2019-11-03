c36b82bea032200d7c943563d6ea1038c8dfc45d
gforney <gforney@gmail.com>
2019-11-02 23:02:50 -0400

    webbot: add mising routines (CHK_REPO and CD_REPO)

--- Webbot/webbot.sh
+++ Webbot/webbot.sh
@@ -45,6 +45,47 @@ run_auto()
 }
 
 #---------------------------------------------
+#                   CHK_REPO
+#---------------------------------------------
+
+CHK_REPO ()
+{
+  local repodir=$1
+
+  if [ ! -e $repodir ]; then
+     echo "***error: the repo directory $repodir does not exist."
+     echo "          Aborting smokebot."
+     return 1
+  fi
+  return 0
+}
+
+#---------------------------------------------
+#                   CD_REPO
+#---------------------------------------------
+
+CD_REPO ()
+{
+  local repodir=$1
+  local branch=$2
+
+  CHK_REPO $repodir || return 1
+
+  cd $repodir
+  if [ "$branch" != "current" ]; then
+    if [ "$branch" != "" ]; then
+       CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
+       if [ "$CURRENT_BRANCH" != "$branch" ]; then
+         echo "***error: was expecting branch $branch in repo $repodir."
+         echo "Found branch $CURRENT_BRANCH. Aborting smokebot."
+         return 1
+       fi
+    fi
+  fi
+  return 0
+}
+
+#---------------------------------------------
 #                   MKDIR
 #---------------------------------------------
 
