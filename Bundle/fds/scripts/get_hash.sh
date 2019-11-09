47ec8c7d6bb17d9fbe17891909a3c30d4a8b1bfb
gforney <gforney@gmail.com>
2019-11-08 22:04:14 -0500

    bundle scripts: add argument parsing to get_hash script

--- Bundle/fds/scripts/get_hash.sh
+++ Bundle/fds/scripts/get_hash.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-# hashtype from_dir bot_host error_log
 
-hashtype=$1
-if [ "$hashtype" != "fds" ]; then
-  hashtype="smv"
-fi
-if [[ "$bot_host" != "" ]] && [[ "$bot_host" != "`hostname`" ]]; then
-  dir_from=$2
-else
-  eval dir_from=$2
-fi
-bot_host=$3
-error_log=$4
+#---------------------------------------------
+#                   usage
+#---------------------------------------------
+
+function usage {
+echo "This script outputs an fds or smv repo hash"
+echo ""
+echo "Options:"
+echo "-b - host where firebot was run" [default: $bot_host]"
+echo "-e - error log [default: $error_log]"
+echo "-f - from directory [default: $from_dir]"
+echo "-h - display this message"
+echo "-r - repo type fds or  smv [default: $repotype]"
+echo "-v - show parameters used to build bundle (the bundle is not generated)"
+exit 0
+}
 
 #---------------------------------------------
 #                   CP
@@ -48,17 +52,67 @@ CP ()
   fi
 }
 
+repotype=fds
+from_dir=\~firebot
+bot_host=`hostname`
+error_log=/tmp/get_hash_error_$USER.log
+SHOWPARMS=
+
+while getopts 'b:e:f:hr:v' OPTION
+do
+case $OPTION  in
+  b)
+   bot_host="$OPTARG"
+   ;;
+  e)
+   error_log="$OPTARG"
+   ;;
+  f)
+   from_dir="$OPTARG"
+   ;;
+  h)
+   usage
+   ;;
+  r)
+   repotype="$OPTARG"
+   if [ "$repotype" != "fds" ]; then
+     repotype="smv"
+   fi
+   ;;
+  v)
+   SHOWPARMS=1
+   ;;
+  \?)
+  echo "***error: unknown option entered. aborting script"
+  exit 1
+esac
+done
+shift $(($OPTIND-1))
+
+if [ "$SHOWPARMS" == "1" ]; then
+  echo bot_host="$bot_host"
+  echo error_log="$bot_host"
+  echo from_dir="$bot_host"
+  echo repo_type="$repo_type"
+  exit 0
+fi
+if [[ "$bot_host" != "" ]] && [[ "$bot_host" != "`hostname`" ]]; then
+  dir_from=$2
+else
+  eval dir_from=$2
+fi
+
 return_code=0
 
 dir_to=/tmp
 
-if [ "$hashtype" == "fds" ]; then
+if [ "$repotype" == "fds" ]; then
   CP FDS_HASH $dir_to/FDS_HASH.$$ || exit 1
   cat $dir_to/FDS_HASH.$$
   rm -f $dir_to/FDS_HASH.$$
 fi
 
-if [ "$hashtype" == "smv" ]; then
+if [ "$repotype" == "smv" ]; then
   CP SMV_HASH $dir_to/SMV_HASH.$$ || exit 1
   cat $dir_to/SMV_HASH.$$
   rm -f $dir_to/SMV_HASH.$$
