7f9f3f6f3536733e464a7c8834b253185ccdae17
gforney <gforney@gmail.com>
2019-11-18 14:31:32 -0500

    namelist compare: remove some extraneous characters that were messing up the diff

--- Firebot/compare_namelists.sh
+++ Firebot/compare_namelists.sh
@@ -6,11 +6,12 @@ rm -f output/namelists_diff.txt
 # generate list of namelist keywords found in FDS_User_Guide tex files
 tex_dir=../../fds/Manuals/FDS_User_Guide/
 awk -F'}' 'BEGIN{inlongtable=0;}{if($1=="\\begin{longtable"&&$4=="|l|l|l|l|l|"){inlongtable=1};if($1=="\\end{longtable"){inlongtable=0};if(inlongtable==1){print $0}}' $tex_dir/*.tex | \
-awk -F' ' 'BEGIN{output=0;namelist="xxx";}{if($1=="\\multicolumn{5}{|c|}{{\\ct"){namelist=$2;};if($1=="{\\ct"){keyword=$2;output=1;}else{output=0;};if(output==1){print "/"namelist"/"keyword;}}' | \
-tr -d '}' |  t\
-r -d '\\' | \
+awk -F' ' 'BEGIN{output=0;namelist="xxx";}{if($1=="\\multicolumn{5}{|c|}{{\\ct"){namelist=$2;};if($1=="{\\ct"){keyword=$2;if(keyword=="\\footnotesize"){keyword=$3;}output=1;}else{output=0;};if(output==1){print "/"namelist"/"keyword;}}' | \
+tr -d '}' | \
+tr -d '\\' | \
 awk -F'(' '{print $1}' | \
 tr -d ',' | \
+tr -d '&' | \
 sort > output/namelists_tex.txt
 echo   FDS user guide namelist keywords in output/namelists_tex.txt
 
