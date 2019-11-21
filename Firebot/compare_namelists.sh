6e013c762c1f293c10c6251db8cc623c307b2ccf
gforney <gforney@gmail.com>
2019-11-20 20:58:42 -0500

    namelist script: ignore namelists using info in .tex files (lines with % ignorenamelists)

--- Firebot/compare_namelists.sh
+++ Firebot/compare_namelists.sh
@@ -7,12 +7,28 @@ rm -f output/namelists_diff.txt
 rm -f output/namelists_nodoc.txt
 rm -f output/namelists_nosource.txt
 
-#ignore following namelists
-EXIT_PARMS=" -e /CORR/ -e /DOOR/ -e /EDEV/ -e /ENTR/ -e /EVAC/ -e /EVHO/ -e /EVSS/ -e /EXIT/ -e /PERS/ -e /STRS/ " 
-DUP_PARMS=" -e /TRNY/ -e /TRNZ/ " 
+# FDS UG directory
+tex_dir=../../fds/Manuals/FDS_User_Guide/
+# fds source directory
+input_dir=../../fds/Source
+
+#ignore namelists found on lines beginning with '% ignorenamelists' in .tex files in the fds/Manuals/FDS_User_Guide directory
+IGNORE_NAMELISTS=`grep ignorenamelists: $tex_dir/*.tex | \
+	awk -F' ' '\
+	{ \
+	  if(NF>2){ \
+            for(i=3; i<=NF; i++){\
+              print "-e /"$i"/ "\
+            }\
+          }\
+        }' | \
+       tr -d ','`
+# in case all ignorenamelists lines are removed from the .tex files
+if [ "$IGNORE_NAMELISTS" == "" ]; then
+  IGNORE_NAMELISTS="-e /dummy/"
+fi
 
 # generate list of namelist keywords found in FDS_User_Guide tex files
-tex_dir=../../fds/Manuals/FDS_User_Guide/
 grep -v ^% $tex_dir/*.tex | \
 awk -F'}' 'BEGIN{inlongtable=0;}{if($1=="\\begin{longtable"&&$4=="|l|l|l|l|l|"){inlongtable=1};if($1=="\\end{longtable"){inlongtable=0};if(inlongtable==1){print $0}}' $tex_dir/*.tex | \
 sed 's/&/ &/g' | \
@@ -49,11 +65,10 @@ tr -d '&' | \
 sed 's/,$//g' | \
 sed 's/,\//\//g' | \
 awk -F',' '{for(i=2; i<=NF; i++){print $1$i}}' | \
-grep -v $EXIT_PARMS $DUP_PARMS |\
+grep -v $IGNORE_NAMELISTS |\
 sort > output/namelists_tex.txt
 
 # generate list of namelist keywords found in FDS Fortran 90 source  files
-input_dir=../../fds/Source
 cat $input_dir/*.f90 | \
 awk -F'!' '{print $1}'  | \
 sed ':a;N;$!ba;s/& *\n//g' | \
@@ -61,7 +76,7 @@ tr -d ' ' | \
 grep ^NAMELIST | \
 awk -F'/' '{print "/"$2"/,"$3}'  | \
 awk -F',' '{for(i=2; i<=NF; i++){print $1$i}}' | \
-grep -v $EXIT_PARMS $DUP_PARMS |\
+grep -v $IGNORE_NAMELISTS |\
 sort > output/namelists_f90.txt
 
 #compute difference between tex and f90 namelist/keywords
