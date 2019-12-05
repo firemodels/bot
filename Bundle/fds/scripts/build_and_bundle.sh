351980fbccedf6ac27e1bd7378f1507247968468
gforney <gforney@gmail.com>
2019-12-05 11:37:24 -0500

    bundle scripts: add force clone option to build_and_bundle script

--- Bundle/fds/scripts/build_and_bundle.sh
+++ Bundle/fds/scripts/build_and_bundle.sh
@@ -9,10 +9,17 @@ if [ "$MAILTO" == "" ]; then
   exit 1
 fi
 
+JOPT="-J"
+if [ "`uname`" == "Darwin" ] ; then
+  JOPT=
+fi
+
+
 curdir=`pwd`
 
 cd ../../../Firebot
-./run_firebot.sh -c -B -g $FIREBOT_HOST -G \~firebot -J -R test -T -m $MAILTO
+echo "./run_firebot.sh -c -B -g $FIREBOT_HOST -G \~firebot $JOPT -R test -T -m $MAILTO"
+./run_firebot.sh -c -C -B -g $FIREBOT_HOST -G \~firebot $JOPT -R test -T -m $MAILTO
 
 cd $curdir
 ./run_bundler.sh -u -p $FIREBOT_HOST -w -t -g
