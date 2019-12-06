01f85a8be719867ba236eaafb2b9c09a81bbf5ec
gforney <gforney@gmail.com>
2019-12-06 11:23:19 -0500

    bundle scripts: preliminary windows batch file to checkout fds and smv repos using hashes from latest firebot pass

---
+++ Bundle/fds/scripts/clone_repos.bat
@@ -0,0 +1,31 @@
+@echo off
+
+call get_hash_revisions.bat
+set /p FDS_HASH=<output\FDS_HASH
+set /p SMV_HASH=<output\SMV_HASH
+
+set CURDIR=%CD%
+
+cd ..\..\..\Scripts
+::call setup_repos -T
+
+cd %CURDIR%
+
+cd ..\..\..\..\fds
+set fdsrepo=%CD%
+
+cd ..\smv
+set smvrepo=%CD%
+
+cd %fdsrepo%
+git checkout -b test %FDS_HASH%
+git describe --dirty --long
+git branch -a
+
+cd %smvrepo%
+git checkout -b test %SMV_HASH%
+git describe --dirty --long
+git branch -a
+
+cd %CURDIR%
+
