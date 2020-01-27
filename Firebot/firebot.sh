15423f373f155525ca676285d548d0d41c252b05
gforney <gforney@gmail.com>
2020-01-26 19:48:46 -0500

    firebot: exclude bot/Firebot/output directory from dos line ending search (false positives in output/stage1_clone file)

--- Firebot/firebot.sh
+++ Firebot/firebot.sh
@@ -107,7 +107,7 @@ find_CRLF()
   crlf_temp=/tmp/crlf.$$
 
   cd $repodir
-  grep -IURl --exclude="*.pdf" --exclude-dir=".git"  $'\r'  > $crlf_temp
+  grep -IURl --exclude="*.pdf" --exclude-dir=".git"  --exclude-dir="output" $'\r'  > $crlf_temp
   nlines=`cat $crlf_temp | wc -l`
   if [ $nlines -gt 0 ]; then
     echo ""                                                 >> $CRLF_WARNINGS
