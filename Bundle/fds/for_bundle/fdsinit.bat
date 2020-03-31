f44abdb62b5d20bab07a0e93436aeee6e2b463b9
gforney <gforney@gmail.com>
2020-03-31 08:53:38 -0400

    bundle scripts: uninitialize port range enviornment variables - were preventing multiple occurrences of fds from running

--- Bundle/fds/for_bundle/fdsinit.bat
+++ Bundle/fds/for_bundle/fdsinit.bat
@@ -1,7 +1,9 @@
 @echo off
-SET I_MPI_ROOT=%~dp0\mpi
-SET PATH=%I_MPI_ROOT%;%PATH%
-SET IN_CMDFDS=1
+set I_MPI_ROOT=%~dp0\mpi
+set PATH=%I_MPI_ROOT%;%PATH%
+set IN_CMDFDS=1
+set MPIEXEC_PORT_RANGE=
+set MPICH_PORT_RANGE=
 
 title FDS
 echo.
