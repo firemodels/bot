d9e621198a39b9bbc8308bd23dab0352c0ad0d26
gforney <gforney@gmail.com>
2020-07-06 14:37:27 -0400

    bundle scripts: update nightly linux/osx build bundle script (to use local files)

--- Bundlebot/build_and_bundle.sh
+++ Bundlebot/build_and_bundle.sh
@@ -1,19 +1,67 @@
 #!/bin/bash
-FIREBOT_HOST=$1
-MAILTO=$2
-FDS_RELEASE=$3
-SMV_RELEASE=$4
+
+#---------------------------------------------
+#                   usage
+#---------------------------------------------
+
+function usage {
+echo "This script builds FDS and Smokeview apps and genrates a bundle using"
+echo "specified fds and smv repo revisions or revisions from the latest firebot pass."
+echo ""
+echo "Options:"
+echo "-f - force this script to run"
+echo "-F - fds repo release"
+echo "-S - smv repo release]"
+echo "-h - display this message"
+echo "-H host - firebot host or LOCAL if revisions and documents are found at"
+echo "          $HOME/.firebot/pass"
+if [ "$MAILTO" != "" ]; then
+  echo "-m mailto - email address [default: $MAILTO]"
+else
+  echo "-m mailto - email address"
+fi
+exit 0
+}
+
+#-------------------- start of script ---------------------------------
+
+FIREBOT_HOST="LOCAL"
+MAILTO=
+if [ "$EMAIL" != "" ]; then
+  MAILTO = $EMAIL
+fi
+FDS_RELEASE=
+SMV_RELEASE=
 
 FORCE=
-#FORCE=-f
 
-if [ "$FIREBOT_HOST" == "" ]; then
-  exit 1
-fi
-if [ "$MAILTO" == "" ]; then
-  exit 1
-fi
+while getopts 'fF:hH:m:S:' OPTION
+do
+case $OPTION  in
+  f)
+   FORCE="-f"
+   ;;
+  F)
+   FDS_RELEASE="$OPTARG"
+   ;;
+  h)
+   usage
+   ;;
+  H)
+   FIREBOT_HOST="$OPTARG"
+   ;;
+  m)
+   MAILTO="$OPTARG"
+   ;;
+  S)
+   SMV_RELEASE="$OPTARG"
+   ;;
+esac
+done
+shift $(($OPTIND-1))
 
+
+# Linux or OSX
 JOPT="-J"
 if [ "`uname`" == "Darwin" ] ; then
   JOPT=
@@ -37,11 +85,24 @@ fi
 FIREBOT_BRANCH="-R $BRANCH"
 BUNDLE_BRANCH="-b $BRANCH"
 
+# get location of firebot files
+FIREBOT_HOME=\~firebot
+if [ "$FIREBOT_HOST" == "LOCAL" ]; then
+  FIREBOT_HOME=\$HOME/.firebot/pass
+fi
+
+# email address
+if [ "$MAILTO" != "" ]; then
+  MAILTO="-m $MAILTO"
+fi
+
 
 curdir=`pwd`
 
+# get apps and documents
 cd ../Firebot
-./run_firebot.sh $FORCE -c -C -B -g $FIREBOT_HOST -G \~firebot $JOPT $FDS_RELEASE $SMV_RELEASE $FIREBOT_BRANCH -T -m $MAILTO
+./run_firebot.sh $FORCE -c -C -B -g $FIREBOT_HOST -G $FIREBOT_HOME $JOPT $FDS_RELEASE $SMV_RELEASE $FIREBOT_BRANCH -T $MAILTO
 
+# generate bundle
 cd $curdir
 ./run_bundlebot.sh $FORCE $BUNDLE_BRANCH -p $FIREBOT_HOST -w -g
