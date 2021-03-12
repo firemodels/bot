66b2ff3543c0de0bf3a43beab9fd2c3fd93249c1
gforney <gforney@gmail.com>
2021-03-11 19:51:21 -0500

    firebot: add scripts that update fig images

---
+++ Smokebot/update_smv_fig_images.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+REPO=$1
+
+CURDIR=`pwd`
+if [ "$REPO" == "" ]; then
+  cd ../..
+  REPO=`pwd`
+  cd $CURDIR
+fi
+
+if [ ! -d  $REPO/fds ]; then
+  echo "***error: $REPO/fds does not exist"
+  exit
+fi
+
+if [ ! -d  $REPO/smv ]; then
+  echo "***error: $REPO/smv does not exist"
+  exit
+fi
+
+if [ ! -d  $REPO/fig ]; then
+  echo "***error: $REPO/fig does not exist"
+  exit
+fi
+
+echo getting fds repo revision
+cd $REPO/fds
+FDS_REPO=`pwd`
+FDS_REVISION=`git describe --dirty --long`
+
+echo getting smv repo revision
+cd $REPO/smv
+SMV_REPO=`pwd`
+SMV_REVISION=`git describe --dirty --long`
+
+FIG_DIR=$REPO/fig/compare/smokebot/images
+
+echo copying SMV user guide figures
+cp $FDS_REPO/Manuals/SMV_User_Guide/SCRIPT_FIGURES/*.png $FIG_DIR/fig/user/.
+echo $FDS_REVISION > $FIG_DIR/user/FDS_REVISION
+echo $SMV_REVISION > $FIG_DIR/user/SMV_REVISION
+
+echo copying SMV verificaiton guide figures
+cp $FDS_REPO/Manuals/SMV_Verification_Guide/SCRIPT_FIGURES/*.png $FIG_DIR/verification/.
+echo $FDS_REVISION > $FIG_DIR/verification/FDS_REVISION
+echo $SMV_REVISION > $FIG_DIR/verification/SMV_REVISION
+
+
+
+
