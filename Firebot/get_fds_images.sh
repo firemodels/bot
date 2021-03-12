56cc48910ba2950a171bd6902cb8cc92ae402fa2
gforney <gforney@gmail.com>
2021-03-11 20:24:18 -0500

    scripts: delete an unused script

--- Firebot/get_fds_images.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-ROOT=$1
-
-if [ "$ROOT" == "" ]; then
-  ROOT=../../../..
-fi
-
-cp $ROOT/fds/Manuals/FDS_User_Guide/SCRIPT_FIGURES/*.png         user/.
-cp $ROOT/fds/Manuals/FDS_Verification_Guide/SCRIPT_FIGURES/*.png verification/.
