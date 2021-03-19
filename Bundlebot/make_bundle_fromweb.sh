8fd67c8c17e3191cbef5efc663dba5cb8cdfd4c2
Forney <gforney@gmail.com>
2021-03-18 22:17:41 -0400

    bundle scripts: add parameter for osx openmpi directory to a bundle script

--- Bundlebot/make_bundle_fromweb.sh
+++ Bundlebot/make_bundle_fromweb.sh
@@ -3,10 +3,13 @@ fds_version=$1
 smv_version=$2
 mpi_version=$3
 intel_mpi_version=$4
+openmpi_dir=$5
 
 export NOPAUSE=1
+if [ "$openmpi_dir" != "" ]; then
+  export FDS_OPENMPIDIR=$openmpi_dir
+fi
 args=$0
 DIR=$(dirname "${args}")
 cd $DIR
-echo ./bundle_generic.sh $fds_version $smv_version $mpi_version $intel_mpi_version
 ./bundle_generic.sh $fds_version $smv_version $mpi_version $intel_mpi_version
