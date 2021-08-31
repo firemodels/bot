bf7eba2e8531a2f190e4918389b6c098c8be9f17
gforney <gforney@gmail.com>
2021-08-30 21:51:26 -0400

    bundle scripts: add script to create a directory of windows run time files

---
+++ Bundle/fds/scripts/make_rt_windows.bat
@@ -0,0 +1,47 @@
+@echo off
+set INTELDIR=INTELoneapiu3
+
+set TODIRBASE=%userprofile%\.bundle\BUNDLE\WINDOWS
+set TODIR=%TODIRBASE%\%INTELDIR%
+
+set CURDIR=%CD%
+cd %TODIRBASE%
+if exist %INTELDIR% rmdir /s /q %INTELDIR%
+mkdir %INTELDIR%
+
+set "FROMDIR=%I_MPI_ONEAPI_ROOT%\bin"
+
+call :copyfile hydra_bstrap_proxy.exe
+call :copyfile hydra_pmi_proxy.exe
+call :copyfile hydra_service.exe
+call :copyfile IMB-MPI1.exe
+call :copyfile IMB-NBC.exe
+call :copyfile IMB-RMA.exe
+call :copyfile libmpi_ilp64.dll
+call :copyfile mpiexec.exe
+
+set "FROMDIR=%I_MPI_ONEAPI_ROOT%\bin\release"
+
+call :copyfile impi.dll
+
+set "FROMDIR=%I_MPI_ONEAPI_ROOT%\libfabric\bin"
+
+call :copyfile libfabric.dll
+goto eof
+
+::---------------------------------------------
+  :copyfile
+::---------------------------------------------
+
+set FROMFILE=%1
+
+if NOT exist "%FROMDIR%\%FROMFILE%" echo "***error: %FROMFILE% was not found in %FROMDIR%"
+if NOT exist "%FROMDIR%\%FROMFILE%" exit /b
+
+copy "%FROMDIR%\%FROMFILE%" %TODIR%\%FROMFILE% > Nul 2> Nul
+if exist "%TODIR%\%FROMFILE%" echo %FROMFILE% copied
+if NOT exist %TODIR%\%FROMFILE% echo %FROMFILE% failed to copy
+exit /b
+
+:eof
+cd %CURDIR%
