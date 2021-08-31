229ab7bbee8ec699c31eccd88a50e071c0ef72a0
gforney <gforney@gmail.com>
2021-08-31 08:13:16 -0400

    bundle scripts: minor formattig edits in windows rt file copy script

--- Bundle/fds/scripts/make_rt_windows.bat
+++ Bundle/fds/scripts/make_rt_windows.bat
@@ -11,6 +11,9 @@ mkdir %INTELDIR%
 
 set "FROMDIR=%I_MPI_ONEAPI_ROOT%\bin"
 
+echo.
+echo ***copying files from %FROMDIR%
+echo.
 call :copyfile hydra_bstrap_proxy.exe
 call :copyfile hydra_pmi_proxy.exe
 call :copyfile hydra_service.exe
@@ -22,11 +25,20 @@ call :copyfile mpiexec.exe
 
 set "FROMDIR=%I_MPI_ONEAPI_ROOT%\bin\release"
 
+echo.
+echo ***copying files from %FROMDIR%
+echo.
 call :copyfile impi.dll
 
 set "FROMDIR=%I_MPI_ONEAPI_ROOT%\libfabric\bin"
 
+echo.
+echo ***copying files from %FROMDIR%
+echo.
 call :copyfile libfabric.dll
+
+echo.
+echo ***copy complete
 goto eof
 
 ::---------------------------------------------
@@ -39,8 +51,8 @@ if NOT exist "%FROMDIR%\%FROMFILE%" echo "***error: %FROMFILE% was not found in 
 if NOT exist "%FROMDIR%\%FROMFILE%" exit /b
 
 copy "%FROMDIR%\%FROMFILE%" %TODIR%\%FROMFILE% > Nul 2> Nul
-if exist "%TODIR%\%FROMFILE%" echo %FROMFILE% copied
-if NOT exist %TODIR%\%FROMFILE% echo %FROMFILE% failed to copy
+if exist "%TODIR%\%FROMFILE%" echo    %FROMFILE% copied
+if NOT exist %TODIR%\%FROMFILE% echo ***error: %FROMFILE% failed to copy
 exit /b
 
 :eof
