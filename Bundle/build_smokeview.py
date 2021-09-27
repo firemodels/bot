d462a8aeb8468e94a335b453bac0d37ecd8ae42d
gforney <gforney@gmail.com>
2021-09-26 23:34:34 -0400

    scripts: simplify build script

--- Bundle/build_smokeview.py
+++ Bundle/build_smokeview.py
@@ -67,225 +67,150 @@ repos_label.grid(column=1, row=R, columnspan=2)
 R=R+1
 show_label = Label(root, text="Show:")
 show_label.grid(column=0, row=R, sticky=E)
-b = Button(root, text="Branch",       command=show_branch)
-b.grid(row=R, column=1)
-c = Button(root, text="Revision",        command=show_repos)
-c.grid(row=R, column=2)
+Button(root, text="Branch",       command=show_branch).grid(row=R, column=1)
+Button(root, text="Revision",     command=show_repos).grid(row=R, column=2)
 
 # ------------------------- Update repos ------------------------------
 
 R=R+1
 update_label = Label(root, text="Update:")
 update_label.grid(column=0, row=R, sticky=E)
-d = Button(root, text="Windows",    command=update_windows)
-d.grid(row=R, column=1)
-
-e = Button(root, text="All",        command=update_all)
-e.grid(row=R, column=2)
+Button(root, text="Windows",    command=update_windows).grid(row=R, column=1)
+Button(root, text="All",        command=update_all).grid(row=R, column=2)
 
 # ------------------------- Set  ------------------------------
 
 R=R+1
-set_label = Label(root, text="Set:")
-set_label.grid(column=0, row=R, sticky=E)
-f = Button(root, text="Bundle rev",      command=set_revision)
-f.grid(row=R, column=1)
-f = Button(root, text="Master branch",      command=set_branch)
-f.grid(row=R, column=2)
+Label(root, text="Set:").grid(column=0, row=R, sticky=E)
+Button(root, text="Bundle rev",      command=set_revision).grid(row=R, column=1)
+Button(root, text="Master branch",   command=set_branch).grid(row=R, column=2)
 
 # ------------------------- clean ------------------------------
 
 R=R+1
-clean_label = Label(root, text="Clean")
-clean_label.grid(column=1, row=R, columnspan=2)
-R=R+1
-set_clean_repos = Label(root, text="repos:")
-set_clean_repos.grid(column=0, row=R, sticky=E)
-ee = Button(root, text="Windows",      command=clean_repos_win)
-ee.grid(row=R, column=1)
-ff = Button(root, text="All",      command=clean_repos_all)
-ff.grid(row=R, column=2)
+Label(root, text="Clean").grid(column=1, row=R, columnspan=2)
 
 R=R+1
-set_clean_uploads = Label(root, text="uploads:")
-set_clean_uploads.grid(column=0, row=R, sticky=E)
-gg = Button(root, text="Windows",      command=clean_uploads_win)
-gg.grid(row=R, column=1)
-hh = Button(root, text="All",      command=clean_uploads_all)
-hh.grid(row=R, column=2)
+Label(root, text="repos:").grid(column=0, row=R, sticky=E)
+Button(root, text="Windows",  command=clean_repos_win).grid(row=R, column=1)
+Button(root, text="All",      command=clean_repos_all).grid(row=R, column=2)
 
 R=R+1
-set_clean_smv = Label(root, text="SMV build dirs:")
-set_clean_smv.grid(column=0, row=R, sticky=E)
-ii = Button(root, text="Windows",      command=clean_smv_win)
-ii.grid(row=R, column=1)
-jj = Button(root, text="All",      command=clean_smv_all)
-jj.grid(row=R, column=2)
+Label(root, text="uploads:").grid(column=0, row=R, sticky=E)
+Button(root, text="Windows",  command=clean_uploads_win).grid(row=R, column=1)
+Button(root, text="All",      command=clean_uploads_all).grid(row=R, column=2)
+
+R=R+1
+Label(root, text="SMV build dirs:").grid(column=0, row=R, sticky=E)
+Button(root, text="Windows",  command=clean_smv_win).grid(row=R, column=1)
+Button(root, text="All",      command=clean_smv_all).grid(row=R, column=2)
 
 # ------------------------- Edit ------------------------------
 
 R=R+1
-edit_label = Label(root, text="Edit")
-edit_label.grid(column=1, row=R, columnspan=3)
-R=R+1
-release_notes = Label(root, text="Release notes:")
-release_notes.grid(column=0, row=R, sticky=E)
-aaa = Button(root, text="Add",      command=add_notes)
-aaa.grid(row=R, column=1)
-bbb = Button(root, text="Edit",      command=edit_notes)
-bbb.grid(row=R, column=2)
-ccc = Button(root, text="View",      command=view_notes)
-ccc.grid(row=R, column=3)
+Label(root, text="Edit").grid(column=1, row=R, columnspan=3)
 
 R=R+1
-release_notes = Label(root, text="Edit:")
-release_notes.grid(column=0, row=R, sticky=E)
-ddd = Button(root, text="This script",      command=edit_this_page)
-ddd.grid(row=R, column=1)
-eee = Button(root, text="Settings",      command=edit_settings)
-eee.grid(row=R, column=2)
+Label(root, text="Release notes:").grid(column=0, row=R, sticky=E)
+Button(root, text="Add",      command=add_notes).grid(row=R, column=1)
+Button(root, text="Edit",     command=edit_notes).grid(row=R, column=2)
+Button(root, text="View",     command=view_notes).grid(row=R, column=3)
+
+R=R+1
+Label(root, text="Script:").grid(column=0, row=R, sticky=E)
+Button(root, text="Edit",    command=edit_this_page).grid(row=R, column=1)
+Button(root, text="Refresh", command=restart_program).grid(row=R, column=2)
+
+R=R+1
+Label(root, text="Settings:").grid(column=0, row=R, sticky=E)
+Button(root, text="Edit",      command=edit_settings).grid(row=R, column=1)
 
 # ------------------------- Platform labels ------------------------------
 
 R=R+1
-build_label = Label(root, text="Build libraries/utilities")
-build_label.grid(column=1, row=R, columnspan=3)
+Label(root, text="Build libraries/utilities").grid(column=1, row=R, columnspan=3)
 
 R=R+1
-windows_label = Label(root, text="Windows")
-windows_label.grid(column=1, row=R)
-linux_label = Label(root, text="Linux")
-linux_label.grid(column=2, row=R)
-osx_label = Label(root, text="OSX")
-osx_label.grid(column=3, row=R)
+Label(root, text="Windows").grid(column=1, row=R)
+Label(root, text="Linux").grid(column=2, row=R)
+Label(root, text="OSX").grid(column=3, row=R)
 
 # ------------------------- Build libraries ------------------------------
 
 R=R+1
-lib_label = Label(root, text="Libraries:")
-lib_label.grid(column=0, row=R, sticky=E)
-g = Button(root, text="Build",     command=partial(build_lib, "windows"))
-g.grid(row=R, column=1)
-
-h = Button(root, text="Build",     command=partial(build_lib, "linux"))
-h.grid(row=R, column=2)
-
-i = Button(root, text="Build",     command=partial(build_lib, "osx"))
-i.grid(row=R, column=3)
+Label(root, text="Libraries:").grid(column=0, row=R, sticky=E)
+Button(root, text="Build",     command=partial(build_lib, "windows")).grid(row=R, column=1)
+Button(root, text="Build",     command=partial(build_lib, "linux")).grid(row=R, column=2)
+Button(root, text="Build",     command=partial(build_lib, "osx")).grid(row=R, column=3)
 
 # ------------------------- Build utilities ------------------------------
 
 R=R+1
-util_label = Label(root, text="Utilities:")
-util_label.grid(column=0, row=R, sticky=E)
-j = Button(root, text="Build",     command=partial(build_util, "windows"))
-j.grid(row=R, column=1)
-
-k = Button(root, text="Build",     command=partial(build_util, "linux"))
-k.grid(row=R, column=2)
-
-l = Button(root, text="Build",     command=partial(build_util, "osx"))
-l.grid(row=R, column=3)
+Label(root, text="Utilities:").grid(column=0, row=R, sticky=E)
+Button(root, text="Build",     command=partial(build_util, "windows")).grid(row=R, column=1)
+Button(root, text="Build",     command=partial(build_util, "linux")).grid(row=R, column=2)
+Button(root, text="Build",     command=partial(build_util, "osx")).grid(row=R, column=3)
 
 # ------------------------- Build smokeview ------------------------------
 
 R=R+1
-build2_label = Label(root, text="Test Smokeview")
-build2_label.grid(column=1, row=R, columnspan=3)
+Label(root, text="Test Smokeview").grid(column=1, row=R, columnspan=3)
 
 R=R+1
-windows2_label = Label(root, text="Windows")
-windows2_label.grid(column=1, row=R)
-linux2_label = Label(root, text="Linux")
-linux2_label.grid(column=2, row=R)
-osx2_label = Label(root, text="OSX")
-osx2_label.grid(column=3, row=R)
+Label(root, text="Windows").grid(column=1, row=R)
+Label(root, text="Linux").grid(column=2, row=R)
+Label(root, text="OSX").grid(column=3, row=R)
 
 R=R+1
 R2=R+1
-smokeview_label = Label(root, text="Smokeview:")
-smokeview_label.grid(column=0, row=R, sticky=E)
-m = Button(root, text="Build",     command=partial(build_smv, "windows", "test") )
-m.grid(row=R, column=1)
-
-n = Button(root, text="Build inc", command=build_smv_win_inc)
-n.grid(row=R2, column=1)
-
-o = Button(root, text="Build",     command=partial(build_smv, "linux", "test") )
-o.grid(row=R, column=2)
-
-p = Button(root, text="Build",     command=partial(build_smv, "osx", "test") )
-p.grid(row=R, column=3)
+Label(root, text="Smokeview:").grid(column=0, row=R, sticky=E)
+Button(root, text="Build",     command=partial(build_smv, "windows", "test") ).grid(row=R, column=1)
+Button(root, text="Build inc", command=build_smv_win_inc).grid(row=R2, column=1)
+Button(root, text="Build",     command=partial(build_smv, "linux", "test") ).grid(row=R, column=2)
+Button(root, text="Build",     command=partial(build_smv, "osx", "test") ).grid(row=R, column=3)
 
 # ------------------------- bundle smokeview ------------------------------
 
 R=R+2
-bundle_label = Label(root, text="Bundle:")
-bundle_label.grid(column=0, row=R, sticky=E)
-q = Button(root, text="Bundle",     command=partial(bundle_smv, "windows", "test"))
-q.grid(row=R, column=1)
-
-r = Button(root, text="Bundle",     command=partial(bundle_smv, "linux", "test"))
-r.grid(row=R, column=2)
-
-s = Button(root, text="Bundle",     command=partial(bundle_smv, "osx", "test"))
-s.grid(row=R, column=3)
+Label(root, text="Bundle:").grid(column=0, row=R, sticky=E)
+Button(root, text="Bundle",     command=partial(bundle_smv, "windows", "test")).grid(row=R, column=1)
+Button(root, text="Bundle",     command=partial(bundle_smv, "linux", "test")).grid(row=R, column=2)
+Button(root, text="Bundle",     command=partial(bundle_smv, "osx", "test")).grid(row=R, column=3)
 
 # ------------------------- archive smokeview ------------------------------
 
 R=R+1
-t = Button(root, text="Archive",   command=partial(archive_smv, "linux", "test"))
-t.grid(row=R, column=2)
-
-u = Button(root, text="Archive",   command=partial(archive_smv, "osx", "test"))
-u.grid(row=R, column=3)
+Button(root, text="Archive",   command=partial(archive_smv, "linux", "test")).grid(row=R, column=2)
+Button(root, text="Archive",   command=partial(archive_smv, "osx", "test")).grid(row=R, column=3)
 
 # ------------------------- install smokeview ------------------------------
 
 R=R+1
-install_label = Label(root, text="Install:")
-install_label.grid(column=0, row=R, sticky=E)
-v = Button(root, text="Install",     command=partial(install_smv, "windows", "test"))
-v.grid(row=R, column=1)
-w = Button(root, text="Install",     command=partial(install_smv, "linux", "test"))
-w.grid(row=R, column=2)
-x = Button(root, text="Install",     command=partial(install_smv, "osx", "test"))
-x.grid(row=R, column=3)
+Label(root, text="Install:").grid(column=0, row=R, sticky=E)
+Button(root, text="Install",     command=partial(install_smv, "windows", "test")).grid(row=R, column=1)
+Button(root, text="Install",     command=partial(install_smv, "linux", "test")).grid(row=R, column=2)
+Button(root, text="Install",     command=partial(install_smv, "osx", "test")).grid(row=R, column=3)
 
 # ------------------------- upload smv bundle ------------------------------
 
 R=R+1
-upload_label = Label(root, text="Upload:")
-upload_label.grid(column=0, row=R, sticky=E)
-y = Button(root, text="Upload",     command=upload_bundle)
-y.grid(row=R, column=2)
-z = Button(root, text="Copy",       command=copy_bundle)
-z.grid(row=R, column=3)
+Label(root, text="Upload:").grid(column=0, row=R, sticky=E)
+Button(root, text="Upload",     command=upload_bundle).grid(row=R, column=2)
+Button(root, text="Copy",       command=copy_bundle).grid(row=R, column=3)
 
 # ------------------------- synchronize ------------------------------
 
 R=R+1
-synch_label = Label(root, text="Sychronize")
-synch_label.grid(column=1, row=R, columnspan=2)
+Label(root, text="Sychronize").grid(column=1, row=R, columnspan=2)
 R=R+1
-synch2_label = Label(root, text="settings:")
-synch2_label.grid(column=0, row=R, sticky=E)
-aa = Button(root, text="==>>smv",     command=webCOPYhome2config)
-aa.grid(row=R, column=1)
-bb = Button(root, text="<<==smv",     command=webCOPYconfig2home)
-bb.grid(row=R, column=2)
+Label(root, text="settings:").grid(column=0, row=R, sticky=E)
+Button(root, text="==>>smv",     command=webCOPYhome2config).grid(row=R, column=1)
+Button(root, text="<<==smv",     command=webCOPYconfig2home).grid(row=R, column=2)
 
 
 R=R+1
-gsmv_label = Label(root, text="gsmv/bib:")
-gsmv_label.grid(column=0, row=R, sticky=E)
-cc = Button(root, text="fds==>>smv",     command=webSYNCHfds2smv)
-cc.grid(row=R, column=1)
-dd = Button(root, text="fds<<==smv",     command=webSYNCHsmv2fds)
-dd.grid(row=R, column=2)
-
-R=R+1
-Button(root, text="Refresh", command=restart_program).grid(row=R, column=0)
-
+Label(root, text="gsmv/bib:").grid(column=0, row=R, sticky=E)
+Button(root, text="fds==>>smv",     command=webSYNCHfds2smv).grid(row=R, column=1)
+Button(root, text="fds<<==smv",     command=webSYNCHsmv2fds).grid(row=R, column=2)
 
 root.mainloop()
