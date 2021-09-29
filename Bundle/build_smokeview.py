da4080dbaaf68a2cf4f4bd22a9e4185c0b9757fb
gforney <gforney@gmail.com>
2021-09-28 21:58:17 -0400

    build scripts: minor formatting edit

--- Bundle/build_smokeview.py
+++ Bundle/build_smokeview.py
@@ -73,21 +73,21 @@ Label(root, text="Repos").grid(column=0, row=R, columnspan=3)
 R=R+1
 Label(root, text="Show:").grid(column=0, row=R, sticky=E)
 Button(root, text="Branch",       width=button_width, command=show_branch).grid(row=R, column=1)
-Button(root, text="Revision",     width=button_width, command=show_repos).grid(row=R, column=2)
+Button(root, text="Revision",     width=button_width, command=show_repos).grid(row=R,  column=2)
 
 # ------------------------- Update repos ------------------------------
 
 R=R+1
 Label(root, text="Update:").grid(column=0, row=R, sticky=E)
 Button(root, text="Windows",    width=button_width, command=update_windows).grid(row=R, column=1)
-Button(root, text="All",        width=button_width, command=update_all).grid(row=R, column=2)
+Button(root, text="All",        width=button_width, command=update_all).grid(row=R,     column=2)
 
 # ------------------------- Set  ------------------------------
 
 R=R+1
 Label(root, text="Set:").grid(column=0, row=R, sticky=E)
 Button(root, text="Bundle",   width=button_width, command=set_revision).grid(row=R, column=1)
-Button(root, text="Master",   width=button_width, command=set_branch).grid(row=R, column=2)
+Button(root, text="Master",   width=button_width, command=set_branch).grid(row=R,   column=2)
 
 # ------------------------- clean ------------------------------
 
@@ -109,10 +109,9 @@ Radiobutton(root,
                value=2).grid(row=R, column=1)
 
 R=R+1
-Label(root, text="Clean:").grid(column=0, row=R)
-Button(root, text="Repos",   width=button_width, command=clean_repos).grid(row=R,   column=1)
-Button(root, text="Uploads", width=button_width, command=clean_uploads).grid(row=R, column=2)
-Button(root, text="SMV",     width=button_width, command=clean_smv).grid(row=R,     column=3)
+Button(root, text="Repos",   width=button_width, command=clean_repos).grid(row=R,   column=0)
+Button(root, text="Uploads", width=button_width, command=clean_uploads).grid(row=R, column=1)
+Button(root, text="Smv",     width=button_width, command=clean_smv).grid(row=R,     column=2)
 
 # ------------------------- Edit ------------------------------
 
@@ -121,14 +120,13 @@ Label(root, text="Edit").grid(column=0, row=R, columnspan=4)
 
 R=R+1
 Label(root, text="Notes:").grid(column=0, row=R, sticky=E)
-Button(root, text="Add",      width=button_width, command=add_notes).grid(row=R, column=1)
+Button(root, text="Add",      width=button_width, command=add_notes).grid(row=R,  column=1)
 Button(root, text="View",     width=button_width, command=view_notes).grid(row=R, column=2)
 
 R=R+1
 Label(root, text="Edit:").grid(column=0, row=R, sticky=E)
 Button(root, text="Script",    width=button_width, command=edit_this_page).grid(row=R, column=1)
-Button(root, text="Settings",  width=button_width, command=edit_settings).grid(row=R, column=2)
-Button(root, text="Notes",     width=button_width, command=edit_notes).grid(row=R, column=3)
+Button(root, text="Settings",  width=button_width, command=edit_settings).grid(row=R,  column=2)
 
 # ------------------------- Build/Bundle/Install ------------------------------
 
@@ -158,22 +156,22 @@ Radiobutton(root,
 # ------------------------- Build libraries ------------------------------
 
 R=R+1
-Button(root, text="Build Libs",  width=button_width, command=build_lib).grid(row=R, column=0)
+Button(root, text="Build Libs",  width=button_width, command=build_lib).grid(row=R,  column=0)
 Button(root, text="Build Utils", width=button_width, command=build_util).grid(row=R, column=1)
-Button(root, text="Build SMV",   width=button_width, command=build_smv).grid(row=R, column=2)
-Button(root, text="INC",         width=button_width, command=build_smv_win_inc).grid(row=R, column=3)
+Button(root, text="Build Smv",   width=button_width, command=build_smv).grid(row=R,  column=2)
 
 # ------------------------- bundle smokeview ------------------------------
 
 R=R+1
-Button(root, text="Bundle",  width=button_width, command=bundle_smv).grid(row=R, column=0)
-Button(root, text="Install", width=button_width, command=install_smv).grid(row=R, column=1)
+Button(root, text="Bundle",  width=button_width, command=bundle_smv).grid(row=R,        column=0)
+Button(root, text="Install", width=button_width, command=install_smv).grid(row=R,       column=1)
+Button(root, text="Inc Smv",     width=button_width, command=build_smv_win_inc).grid(row=R, column=2)
 
 
 R=R+1
 Button(root, text="Upload",  width=button_width, command=upload_bundle).grid(row=R, column=0)
-Button(root, text="Copy",    width=button_width, command=copy_bundle).grid(row=R, column=1)
-Button(root, text="Archive", width=button_width, command=archive_smv).grid(row=R, column=2)
+Button(root, text="Copy",    width=button_width, command=copy_bundle).grid(row=R,   column=1)
+Button(root, text="Archive", width=button_width, command=archive_smv).grid(row=R,   column=2)
 
 # ------------------------- synchronize ------------------------------
 
