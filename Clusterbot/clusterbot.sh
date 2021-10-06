4d0ea62d15a14da7fa72b3598fb51129f1aad983
gforney <gforney@gmail.com>
2021-10-05 22:35:35 -0400

    clusterbot: make more general using environment variables (don't hardwire to a specific cluster)

--- Clusterbot/clusterbot.sh
+++ Clusterbot/clusterbot.sh
@@ -1,15 +1,53 @@
 #!/bin/bash
 
+ERROR=
+if [ "$CB_HOSTS" == "" ]; then
+  ERROR=1
+  echo "***error: environment variable CB_HOSTS not define"
+fi
+if [[ "$CB_HOST1" != "" ]] && [[ "$CB_HOSTIB1" == "" ]]; then
+  ERROR=1
+  echo "***error: CB_HOSTIB1 must be defined if CB_HOST1 is defined"
+fi
+if [[ "$CB_HOST2" != "" ]] && [[ "$CB_HOSTIB2" == "" ]]; then
+  ERROR=1
+  echo "***error: CB_HOSTIB2 must be defined if CB_HOST2 is defined"
+fi
+if [[ "$CB_HOST3" != "" ]] && [[ "$CB_HOSTIB3" == "" ]]; then
+  ERROR=1
+  echo "***error: CB_HOSTIB3 must be defined if CB_HOST3 is defined"
+fi
+if [[ "$CB_HOST4" != "" ]] && [[ "$CB_HOSTIB4" == "" ]]; then
+  ERROR=1
+  echo "***error: CB_HOSTIB4 must be defined if CB_HOST4 is defined"
+fi
+if [ "$ERROR" == "1" ]; then
+  exit
+fi
+if [ "$CB_HOST1" != "" ]; then
+  HAVE_IB=1
+fi
+if [ "$CB_HOST2" != "" ]; then
+  HAVE_IB=1
+fi
+if [ "$CB_HOST3" != "" ]; then
+  HAVE_IB=1
+fi
+if [ "$CB_HOST4" != "" ]; then
+  HAVE_IB=1
+fi
+
+export CHECK_CLUSTER=1
 ETHOUT=/tmp/out.$$
 FSOUT=/tmp/fsout.$$
 IBOUT=/tmp/ibout.$$
 
 # check ethernet
-pdsh -t 2 -w blaze[001-144] date   >& $ETHOUT
+pdsh -t 2 -w $CB_HOSTS date   >& $ETHOUT
 ETHDOWN=`grep timed  $ETHOUT | grep out | awk '{printf "%s%s", $6," " }'`
 
 # check file systems
-pdsh -t 2 -w blaze[001-144] "df -k -t nfs | wc -l" |&  grep -v ssh | sort >& $FSOUT
+pdsh -t 2 -w $CB_HOSTS "df -k -t nfs | wc -l" |&  grep -v ssh | sort >& $FSOUT
 
 NF0=`head -1 $FSOUT | awk '{print $2}'`
 FSDOWN=
@@ -36,16 +74,26 @@ else
 fi
 
 # check infiniband
-echo "" > $IBOUT
-ssh blaze001 pdsh -t 2 -w blaze[001-036]-ib date  >>  $IBOUT 2>&1
-ssh blaze037 pdsh -t 2 -w blaze[037-072]-ib date  >>  $IBOUT 2>&1
-ssh blaze073 pdsh -t 2 -w blaze[073-108]-ib date  >>  $IBOUT 2>&1
-ssh blaze109 pdsh -t 2 -w blaze[109-144]-ib date  >>  $IBOUT 2>&1
-IBDOWN=`grep timed  $IBOUT | grep out | awk '{printf "%s%s", $6," " }'`
-if [ "$IBDOWN" == "" ]; then
-  echo "all infiniband interfaces are working"
-else
-  echo "hosts down(IB): $IBDOWN"
+if [ "$HAVE_IB" == "1" ]; then
+  echo "" > $IBOUT
+  if [ "$CB_HOST1" != "" ]; then
+    ssh $CB_HOST1 pdsh -t 2 -w $CB_HOSTIB1 date  >>  $IBOUT 2>&1
+  fi
+  if [ "$CB_HOST2" != "" ]; then
+    ssh $CB_HOST2 pdsh -t 2 -w $CB_HOSTIB2 date  >>  $IBOUT 2>&1
+  fi
+  if [ "$CB_HOST3" != "" ]; then
+    ssh $CB_HOST3 pdsh -t 2 -w $CB_HOSTIB3 date  >>  $IBOUT 2>&1
+  fi
+  if [ "$CB_HOST4" != "" ]; then
+    ssh $CB_HOST4 pdsh -t 2 -w $CB_HOSTIB4 date  >>  $IBOUT 2>&1
+  fi
+  IBDOWN=`grep timed  $IBOUT | grep out | awk '{printf "%s%s", $6," " }'`
+  if [ "$IBDOWN" == "" ]; then
+    echo "all infiniband interfaces are working"
+  else
+    echo "hosts down(IB): $IBDOWN"
+  fi
 fi
 
 # cleanup
