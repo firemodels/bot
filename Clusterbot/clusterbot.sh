49d95a012fe6caae218077d74adeadde06312572
gforney <gforney@gmail.com>
2021-11-04 11:11:33 -0400

    clusterbot: fix typo

--- Clusterbot/clusterbot.sh
+++ Clusterbot/clusterbot.sh
@@ -858,7 +858,7 @@ if [ "$DOWN_IB" != "" ]; then
     fi
   done
 fi
-if [ "$IB_LIST" != "" ]; then
+if [ "$IB_LIST" == "" ]; then
   echo "   $CB_HOSTS: infiniband up on all hosts with working ethernet"
 else
   echo "   $CB_HOSTS: ***Error: ethernet up, infiniband down on $IB_LIST"
