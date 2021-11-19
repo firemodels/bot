4c5b849906367b3075e29e22e42957bf5e7ff467
gforney <gforney@gmail.com>
2021-11-18 21:21:11 -0500

    bundle scripts: update build manuals scripts for 6.7.7 release

--- Bundlebot/build_smv_manuals.sh
+++ Bundlebot/build_smv_manuals.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-# run smokebot and build manuals for FDS6.7.6/SMV6/7.16 release
 cd ../Smokebot
-./run_smokebot.sh -c -u -J -q batch3 -m gforney@gmail.com -x 536c429eb -X FDS6.7.6 -y 485e0cd19 -Y SMV6.7.16 -R release -M -U -w smokebot/clone
+
+# run smokebot and build manuals for FDS6.7.6/SMV6/7.16 release
+#./run_smokebot.sh -c -u -J -q batch3 -m gforney@gmail.com -x 536c429eb -X FDS6.7.6 -y 485e0cd19 -Y SMV6.7.16 -R release -M -U -w smokebot/clone
+
+# run smokebot and build manuals for FDS6.7.7/SMV6/7.18 release
+./run_smokebot.sh -c -u -J -q batch3 -m gforney@gmail.com -x gfe0d4ef38 -X FDS6.7.7 -y dce043cd7 -Y SMV6.7.18 -R release -M -U -w smokebot/clone
