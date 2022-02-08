025c2d541b045955329c4fa7ed2c42cf7d507d48
gforney <gforney@gmail.com>
2022-02-08 18:40:25 -0500

    scripts: create a script that will make an fds for each commit found in a file

--- Scripts/make_fdss.sh
+++ Scripts/make_fdss.sh
@@ -3,23 +3,30 @@ if [ "$1" == "" ]; then
   echo commit file not specified
   exit
 fi
-TEST_DIR=TEST_DIR
-if [ ! -d TEST_DIR ]; then
-  mkdir TEST_DIR
+if [ ! -d TESTDIR ]; then
+  mkdir TESTDIR
 fi
 CURDIR=`pwd`
-cd TEST_DIR
+
+cd TESTDIR
+TESTDIR=`pwd`
 git clean -dxf
 cd $CURDIR/../../fds
 FDSREPO=`pwd`
 commit_file=$CURDIR/$1
 cat $commit_file
 COMMITS=`cat $commit_file | awk -F';' '{print $1}'`
+count=1
 for commit in $COMMITS; do
 git checkout master
-DIR=$CURDIR/$TEST_DIR/Source_$commit
+COMMITDIR=$TESTDIR/${count}_$commit
+count=$((count+1))
+mkdir $COMMITDIR
 cd $FDSREPO
 git checkout $commit
-cp -r $FDSREPO/Source $DIR
+cp -r $FDSREPO/Source $COMMITDIR/Source
+cp -r $FDSREPO/Build $COMMITDIR/Build
+cd $COMMITDIR/Build/impi_intel_linux_64
+./make_fds.sh >& /dev/null &
 done
 git checkout master
