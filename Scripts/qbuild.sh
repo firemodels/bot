0dee80c1df2b15e17a552fe79ad7c10b6e457852
gforney <gforney@gmail.com>
2022-02-08 19:55:54 -0500

    scripts: updats to fds build script

--- Scripts/qbuild.sh
+++ Scripts/qbuild.sh
@@ -3,19 +3,14 @@
 # ---------------------------- usage ----------------------------------
 
 function usage {
-  if [ "$use_intel_mpi" == "1" ]; then
-    MPI=impi
-  else
-    MPI=mpi
-  fi
   echo "Usage: qbuild.sh [-d dir ][-q queue]"
   echo ""
   echo "qbuild.sh builds FDS"
   echo ""
-  echo " -h   - show ptions"
-  echo " -d dir - build directory"
+  echo " -d dir - directory where fds is built"
+  echo " -h   - show this message"
   echo " -q q - name of queue. [default: batch]"
-  echo "input_file - input file"
+  echo " -v   - show script"
   exit
 }
 
@@ -33,14 +28,15 @@ FDSROOT=~/FDS-SMV
 if [ "$FIREMODELS" != "" ]; then
   FDSROOT=$FIREMODELS
 fi
+showscript=
 
 #*** read in parameters from command line
 
-while getopts 'd:hq:' OPTION
+while getopts 'd:hq:v' OPTION
 do
 case $OPTION  in
   d)
-   dir="$OPTARG"
+   builddir="$OPTARG"
    ;;
   h)
    usage
@@ -49,9 +45,22 @@ case $OPTION  in
   q)
    queue="$OPTARG"
    ;;
+  v)
+   showscript=1
+   ;;
 esac
 done
 shift $(($OPTIND-1))
+
+if [ ! -d $builddir ]; then
+  echo "***error director $builddir does not exist"
+  exit
+fi
+cd $builddir
+fulldir=`pwd`
+
+outerr=$fulldir/fdsbuild.err
+outlog=$fulldir/fdsbuild.log
 
 cat << EOF >> $scriptfile
 #SBATCH -J $JOBPREFIX$infile
@@ -87,72 +96,36 @@ fi
 
 cat << EOF >> $scriptfile
 
-cd $fulldir
 echo
 echo \`date\`
-EOF
-
-cat << EOF >> $scriptfile
 echo "     Directory: \`pwd\`"
 echo "          Host: \`hostname\`"
 echo "----------------" >> $qlog
-echo "started running at \`date\`" >> $qlog
-EOF
-
-cat << EOF >> $scriptfile
-echo "finished running at \`date\`" >> $qlog
+echo "started fds build at \`date\`"  >> $qlog
+cd $fulldir
+./make_fds.sh                         >> $qlog
+echo "finished fds build at \`date\`" >> $qlog
 EOF
 
 #*** output script file to screen if -v option was selected
 
-if [ "$showinput" == "1" ]; then
+if [ "$showscript" == "1" ]; then
   cat $scriptfile
   echo
   exit
 fi
 
 #*** output info to screen
-echo "submitted at `date`"                          > $qlog
-if [ "$queue" != "none" ]; then
-if [ "$OPENMPCASES" == "" ]; then
-  echo "         Input file:$in"             | tee -a $qlog
-if [ "$casedir" != "" ]; then
-  echo "          Input dir:$casedir"             | tee -a $qlog
-fi
-else
-  echo "         Input files:"               | tee -a $qlog
-for i in `seq 1 $OPENMPCASES`; do
-  echo "            ${files[$i]}"            | tee -a $qlog
-done
-fi
-  echo "         Executable:$exe"            | tee -a $qlog
-  if [ "$OPENMPI_PATH" != "" ]; then
-    echo "            OpenMPI:$OPENMPI_PATH" | tee -a $qlog
-  fi
-  if [ "$use_intel_mpi" != "" ]; then
-    echo "           Intel MPI"              | tee -a $qlog
-  fi
-
-#*** output modules used when fds is run
-  if [[ "$MODULES" != "" ]] && [[ "$MODULES_OUT" == "" ]]; then
-    echo "            Modules:$MODULES"                    | tee -a $qlog
-  fi
-  echo "   Resource Manager:$RESOURCE_MANAGER"             | tee -a $qlog
+  echo "submitted at `date`"                                      > $qlog
+  echo "          Input dir:$builddir"                     | tee -a $qlog
   echo "              Queue:$queue"                        | tee -a $qlog
-fi
 
 #*** run script
 
 echo 
 chmod +x $scriptfile
 
-if [ "$queue" != "none" ]; then
-  $QSUB $scriptfile | tee -a $qlog
-else
-  $QSUB $scriptfile
-fi
-if [ "$queue" != "none" ]; then
-  cat $scriptfile > $scriptlog
-  echo "#$QSUB $scriptfile" >> $scriptlog
-  rm $scriptfile
-fi
+$QSUB $scriptfile | tee -a $qlog
+cat $scriptfile            > $scriptlog
+echo "#$QSUB $scriptfile" >> $scriptlog
+rm $scriptfile
