9025d02cd33bdb0e18e372d193fc7dd2a21b6365
gforney <gforney@gmail.com>
2022-02-09 13:24:48 -0500

    scripts: build multiple fdss on one node

--- Scripts/qbuild.sh
+++ Scripts/qbuild.sh
@@ -82,7 +82,7 @@ cat << EOF > $scriptfile
 #SBATCH --partition=$QUEUE
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
-#SBATCH --exclusive
+# #SBATCH --exclusive
 #SBATCH --cpus-per-task=1
 #SBATCH --ntasks-per-node=1
 EOF
