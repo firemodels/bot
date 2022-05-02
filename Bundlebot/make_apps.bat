c8e0e19e00eff1293eb218345df276dcc2148d05
gforney <gforney@gmail.com>
2022-05-02 10:15:41 -0400

    bunde scripts: correcet some more epaths

--- Bundlebot/make_apps.bat
+++ Bundlebot/make_apps.bat
@@ -45,7 +45,7 @@ call setup_intel_compilers.bat 1>> %compile_log% 2>&1
 timeout /t 30 > Nul
 
 :: build fds apps
-call :BUILDUTIL fds2ascii intel_win_64 win_64
+call :BUILDUTIL fds2ascii intel_win
 call :BUILDUTIL test_mpi  impi_intel_win
 call :BUILDFDS
 
@@ -62,7 +62,7 @@ call :BUILD     get_time
 call :BUILDSMV
 
 :: verify fds apps were built
-call :CHECK_BUILDUTIL fds2ascii intel_win_64 _win_64
+call :CHECK_BUILDUTIL fds2ascii intel_win
 call :CHECK_BUILDUTIL test_mpi  impi_intel_win
 call :CHECK_BUILDFDS
 call :CHECK_BUILDFDSOPENMP
@@ -148,11 +148,11 @@ set prog=%1
 set builddir=%2
 set suffix=%3
 
-if NOT exist %fdsrepo%\Utilities\%prog%\%builddir%\%prog%%suffix%.exe goto check_util
+if NOT exist %fdsrepo%\Utilities\%prog%\%builddir%\%prog%_%builddir%.exe goto check_util
 exit /b /0
 :check_util
-echo ***error: The program %prog%%suffix%.exe failed to build
-echo ***error: The program %prog%%suffix%.exe failed to build  1>> %error_log% 2>&1
+echo ***error: The program %prog%_%builddir%.exe failed to build
+echo ***error: The program %prog%_%builddir%.exe failed to build  1>> %error_log% 2>&1
 set error=1
 exit /b /1
 
