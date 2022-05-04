21385bbd1e5a08202f872dad984a28b79eb501bb
Forney <gforney@gmail.com>
2022-05-04 12:36:38 -0400

    bundle scripts: minor edits to test bundle script

--- Bundlebot/run_bundlebot_latest_rev.sh
+++ Bundlebot/run_bundlebot_latest_rev.sh
@@ -2,13 +2,12 @@
 
 CONFIG=$HOME/.bundle/bundle_config.sh
 echo "testing bundle scripts - building test bunde"
-if [ -e $CONFIG ]; then
-  source  $CONFIG
-  if [ "$bundle_hostname" != "" ]; then
-    ./run_bundlebot.sh -f -H $bundle_hostname -L -U
-  else
-    echo "***error: bundle_hostname is not defined"
-  fi
-else
+if [ ! -e $CONFIG ]; then
   echo "***error: $CONFIG does not exist"
 fi
+source  $CONFIG
+if [ "$bundle_hostname" == "" ]; then
+    echo "***error: bundle_hostname is not defined"
+    echo "          deine in $CONFIG"
+fi
+./run_bundlebot.sh -c -f -H $bundle_hostname -L -U
