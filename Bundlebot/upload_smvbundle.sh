b9dd5aeff77253da5ad2474f3f72384ba3835f0e
gforney <gforney@gmail.com>
2022-05-10 14:30:20 -0400

    scripts: modify scripts to upload test smokeviews to google drive (preliminary)

---
+++ Bundlebot/upload_smvbundle.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+FROM_DIR=$1
+FROM_FILE=$2
+PLATFORM=$3
+
+scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd $scriptdir
+
+GDRIVE=~/bin/gdrive
+GOOGLE_ID=`cat $HOME/.bundle/GOOGLE_SMOKEVIEW_ID`
+
+if [ ! -e $GDRIVE ] ; then
+  echo "***error: the program $GDRIVE used to upload files to google drive does not exist"
+  exit
+fi
+
+$GDRIVE list  | grep ^SMV | grep $PLATFORM | awk '{ system("~/bin/gdrive delete -i " $1)} '
+echo ""
+echo "***uploading $FROM_FILE"
+echo ""
+$GDRIVE upload -p $GOOGLE_ID -f $FROM_DIR/$FROM_FILE
+nfiles=`$GDRIVE list  | grep $FROM_FILE | wc -l`
+if [ $nfiles -eq 0 ]; then
+  echo "*** warning: The file $FROM_FILE failed to upload to google drive"
+else
+  echo "$FROM_FILE uploaded."
+fi
