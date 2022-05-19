b42ddda9a782c80b8f57fb76b5ce297dd3a71743
gforney <gforney@gmail.com>
2022-05-18 22:09:22 -0400

    bunde scripts: update linux/osx bundle build script for new release

--- Bundlebot/build_release.sh
+++ Bundlebot/build_release.sh
@@ -7,4 +7,4 @@
 # ./run_bundlebot.sh -f -P $HOME/.bundle/bundle_settings.sh -R release -F fe0d4ef38 -X FDS6.7.7 -S dce043cd7 -Y SMV6.7.18 
 
 # FDS 6.7.8 and smokeview 6.7.19
- ./run_bundlebot.sh -f -P $HOME/.bundle/bundle_settings.sh -R release -F 218333b93 -X FDS6.7.8 -S 00a6dc437 -Y SMV6.7.19 
+ ./run_bundlebot.sh -f -P $HOME/.bundle/bundle_settings.sh -R release -F 8b2548bcf -X FDS6.7.8 -S dba4189c8 -Y SMV6.7.19 
