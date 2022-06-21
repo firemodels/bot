5322bc3827eec7d940864672327a113cfd15cf67
gforney <gforney@gmail.com>
2022-06-21 15:24:12 -0400

    bundle scripts: use parameter file when building bundles

--- Bundlebot/run_bundlebot.sh
+++ Bundlebot/run_bundlebot.sh
@@ -1,12 +1,5 @@
  #!/bin/bash
 
-if [ "`uname`" == "Darwin" ] ; then
-
-#*** OSX parameters
-
-  export FDS_OPENMPIDIR=/opt/openmpi410_oneapi_64
-fi
-
 #---------------------------------------------
 #                   usage
 #---------------------------------------------
@@ -25,7 +18,6 @@ echo "-F - fds repo hash/release"
 echo "-h - display this message"
 echo "-p host -  host containing pubs"
 echo "           firebot/fds pubs: ~firebot/.firebot/pubs"
-echo "-P parameter_file - file containing bundle settings"
 echo "-C - use pubs in $HOME/.bundle/manuals on pub_host"
 echo "-X fdstag - when cloning, tag fds repo with fdstag"
 echo "-Y smvtag - when cloning, tag smv repo with smvtag"
@@ -142,10 +134,9 @@ PUB_HOST=$FIREBOT_HOST
 FDS_TAG=
 SMV_TAG=
 CUSTOM_PUBS=
-PARAMETER_FILE=
 LATEST=
 
-while getopts 'cCfF:hH:Lm:p:P:rR:S:UvX:Y:' OPTION
+while getopts 'cCfF:hH:Lm:p:rR:S:UvX:Y:' OPTION
 do
 case $OPTION  in
   c)
@@ -175,9 +166,6 @@ case $OPTION  in
   p)
    PUB_HOST="$OPTARG"
    ;;
-  P)
-   PARAMETER_FILE="$OPTARG"
-   ;;
   r)
    BRANCH=release
    ;;
@@ -202,14 +190,6 @@ case $OPTION  in
 esac
 done
 shift $(($OPTIND-1))
-
-if [ "$PARAMETER_FILE" != "" ]; then
-  if [ -e $PARAMEER_FILE ]; then
-    source $PARAMETER_FILE
-  else
-    echo "***error: parameter file, $PARAMETER_FILE, does not exist"
-  fi
-fi
 
 # Linux or OSX
 JOPT="-J"
