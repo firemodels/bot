016378cb7078be7758aa9a92fb9bedb22681032b
gforney <gforney@gmail.com>
2022-06-22 14:28:33 -0400

    vv: make test bundle sript more general

--- Bundlebot/Verification/test_bundle.sh
+++ Bundlebot/Verification/test_bundle.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
+QFDS="../../../fds/Utilities/Scripts/qfds.sh -j BUN"
+RUN_FDS()
+{
+P=$1
+O=$2
+INPUT=$3
+if [ "`uname`" == "Darwin" ]; then
+  mpiexec -n $P fds $INPUT
+else
+  $QFDS -i -o $O -p $P $INPUT
+fi
+}
 
-QFDS=../../../fds/Utilities/Scripts/qfds.sh -j BUN
-$QFDS -i -o 1 -p 1  test01a.fds
-$QFDS -i -o 4 -p 1  test01b.fds
-$QFDS -i -o 1 -p 4  test04a.fds
-$QFDS -i -o 2 -p 4  test04b.fds
+RUN_FDS 1 1 test01a.fds
+RUN_FDS 1 4 test01b.fds
+RUN_FDS 4 1 test04a.fds
+RUN_FDS 4 2 test04b.fds
