90ebe31a90e01fca427906fada46f41b4c3fcada
gforney <gforney@gmail.com>
2022-11-19 00:46:08 -0500

    firebot: add hash to get_rev_date script

--- Scripts/get_rev_date.sh
+++ Scripts/get_rev_date.sh
@@ -4,5 +4,5 @@ cd $repo
 HASH=`git rev-parse --short HEAD`
 TIMESTAMP=`git show -s --format=%ct $SHORTHASH`
 REVDATE=`date -d "@$TIMESTAMP" | awk '{print $6$2$3}'`
-echo $REVDATE
+echo ${REVDATE}_$HASH
 
