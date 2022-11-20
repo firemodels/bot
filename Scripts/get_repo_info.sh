48e546622783551cd23073c7fa4166194a50b646
Forney <gforney@gmail.com>
2022-11-19 23:20:03 -0500

    bundle scripts: fix get_repo_info script for OSX

--- Scripts/get_repo_info.sh
+++ Scripts/get_repo_info.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 repopath=$1
 
+if [ "`uname`" == "Darwin" ]; then
+  platform="osx"
+else
+  platform="linux"
+fi
+
 repo=`basename $repopath`
 cd $repopath
 HASH=`git rev-parse --short HEAD`
 TIMESTAMP=`git show -s --format=%ct $SHORTHASH`
-REVDATE=`date -d "@$TIMESTAMP" `
+if [ "$platform" == "linux" ]; then
+  REVDATE=`date -d "@$TIMESTAMP" `
+else
+  REVDATE=`date -r "$TIMESTAMP" `
+fi
 echo $repo repo with hash $HASH commited on ${REVDATE}
 
