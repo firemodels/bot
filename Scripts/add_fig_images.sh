431f3dcabeb82ed4581f00a73a0de4c27ef9d00c
gforney <gforney@gmail.com>
2022-12-28 09:14:11 -0500

    scripts: update script for adding reference fig images

---
+++ Scripts/add_fig_images.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+#---------------------------------------------
+#                   usage
+#---------------------------------------------
+
+function usage {
+echo "add modified fig images to fig repo"
+echo ""
+echo "Options:"
+echo "-h - display this message"
+echo "-b - html base"
+exit 0
+}
+HTMLBASE=clonef
+
+#*** parse options
+
+while getopts 'hb:' OPTION
+do
+case $OPTION  in
+  h)
+   usage;
+   ;;
+  b)
+   HTMLBASE="$OPTARG"
+   ;;
+esac
+done
+shift $(($OPTIND-1))
+
+CURDIR=`pwd`
+TODIR=../../fig/smv/Reference_Figures
+cd $TODIR
+TODIR=`pwd`
+FROMBASE=/var/www/html/`whoami`/$HTMLBASE/diffs/base
+cd $FROMBASE/user
+USERFILES=`ls *.png`
+cd $FROMBASE/verification
+VERFILES=`ls *.png`
+cd $TODIR
+for f in $USERFILES
+do
+git add $f
+done
+for f in $VERFILES
+do
+git add $f
+done
+git checkout *.png
+
