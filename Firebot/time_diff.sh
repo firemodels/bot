ed44a825cb2d02b1d501a2c6efad361cbcc0d732
gforney <gforney@gmailcom>
2023-01-23 22:43:25 -0500

    scripts: minor formatting changes time_diff output

--- Firebot/time_diff.sh
+++ Firebot/time_diff.sh
@@ -6,4 +6,13 @@ HOURS=$(($DIFF / 3600))
 MINUTES=$(($DIFF / 60))
 MINUTES=$(($MINUTES % 60))
 SEC=$(($DIFF % 60))
-echo "${HOURS}H ${MINUTES}M ${SEC}S "
+if [ $HOURS -lt 10 ]; then
+  HOURS="0$HOURS"
+fi
+if [ $MINUTES -lt 10 ]; then
+  MINUTES="0$MINUTES"
+fi
+if [ $SEC -lt 10 ]; then
+  SEC="0$SEC"
+fi
+echo "${HOURS}:${MINUTES}:${SEC}"
