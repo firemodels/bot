40e97168b26cca778fd998b2b43909a816e49b03
gforney <gforney@gmail.com>
2023-02-22 11:30:21 -0500

    cfast bundle: update upload script

--- Bundle/cfast/upload_cfast_bundle.bat
+++ Bundle/cfast/upload_cfast_bundle.bat
@@ -3,10 +3,14 @@ setlocal
 
 set cfast_revision=%1
 set smv_revision=%2
+set cfast_revision=CFAST7.7.4-31
+set smv_revision=SMV6.7.21-1044
+set nightly_arg=tst
+
 
 set BUNDLEDIR=%userprofile%\.bundle\uploads
 set upload_host=blaze.el.nist.gov
-set basename=%cfast_version%_%smv_version_arg%_tst_win
+set basename=%cfast_revision%_%smv_revision%_tst_win
 set fullfile=%BUNDLEDIR%\%basename%.exe
 
 set envfile="%userprofile%"\fds_smv_env.bat
@@ -24,7 +28,6 @@ goto:eof
 call %envfile%
 echo.
 
-
 if NOT EXIST %fullfile% echo ***Error: bundle file %basename%.exe does not exist in %BUNDLEDIR%
 if NOT EXIST %fullfile% exit /b 1
 
@@ -35,9 +38,9 @@ echo ------------------------------------------------------
 echo uploading %bundlefile% to %upload_host%
 echo.
 
-pscp -P 22 %fullfile% %upload_host%:.bundle/bundles/.
+pscp -P 22 %fullfile% %upload_host%:.bundle/uploads/.
 
 :: upload to google drive
-plink %plink_options% %linux_logon% %linux_svn_root%/bot/Bundle/Cfast/upload_bundle.sh $HOME/.bundle/uploads %basename% %nightly_arg% win %GOOGLE_DIR%
+plink %plink_options% %linux_logon% %linux_svn_root%/bot/Bundle/cfast/upload_cfast_bundle.sh .bundle/uploads %basename%
 
 exit /b 0
