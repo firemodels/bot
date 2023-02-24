c900cb24b58ebe22136eb454360422e002d7e330
Glenn Forney <gforney@gmail.com>
2023-02-23 21:53:15 -0500

    cfast bundle: add parameter interface to copy_pubs script

--- Bundle/cfast/copy_pubs_fromrepo.bat
+++ Bundle/cfast/copy_pubs_fromrepo.bat
@@ -1,12 +1,16 @@
 @echo off
 setlocal
 
-set cfastbundledir=%CD%
+set manuals_dir=%userprofile%\FireModels_fork\cfast\Manuals
+set THISDIR=%CD%
+cd ..\..\..\cfast\Manuals
+set local_dir=%CD%
+cd %THISDIR%
 
-cd ..\..\..\cfast
-set cfastrepo=%CD%
-set manuals=%cfastrepo%\Manuals
 set PDFS=%userprofile%\.cfast\PDFS
+
+call :getopts %*
+if x%stopscript% == x1 exit /b
 
 if NOT exist %userprofile%\.cfast mkdir %userprofile%\.cfast
 if NOT exist %PDFS% mkdir %PDFS%
@@ -23,7 +27,7 @@ goto eof
 :copy_file
 :: -------------------------------------------------
 set file=%1
-set fromfile=%manuals%\%file%\%file%.pdf
+set fromfile=%manuals_dir%\%file%\%file%.pdf
 set tofile=%PDFS%\%file%.pdf
 if exist %tofile% erase %tofile%
 if exist %fromfile% copy %fromfile% %tofile%  > Nul 2>&1
@@ -33,5 +37,61 @@ if NOT exist %tofile% echo error***: %file%.pdf failed to copy
 if exist %tofile% echo %file%.pdf copied
 exit /b 1
 
-cd %cfastbundledir%
+cd %THISDIR%
+
+goto eof
+
+::-----------------------------------------------------------------------
+:usage
+::-----------------------------------------------------------------------
+
+:usage
+echo.
+echo copy_pubs_fromrepo usage
+echo.
+echo This script copies manuals from a cfast repo to %userprofile%\.bundle\PDFS
+echo.
+echo Options:
+echo -f  root - copy manuals from %userprofile%\root\cfast\Manuals (default: root=FireModels_fork)
+echo -h  display this message
+echo -l - copy manuals from %local_dir%
+exit /b 0
+
+::-----------------------------------------------------------------------
+:getopts
+::-----------------------------------------------------------------------
+ set stopscript=
+ if (%1)==() exit /b
+ set valid=0
+ set arg=%1
+ if "%1" EQU "-f" (
+   set manuals_dir=%userprofile%\%2\cfast\Manuals
+   shift
+   set valid=1
+ )
+ if "%1" EQU "-l" (
+ 
+   set valid=1
+   cd ..\..\..\cfast\Manuals
+   set manuals_dir=%CD%
+   cd %THISDIR%
+ )
+ if "%1" EQU "-h" (
+   call :usage
+   set stopscript=1
+   exit /b
+ )
+ shift
+ if %valid% == 0 (
+   echo.
+   echo ***Error: the input argument %arg% is invalid
+   echo.
+   echo Usage:
+   call :usage
+   set stopscript=1
+   exit /b 1
+ )
+if not (%1)==() goto getopts
+exit /b 0
+
 :eof
