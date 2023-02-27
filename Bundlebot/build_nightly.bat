6ccf07e68631556e590fd6c9781a41a7b2fad3ec
gforney <gforney@gmail.com>
2023-02-27 14:25:27 -0500

    bundle script: add missing redirect

--- Bundlebot/build_nightly.bat
+++ Bundlebot/build_nightly.bat
@@ -2,7 +2,7 @@
 set CURDIR=%CD%
 
 :: build and upload bundle
-call run_bundlebot -c 
+call run_bundlebot -c >  bundle2.out 2>&1
 
 :: build but don't upload bundle (for testing)
 :: call run_bundlebot -c -U
