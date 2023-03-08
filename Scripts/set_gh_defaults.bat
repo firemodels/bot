ce7064694fd858d791b5a52551659acc4aa198d5
gforney <gforney@gmail.com>
2023-03-07 21:58:41 -0500

    bundle scripts: minor edits to set_gh_defaults script

--- Scripts/set_gh_defaults.bat
+++ Scripts/set_gh_defaults.bat
@@ -3,8 +3,9 @@ setlocal
 
 set repo_root=FireModels_fork
 set CURDIR=%CD%
-set user=%USER%
-set user=firemodels
+set user=%USERNAME%
+::set user=firemodels
+set repo=test_bundles
 
 :: setup environment variables (defining where repository resides etc) 
 
@@ -24,11 +25,18 @@ call %envfile%
 %svn_drive%
 echo.
 
-set scriptdir=%svn_root%\bot\Scripts
+set scriptdir=%linux_svn_root%/bot/Scripts
 
-cd %userprofile%\%repo_root%\test_bundles
-gh repo set-default %user%/test_bundles 
+cd %userprofile%\%repo_root%\%repo%
+echo.
+echo setting gh default: %user% on PC
+gh repo set-default %user%/%repo% 
+gh repo set-default --view 
 
-plink %plink_options% %linux_logon% %scriptdir%/set_gh_defaults.sh %repo_root%/test_bundles %user%/test_bundles
+echo.
+echo setting gh default: %user% on %linux_logon%
+plink %plink_options% %linux_logon% %scriptdir%/set_gh_defaults.sh %repo_root% %repo% %user%
 
-plink %plink_options% %osx_logon%   %scriptdir%/set_gh_defaults.sh %repo_root%/test_bundles %user%/test_bundles
+::echo.
+echo setting gh default: %user% on %osx_logon%
+plink %plink_options% %osx_logon% %scriptdir%/set_gh_defaults.sh %repo_root% %repo% %user%
