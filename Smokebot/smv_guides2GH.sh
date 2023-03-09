686a19f4eb0e8579db39710f19ec67acf540e228
gforney <gforney@gmail.com>
2023-03-08 23:21:35 -0500

    smv vv: update smokeview pubs/figures upload to GH script

---
+++ Smokebot/smv_guides2GH.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+CURDIR=`pwd`
+
+FROMDIR=~smokebot/.smokebot/pubs
+
+cd ../../smv/Manuals
+MANDIR=`pwd`
+cd $CURDIR
+
+cd ../../test_bundles
+TESTBUNDLEDIR=`pwd`
+cd $CURDIR
+
+UPLOADHASH ()
+{
+  DIR=$HOME/.smokebot/appslatest
+  FILE=$1
+  if [ -e $DIR/$FILE ]; then
+    cd $TESTBUNDLEDIR
+    echo ***Uploading $FILE
+    gh release upload TEST $DIR/$FILE --clobber
+  fi
+}
+
+UPLOADGUIDE ()
+{
+  FILE=$1
+  FILEnew=${FILE}.pdf
+  if [ -e $FROMDIR/$FILEnew ]; then
+    cd $TESTBUNDLEDIR
+    echo ***Uploading $FILEnew
+    gh release upload TEST $FROMDIR/$FILEnew --clobber
+  fi
+}
+UPLOADFIGURES ()
+{
+  DIRECTORY=$1
+  FILE=$2
+  TARHOME=$HOME/.smokebot/pubs
+  if [ ! -e $HOME/.smokebot ]; then
+    mkdir $HOME/.smokebot
+  fi
+  if [ ! -e $HOME/.smokebot/pubs ]; then
+    mkdir $HOME/.smokebot/pubs
+  fi
+  cd $MANDIR/$DIRECTORY/SCRIPT_FIGURES
+  tarfile=${FILE}_figures.tar
+  rm -f $TARHOME/$tarfile
+  rm -f $TARHOME/$tarfile.gz
+  tar cvf $TARHOME/$tarfile . &> /dev/null
+  cd $TARHOME
+  gzip $tarfile
+  cd $TESTBUNDLEDIR
+  echo ***Uploading $tarfile.gz
+  gh release upload TEST $TARHOME/$tarfile.gz --clobber
+}
+
+if [ -e $TESTBUNDLEDIR ] ; then
+  UPLOADGUIDE SMV_Technical_Reference_Guide
+  UPLOADGUIDE SMV_User_Guide
+  UPLOADGUIDE SMV_Verification_Guide
+  UPLOADFIGURES SMV_User_Guide SMV_UG
+  UPLOADFIGURES SMV_Verification_Guide SMV_VG
+  cd $CURDIR
+fi
