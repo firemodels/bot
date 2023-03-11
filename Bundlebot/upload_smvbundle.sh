6e8505329a7dfb910d92045e8236166e5e60524f
gforney <gforney@gmail.com>
2023-03-11 08:43:09 -0500

    bundle scripts: corrections to gh smoekview upload scripts

--- Bundlebot/upload_smvbundle.sh
+++ Bundlebot/upload_smvbundle.sh
@@ -3,6 +3,8 @@ FROM_DIR=$1
 FROM_FILE=$2
 RELEASEBUNDLEDIR=$3
 RELEASEBRANCH=$4
+GH_OWNER_ARG=$5
+GH_REPO_ARG=$6
 
 if [ ! -e $HOME/$FROM_DIR/$FROM_FILE ] ; then
   echo "***error: $FROM_FILE does not exist in $HOME/$FROM_DIR"
@@ -12,6 +14,6 @@ fi
 if [ "$RELEASEBUNDLEDIR" != "" ]; then
   cd $HOME/$RELEASEBUNDLEDIR
   echo uploading $FROM_FILE to github
-  gh release upload $RELEASEBRANCH $HOME/$FROM_DIR/$FROM_FILE  --clobber
+  gh release upload $RELEASEBRANCH $HOME/$FROM_DIR/$FROM_FILE  -R github.com/$GH_OWNER_ARG/$GH_REPO_ARG --clobber
 fi
 
