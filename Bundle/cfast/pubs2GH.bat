f87dbd4f931520589ae9549f2291153ce2de09e9
gforney <gforney@gmail.com>
2023-03-12 15:23:23 -0400

    bundle scripts: corrections to cfast guides to GH script

--- Bundle/cfast/pubs2GH.bat
+++ Bundle/cfast/pubs2GH.bat
@@ -15,11 +15,15 @@ if NOT exist %userprofile%\.cfast mkdir %userprofile%\.cfast
 if NOT exist %PDFS% mkdir %PDFS%
 
 cd %testbundlerepo%
-call :copy_file CFAST_Tech_Ref
-call :copy_file CFAST_Users_Guide
-call :copy_file CFAST_Validation_Guide
-call :copy_file CFAST_Configuration_Guide
-call :copy_file CFAST_CData_Guide
+call :copy_file CFAST_Tech_Ref.pdf
+call :copy_file CFAST_Users_Guide.pdf
+call :copy_file CFAST_Validation_Guide.pdf
+call :copy_file CFAST_Configuration_Guide.pdf
+call :copy_file CFAST_CData_Guide.pdf
+call :copy_file CFAST_HASH
+call :copy_file CFAST_REVISION
+call :copy_file SMV_HASH
+call :copy_file SMV_REVISION
 
 goto eof
 
@@ -27,11 +31,9 @@ goto eof
 :copy_file
 :: -------------------------------------------------
 set file=%1
-set fromfile=%PDFS%\%file%.pdf
-set cfastfile=%file%.pdf
-if exist %fromfile% copy %fromfile% %TEMP%\%cfastfile% 
-if exist %fromfile% echo Uploading %cfastfile% 
-if exist %fromfile% gh release upload %GH_CFAST_TAG% %TEMP%\%cfastfile% --clobber 
+set fromfile=%PDFS%\%file%
+if exist %fromfile% echo Uploading %fromfile% 
+if exist %fromfile% gh release upload %GH_CFAST_TAG% %fromfile% -R github.com/%GH_OWNER%/%GH_REPO% --clobber 
 if not exist %fromfile% echo ***error: %fromfile% does not exist
 if not exist %fromfile% exit /b 
 exit /b 1
