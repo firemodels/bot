5e76395d608247d0f457e2f2c8156fc76c74657c
gforney <gforney@gmail.com>
2023-03-14 15:50:29 -0400

    bundle scripts: use git describe in release titles

--- Bundlebot/setreleasetitle.bat
+++ Bundlebot/setreleasetitle.bat
@@ -8,19 +8,16 @@ set PREFIX=
 
 set ERROR=1
 if NOT "x%repo%" == "xfds" goto endif1
-  set PREFIX=FDS test
   set tag=FDS_TEST
   set ERROR=
 :endif1
 
 if NOT "x%repo%" == "xsmv" goto endif2
-  set PREFIX=Smokeview test
   set tag=SMOKEVIEW_TEST
   set ERROR=
 :endif2
 
 if NOT "x%repo%" == "xcfast" goto endif3
-  set PREFIX=CFAST test
   set tag=CFAST_TEST
   set ERROR=
 :endif3
@@ -34,10 +31,13 @@ git rev-parse --short HEAD > %tempfile%
 set /p hash=<%tempfile%
 erase %tempfile%
 
+git describe --dirty --long  > %tempfile%
+set /p TITLE=<%tempfile%
+erase %tempfile%
+
 git show -s --format=%%cd --date=format:"%%Y-%%b-%%d %%H:%%M" %hash% > %tempfile%
 set /p INFO=<%tempfile%
 erase %tempfile%
 
-set TITLE=%PREFIX%
-if NOT "x%INFO%" == "x" set TITLE=%TITLE% %hash% %INFO%
+if NOT "x%INFO%" == "x" set TITLE=%TITLE% %INFO%
 gh release edit %tag% -t "%TITLE%" -R github.com/%GH_OWNER%/%GH_REPO%
