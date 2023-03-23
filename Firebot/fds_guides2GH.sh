85cd152391f55c1e20040b60c9a280f7706a86e4
gforney <gforney@gmail.com>
2023-03-22 22:36:35 -0400

    firebot: upload FDS_INFO file to github

--- Firebot/fds_guides2GH.sh
+++ Firebot/fds_guides2GH.sh
@@ -14,19 +14,16 @@ cd ../../fds/Manuals
 MANDIR=`pwd`
 cd $CURDIR
 
-UPLOADHASH ()
+UPLOADINFO ()
 {
   DIR=$HOME/.firebot/appslatest
-  FILE=$1
-  if [ -e $DIR/$FILE ]; then
-    echo ***Uploading $FILE
-    gh release upload $GH_FDS_TAG $DIR/$FILE -R github.com/$GH_OWNER/$GH_REPO --clobber
-    suffix=`head -1 $DIR/$FILE`
-    FILE2=${FILE}_MANUALS_$suffix
-    cp $DIR/$FILE $DIR/$FILE2
-    gh release upload $GH_FDS_TAG $DIR/$FILE2 -R github.com/$GH_OWNER/$GH_REPO --clobber
-    rm -f $DIR/$FILE2
-  fi
+  echo ***Uploading FDS_INFO
+  echo "FDS_HASH     $DIR/FDS_HASH"      > $DIR/FDS_INFO
+  echo "FDS_REVISION $DIR/FDS_REVISION" >> $DIR/FDS_INFO
+  echo "SMV_HASH     $DIR/SMV_HASH"     >> $DIR/FDS_INFO
+  echo "SMV_REVISION $DIR/SMV_REVISION" >> $DIR/FDS_INFO
+  gh release upload $GH_FDS_TAG $DIR/FDS_INFO -R github.com/$GH_OWNER/$GH_REPO --clobber
+  rm -f $DIR/FDS_INFO
 }
 
 UPLOADGUIDE ()
@@ -85,9 +82,6 @@ UPLOADFIGURES FDS_Technical_Reference_Guide FDS_TG
 UPLOADFIGURES FDS_User_Guide FDS_UG
 UPLOADFIGURES FDS_Validation_Guide FDS_VALG
 UPLOADFIGURES FDS_Verification_Guide FDS_VERG
-UPLOADHASH FDS_HASH
-UPLOADHASH FDS_REVISION
-UPLOADHASH SMV_HASH
-UPLOADHASH SMV_REVISION
+UPLOADINFO
 cd $CURDIR
 exit 0
