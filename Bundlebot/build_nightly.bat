2a5c3058c03cf2e4969e588efcac639eaf9bf1ba
gforney <gforney@gmail.com>
2023-03-25 08:38:40 -0400

    bundle scripts: add -D option for development (do not build apps, do not upload bundle), put docs in Guides directory

--- Bundlebot/build_nightly.bat
+++ Bundlebot/build_nightly.bat
@@ -1,9 +1,10 @@
 @echo off
 set CURDIR=%CD%
+set develop=%1
 
 :: build and upload bundle
 ::call run_bundlebot -c >  bundle2.out 2>&1
-call run_bundlebot -c
+call run_bundlebot -c %develop%
 
 :: build but don't upload bundle (for testing)
 :: call run_bundlebot -c -U
