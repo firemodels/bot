4b1bed921a56565030ed4e6c9d76e1f2840fcb22
gforney <gforney@gmail.com>
2023-04-16 08:51:30 -0400

    scripts: set title in upload release script

--- Bundlebot/upload_release.sh
+++ Bundlebot/upload_release.sh
@@ -2,9 +2,12 @@
 TO_OWNER=gforney
 TO_REPO=test7_bundles
 TO_TAG=COPY_TEST
+TO_TITLE="FDS/Smokeview release title"
 
 echo upload release
 echo OWNER=$TO_OWNER
 echo REPO=$TO_REPO
 echo TAG=$TO_TAG
+echo TITLE=$TO_TITLE
+gh release edit $TO_TAG -t "$TO_TITLE" -R github.com/$TO_OWNER/$TO_REPO
 gh release upload $TO_TAG FILES/* -R github.com/$TO_OWNER/$TO_REPO --clobber
