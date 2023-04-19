bbe7e8c89975aca0f5f9c3cd69a8c9c4854fdb2b
gforney <gforney@gmail.com>
2023-04-18 21:11:32 -0400

    add script to upload files to a release

--- Bundlebot/upload_release.sh
+++ Bundlebot/upload_release.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
+#TO_OWNER=firemodels
+#TO_REPO=fds
+#TO_TAG=FDS-6.8.0
 TO_OWNER=gforney
 TO_REPO=test7_bundles
 TO_TAG=COPY_TEST
-TO_TITLE="FDS/Smokeview release title"
 
-echo upload release
+echo uploading release
 echo OWNER=$TO_OWNER
 echo REPO=$TO_REPO
 echo TAG=$TO_TAG
-echo TITLE=$TO_TITLE
-gh release edit $TO_TAG -t "$TO_TITLE" -R github.com/$TO_OWNER/$TO_REPO
-gh release upload $TO_TAG FILES/* -R github.com/$TO_OWNER/$TO_REPO --clobber
+echo "------------------"
+for file in FILES/*; do
+  echo uploading $file
+  gh release upload $TO_TAG -R github.com/$TO_OWNER/$TO_REPO $file --clobber
+done
