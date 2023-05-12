6a17d07ec94cb1ddfc016a0102b7783858c9e4cf
gforney <gforney@gmail.com>
2023-05-12 19:54:53 -0400

    bundle scripts: don't update repo if -h option is invoked

--- Bundlebot/smv/scripts/run_smvbundle.sh
+++ Bundlebot/smv/scripts/run_smvbundle.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+opt=$1
 
-echo "*** updating bot repo"
-git clean -dxf          >& /dev/null
-git remote update       >& /dev/null
-git merge origin/master >& /dev/null
+if [ "$opt" != "-h" ]; then
+  echo "*** updating bot repo"
+  git clean -dxf          >& /dev/null
+  git remote update       >& /dev/null
+  git merge origin/master >& /dev/null
+fi
 ./smvbundle.sh $*
 
