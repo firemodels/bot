1269ee27db8fbd689e8ecf1c3dde23cd9a9628d0
gforney <gforney@gmail.com>
2023-07-27 10:17:31 -0400

    don't send an email if BUNDLE_EMAIL variable does not exist

--- Bundlebot/scripts/check_install.sh
+++ Bundlebot/scripts/check_install.sh
@@ -4,5 +4,9 @@ INSTALLDIR=$1
 CURRENTDATE=`date +"%b %d, %Y"`
 FDSDATE=`$INSTALLDIR/fds |& grep Compilation | awk -F':' '{print $2}' | awk '{print $1,$2,$3}'`
 if [ "$CURRENTDATE" != "$FDSDATE" ]; then
-  echo "FDS compilation date: $FDSDATE, Current date: $CURRENTDATE ." | mail -s "Warning: nightly fds bundle not installed today" $BUNDLE_EMAIL
+  if [ "$BUNDLE_EMAIL" != "" ]; then
+    echo "FDS compilation date: $FDSDATE, Current date: $CURRENTDATE ." | mail -s "Warning: nightly fds bundle not installed today" $BUNDLE_EMAIL
+  else
+    echo "***Warning: The environment variable BUNDLE_EMAIL is not defined"
+  fi
 fi
