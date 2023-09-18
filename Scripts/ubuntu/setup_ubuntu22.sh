8d02c40edc5c8521711c09b05e840c8c31d9a164
gforney <gforney@fire.myguest.virtualbox.org>
2023-09-18 12:25:01 -0400

    scripts: update ubuntu setup script

--- Scripts/ubuntu/setup_ubuntu22.sh
+++ Scripts/ubuntu/setup_ubuntu22.sh
@@ -4,5 +4,5 @@ sudo apt -y install gfortran
 sudo apt -y install freeglut3-dev mesa-utils
 sudo apt -y install libx11-dev libxmu-dev libxi-dev
 sudo apt -y install openmpi-bin openmpi-doc libopenmpi-dev 
-sudo apt -y install texlive texlive-extras texlive-science
+sudo apt -y install texlive texlive-latex-recommended texlive-latex-extra texlive-science
 
