4fd83b8c8f23f8a6f4a8b459e086994943b6f6f4
gforney <gforney@flame.myguest.virtualbox.org>
2023-09-23 14:19:34 -0400

    scripts: update ubuntu22 setup script

--- Scripts/ubuntu/setup_ubuntu22.sh
+++ Scripts/ubuntu/setup_ubuntu22.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 sudo apt -y update
-sudo apt -y install gfortran
-sudo apt -y install freeglut3-dev mesa-utils
-sudo apt -y install libx11-dev libxmu-dev libxi-dev
-sudo apt -y install openmpi-bin openmpi-doc libopenmpi-dev 
+
+# install packages for building fds
+sudo apt -y install gfortran openmpi-bin openmpi-doc libopenmpi-dev 
+
+# install packages for building smokeview
+sudo apt -y install freeglut3-dev mesa-utils libx11-dev libxmu-dev libxi-dev
+sudo apt -y install clang-format clang-tidy
+
+# install packages for building manuals
 sudo apt -y install texlive texlive-latex-recommended texlive-latex-extra texlive-science
+
+# install package for comparing images
+sudo apt -y install imagemagick-6.q16
+
+echo "see following url for installing latest gh (command line version of git)
+echo https://github.com/cli/cli/blob/trunk/docs/install_linux.md 
+
+
 
