fb73d9a489e8ba9e3be2e9899daf6c2c4ca9a93a
gforney <gforney@gmail.com>
2023-09-23 20:39:07 -0400

    scripts: add entry for oneapi compilers to ubuntu setup script

--- Scripts/ubuntu/setup_ubuntu22.sh
+++ Scripts/ubuntu/setup_ubuntu22.sh
@@ -17,5 +17,13 @@ sudo apt -y install imagemagick-6.q16
 echo "see following url for installing latest gh (command line version of git)
 echo https://github.com/cli/cli/blob/trunk/docs/install_linux.md 
 
+# download and setup oneapi compilers
+
+#wget https://registrationcenter-download.intel.com/akdlm/IRC_NAS/992857b9-624c-45de-9701-f6445d845359/l_BaseKit_p_2023.2.0.49397_offline.sh
+#sudo sh ./l_BaseKit_p_2023.2.0.49397_offline.sh
+
+#wget https://registrationcenter-download.intel.com/akdlm/IRC_NAS/0722521a-34b5-4c41-af3f-d5d14e88248d/l_HPCKit_p_2023.2.0.49440_offline.sh
+#sudo sh ./l_HPCKit_p_2023.2.0.49440_offline.sh
+
 
 
