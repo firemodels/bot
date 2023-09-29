6161f231c7a363f8445322e1d71f12ac908f0994
gforney <gforney@gmail.com>
2023-09-29 08:12:01 -0400

    smokebot: add timing code

--- Smokebot/run_full_local.sh
+++ Smokebot/run_full_local.sh
@@ -4,5 +4,5 @@ OPTION=$1
 # -c clean repos
 # -b use existing branches
 # -J use intel compilers
-#./run_smokebot.sh -s ../../smokebot_cache -b -J $OPTION -q none
-./run_smokebot.sh -c -b -J $OPTION -q none
+time ./run_smokebot.sh -s ../../smokebot_cache -b -J $OPTION -q none
+# time ./run_smokebot.sh -c -b -J $OPTION -q none
