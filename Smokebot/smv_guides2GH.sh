e9330d6eb0c2db911186e091de26eb201e20c893
gforney <gforney@gmail.com>
2023-09-29 23:42:34 -0400

    smokebot: upload SMV_Summary directory to github

--- Smokebot/smv_guides2GH.sh
+++ Smokebot/smv_guides2GH.sh
@@ -53,6 +53,27 @@ UPLOADFIGURES ()
   echo ***Uploading $tarfile.gz
   gh release upload $GH_SMOKEVIEW_TAG $TARHOME/$tarfile.gz -R github.com/$GH_OWNER/$GH_REPO --clobber
 }
+UPLOADSUMMARY ()
+{
+  DIRECTORY=SMV_Summary
+  TARHOME=$HOME/.smokebot/pubs
+  if [ ! -e $HOME/.smokebot ]; then
+    mkdir $HOME/.smokebot
+  fi
+  if [ ! -e $HOME/.smokebot/pubs ]; then
+    mkdir $HOME/.smokebot/pubs
+  fi
+  cd $MANDIR
+  tarfile=SMV_Summary.tar
+  rm -f $TARHOME/$tarfile
+  rm -f $TARHOME/$tarfile.gz
+  tar cvf $tarfile SMV_Summary  &> /dev/null
+  cd $TARHOME
+  gzip $tarfile
+  cd $TESTBUNDLEDIR
+  echo ***Uploading $tarfile.gz
+  gh release upload $GH_SMOKEVIEW_TAG $TARHOME/$tarfile.gz -R github.com/$GH_OWNER/$GH_REPO --clobber
+}
 
 if [ -e $TESTBUNDLEDIR ] ; then
   UPLOADGUIDE SMV_Technical_Reference_Guide
@@ -61,5 +82,6 @@ if [ -e $TESTBUNDLEDIR ] ; then
   UPLOADFIGURES SMV_User_Guide SMV_UG
   UPLOADFIGURES SMV_Verification_Guide SMV_VG
   UPLOADINFO
+  UPLOADSUMMARY
   cd $CURDIR
 fi
