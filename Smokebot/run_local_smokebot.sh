c6b4b49fcdaef487d7bc98b0c8d1ff8f5a4b540c
gforney <gforney@gmail.com>
2023-10-04 16:23:31 -0400

    smokebot: add a comment to smokebot run script

--- Smokebot/run_local_smokebot.sh
+++ Smokebot/run_local_smokebot.sh
@@ -4,6 +4,7 @@ OPTION=$1
 # -c clean repos
 # -b use existing branches
 # -J use intel compilers
+# -s cache_dir use results previously computed
 
 time ./run_smokebot.sh -s ../../smokebot_cache -c -b -J $OPTION -q none
 # time ./run_smokebot.sh -c -b -J $OPTION -q none
