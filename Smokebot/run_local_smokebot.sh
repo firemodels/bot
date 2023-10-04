528d3b74e2e8b7521f97368d2262c0dac76ade9b
gforney <gforney@gmail.com>
2023-10-03 23:24:48 -0400

    smokebot: add clean option when running local smokebot

--- Smokebot/run_local_smokebot.sh
+++ Smokebot/run_local_smokebot.sh
@@ -4,5 +4,6 @@ OPTION=$1
 # -c clean repos
 # -b use existing branches
 # -J use intel compilers
-time ./run_smokebot.sh -s ../../smokebot_cache -b -J $OPTION -q none
+
+time ./run_smokebot.sh -s ../../smokebot_cache -c -b -J $OPTION -q none
 # time ./run_smokebot.sh -c -b -J $OPTION -q none
