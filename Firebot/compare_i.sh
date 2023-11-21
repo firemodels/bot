32201458e5030918a7b1a0270c36effa1982aa36
gforney <gforney@gmail.com>
2023-11-20 22:06:50 -0500

    scripts: update compare_i.sh

--- Firebot/compare_i.sh
+++ Firebot/compare_i.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 WEB_DIR_BASE=$1
 WEB_DIR=
-if [WEB_DIR_BASE!="" ]; then
+if [ "$WEB_DIR_BASE" != "" ]; then
   WEB_DIR=/var/www/html/$WEB_DIR_BASE
 fi
 
@@ -15,6 +15,9 @@ botrepo=$repo/bot
 FDS_SUMMARY_DIR=$fdsrepo/Manuals/FDS_Summary
 OUTPUT_DIR="$firebotdir/output"
 
+cp $fdsrepo/Manuals/FDS_User_Guide/SCRIPT_FIGURES/*.png         $FDS_SUMMARY_DIR/images/user/.
+cp $fdsrepo/Manuals/FDS_Verification_Guide/SCRIPT_FIGURES/*.png $FDS_SUMMARY_DIR/images/verification/.
+
 CURDIR=`pwd`
 cd $botrepo/Firebot
 ./compare_images.sh $FDS_SUMMARY_DIR/images $FDS_SUMMARY_DIR/diffs/images $OUTPUT_DIR/error_images
@@ -23,9 +26,9 @@ if [ "$WEB_DIR" != "" ]; then
   if [ -d $WEB_DIR ]; then
     CUR_DIR=`pwd`
     cd $WEB_DIR
-    rm -r images manuals diffs *.html
+    rm -rf images manuals diffs *.html
     cp -r $FDS_SUMMARY_DIR/* .
-    rm *template.html
+    rm -f *template.html
     cd $CUR_DIR
     UPDATED_WEB_IMAGES=1
   fi
