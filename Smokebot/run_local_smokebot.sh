cc6c42c4347337cbf263e423cebb4f70a0f07a54
gforney <gforney@gmail.com>
2023-12-05 21:20:36 -0500

    smokebot: add -S option to run_local_smokebot script

--- Smokebot/run_local_smokebot.sh
+++ Smokebot/run_local_smokebot.sh
@@ -5,6 +5,7 @@ OPTION=$1
 # -b use existing branches
 # -J use intel compilers
 # -s cache_dir use results previously computed
+# -S build smokeview with the Intel Sanitize option
 
-time ./run_smokebot.sh -s ../../smokebot_cache -c -b -J $OPTION -q none
+time ./run_smokebot.sh -S -s ../../smokebot_cache -c -b -J $OPTION -q none
 # time ./run_smokebot.sh -c -b -J $OPTION -q none
