00008b5d9eb8b3dc9bf32a16ca12f0b0cb66b979
gforney <gforney@gmail.com>
2024-02-14 12:46:21 -0500

    bundle scripts: add scripts for building manuals and bundles (preliminary)

---
+++ Bundlebot/scripts/BUILD_osx_release.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./run_bundlebot.sh -f -c -R release -F 886e00965 -X FDS-6.8.0 -S d02fd6867 -Y SMV-6.8.0 -o firemodels -r fds
---
+++ Bundlebot/scripts/BUILD_smv_manuals.sh
@@ -0,0 +1,1 @@
+./run_smokebot.sh -c -u -J -q smokebot -m gforney@gmail.com  -x b81287c1e -X FDS6.7.9 -y 33420d46b -Y SMV6.7.21 -R release -M -U -w smokebot/clone
