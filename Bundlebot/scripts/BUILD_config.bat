cfc1163cb20f290eca65d735aebf97a3042e10fa
gforney <gforney@gmail.com>
2024-02-16 22:20:47 -0500

    bundle scripts: update windows bundle scripts

--- Bundlebot/scripts/BUILD_config.bat
+++ Bundlebot/scripts/BUILD_config.bat
@@ -3,6 +3,6 @@
 :: edit fds and smv revisions and tags
 :: for testing set to firebot pass on 2/9/2024
 set BUNDLE_FDS_REVISION=c1b5f1a
-set BUNDLE_FDS_TAG=FDS-6.9.0test
+set BUNDLE_FDS_TAG=FDS-6.9.0tst
 set BUNDLE_SMV_REVISION=b837eeb
-set BUNDLE_SMV_TAG=SMV-6.9.0test
+set BUNDLE_SMV_TAG=SMV-6.9.0tst
--- Bundlebot/scripts/BUILD_test_bundle.bat
+++ Bundlebot/scripts/BUILD_test_bundle.bat
@@ -1,5 +1,5 @@
 @echo off
-set GH_REPO=test7_bundles
-set GH_OWNER=gforney
+set GH_REPO=test_bundles
+set GH_OWNER=firemodels
 
 call BUILD_release_bundle
