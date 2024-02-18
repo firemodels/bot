3064c08096bbe81188feeb779f313107a49bdd67
gforney <gforney@gmail.com>
2024-02-17 21:36:31 -0500

    revert previous commit

--- Bundlebot/scripts/BUILD_nightly.bat
+++ Bundlebot/scripts/BUILD_nightly.bat
@@ -4,7 +4,7 @@ set develop=%1
 
 :: build and upload bundle
 ::call run_bundlebot -c >  bundle2.out 2>&1
-call run_bundlebot -c %develop% -G nightly_bundles
+call run_bundlebot -c %develop%
 
 :: build but don't upload bundle (for testing)
 :: call run_bundlebot -c -U
