70163e476f7b26dad53ef0eefdaf7342801fbb44
gforney <gforney@gmail.com>
2024-02-21 16:46:32 -0500

    bundle scripts: configure for 9/21/2024 firebot pass

--- Bundlebot/build/BUILD_config.bat
+++ Bundlebot/build/BUILD_config.bat
@@ -3,24 +3,21 @@
 :: It is run by the other BUILD scripts.
 :: You do not need to run it.
 
-:: firebot pass 2/17/2024
-:: FDS-6.8.0-1523-g29bcb71/master
-:: SMV-6.8.0-2073-g0f8b692/master
+:: firebot pass 2/21/2024
+:: FDS-6.8.0-1538-g3abe090/master
+:: SMV-6.8.0-2075-g38047e9/master
 
 set BUNDLE_OPTION=test
 ::set BUNDLE_OPTION=release
-set BUNDLE_FDS_REVISION=debb46d
+
+set BUNDLE_FDS_REVISION=3abe090
 set BUNDLE_FDS_TAG=FDS-6.9.0
-set BUNDLE_SMV_REVISION=0f8b692
+set BUNDLE_SMV_REVISION=38047e9
 set BUNDLE_SMV_TAG=SMV-6.9.0
 
 :: lines below should not need to be changed
 
-set valid=
-if "%BUNDLE_OPTION%" == "nightly" set valid=1
-if "%BUNDLE_OPTION%" == "test" set valid=1
-if "%BUNDLE_OPTION%" == "release" set valid=1
-if "%BUNDLE_OPTION%" == "" set option=test
-if "%BUNDLE_OPTION%" == "nightly" set GH_REPO=nightly_bundles
+if NOT "%BUNDLE_OPTION%" == "release" set BUNDLE_OPTION=test
 if "%BUNDLE_OPTION%" == "test" set GH_REPO=test_bundles
+if "%BUNDLE_OPTION%" == "test" set GH_FDS_TAG=BUNDLE_TEST
 if "%BUNDLE_OPTION%" == "release" set GH_REPO=fds
--- Bundlebot/build/BUILD_config.sh
+++ Bundlebot/build/BUILD_config.sh
@@ -3,34 +3,29 @@
 # It is run by the other BUILD scripts. 
 # You do not need to run it.
 
-# firebot pass 2/17/2024
-# FDS-6.8.0-1523-g29bcb71/master
-# SMV-6.8.0-2073-g0f8b692/master
+# firebot pass 2/21/2024
+# FDS-6.8.0-1538-g3abe090/master
+# SMV-6.8.0-2075-g38047e9/master
 
 export BUNDLE_OPTION=test
 #export BUNDLE_OPTION=release
-export BUNDLE_FDS_REVISION=debb46d
-export BUNDLE_FDS_TAG=FDS-6.9.0
-export BUNDLE_SMV_REVISION=0f8b692
-export BUNDLE_SMV_TAG=SMV-6.9.0
 
+export BUNDLE_FDS_REVISION=3abe090
+export BUNDLE_FDS_TAG=FDS-6.9.0
+export BUNDLE_SMV_REVISION=38047e9
+export BUNDLE_SMV_TAG=SMV-6.9.0
 
 # lines below should not need to be changed
 
-if [ "BUNDLE_OPTION" == "nightly" ]; then
-  GH_REPO=nightly_bundles
-  valid=1
+if [ "BUNDLE_OPTION" != "release" ]; then
+  BUNDLE_OPTION=test
 fi
 if [ "BUNDLE_OPTION" == "test" ]; then
   GH_REPO=test_bundles
-  valid=1
+  GH_FDS_TAG=BUNDLE_TEST
 fi
 if [ "BUNDLE_OPTION" == "release" ]; then
   GH_REPO=fds
-  valid=1
-fi
-if [ "$valid" == "" ]; then
-  GH_REPO=test_bundles
-  option=test
 fi
 export GH_REPO
+export GH_FDS_TAG
