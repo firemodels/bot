9127d4431540d771f29b0c0f7d37ce76ea774aa7
gforney <gforney@gmailcom>
2024-02-21 20:15:12 -0500

    script: update line ending check script

--- Firebot/line_check.sh
+++ Firebot/line_check.sh
@@ -16,38 +16,14 @@ find_CRLF()
   grep -IURl --exclude="*.pdf" --exclude-dir=".git"  --exclude-dir="output" $'\r'  > $crlf_temp
   nlines=`cat $crlf_temp | wc -l`
   if [ $nlines -gt 0 ]; then
-    echo ""                                                 >> $CRLF_WARNINGS
-    echo "$reponame repo text files with dos line endings:" >> $CRLF_WARNINGS
-    cat $crlf_temp                                          >> $CRLF_WARNINGS
+    echo "$reponame files with CRLF line endings: "
+    cat $crlf_temp
     rm $crlf_temp
+  else
+    echo $reponame: none
   fi
+
   cd $curdir
-}
-
-#---------------------------------------------
-#                   check_CRLF
-#---------------------------------------------
-
-check_CRLF()
-{
-
-  WARNINGS=
-  if [ -e $CRLF_WARNINGS ]; then
-    nwarnings=`cat $CRLF_WARNINGS | wc -l`
-    if [ $nwarnings -gt 0 ]; then
-      WARNINGS=1
-      echo ""
-      echo "Warning: files with DOS line endings were found"
-      echo "see $WARNING_LOG"
-      echo "Warning: files with DOS line endings were found" >> $WARNING_LOG
-      cat $CRLF_WARNINGS                                   >> $WARNING_LOG
-      echo ""                                              >> $WARNING_LOG
-      echo ""
-    fi
-  fi
-  if [ "$WARNINGS" == "" ]; then
-    echo "*** no files with DOS line endings were found"
-  fi
 }
 
 WARNING_LOG=warnings
@@ -60,19 +36,8 @@ cd $CURDIR
 rm -f $CRLF_WARNINGS
 rm -f $WARNING_LOG
 echo Checking for DOS line endings
-echo "   bot repo"
 find_CRLF $repo/bot bot
-
-echo "   exp repo"
 find_CRLF $repo/exp exp
-
-echo "   fds repo"
 find_CRLF $repo/fds fds
-
-echo "   out repo"
 find_CRLF $repo/out out
-
-echo "   smv repo"
 find_CRLF $repo/smv smv
-
-check_CRLF
