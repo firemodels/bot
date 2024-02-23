7dac8677b07210ac9cd10bcd68c2abf912ebcd2b
gforney <gforney@gmail.com>
2024-02-23 18:22:04 -0500

    bundle scripts: only upload to bundle test site

--- Bundlebot/build/BUILD_config.bat
+++ Bundlebot/build/BUILD_config.bat
@@ -7,9 +7,6 @@
 :: FDS-6.8.0-1561-g50afa7f/master
 :: SMV-6.8.0-2075-g38047e9/master
 
-set BUNDLE_OPTION=test
-::set BUNDLE_OPTION=release
-
 set BUNDLE_FDS_REVISION=50afa7f
 set BUNDLE_FDS_TAG=FDS-6.9.0
 set BUNDLE_SMV_REVISION=38047e9
@@ -17,8 +14,6 @@ set BUNDLE_SMV_TAG=SMV-6.9.0
 
 :: lines below should not need to be changed
 
-if NOT "%BUNDLE_OPTION%" == "release" set BUNDLE_OPTION=test
-if "%BUNDLE_OPTION%" == "test" set GH_REPO=test_bundles
-if "%BUNDLE_OPTION%" == "test" set GH_FDS_TAG=BUNDLE_TEST
-if "%BUNDLE_OPTION%" == "test" set GH_SMOKEVIEW_TAG=BUNDLE_TEST
-if "%BUNDLE_OPTION%" == "release" set GH_REPO=fds
+set GH_REPO=test_bundles
+set GH_FDS_TAG=BUNDLE_TEST
+set GH_SMOKEVIEW_TAG=BUNDLE_TEST
--- Bundlebot/build/BUILD_config.sh
+++ Bundlebot/build/BUILD_config.sh
@@ -7,9 +7,6 @@
 # FDS-6.8.0-1561-g50afa7f/master
 # SMV-6.8.0-2075-g38047e9/master
 
-export BUNDLE_OPTION=test
-#export BUNDLE_OPTION=release
-
 export BUNDLE_FDS_REVISION=50afa7f
 export BUNDLE_FDS_TAG=FDS-6.9.0
 export BUNDLE_SMV_REVISION=38047e9
@@ -17,17 +14,10 @@ export BUNDLE_SMV_TAG=SMV-6.9.0
 
 # lines below should not need to be changed
 
-if [ "$BUNDLE_OPTION" != "release" ]; then
-  BUNDLE_OPTION=test
-fi
-if [ "$BUNDLE_OPTION" == "test" ]; then
-  GH_REPO=test_bundles
-  GH_FDS_TAG=BUNDLE_TEST
-  GH_SMOKEVIEW_TAG=BUNDLE_TEST
-fi
-if [ "$BUNDLE_OPTION" == "release" ]; then
-  GH_REPO=fds
-fi
+GH_REPO=test_bundles
+GH_FDS_TAG=BUNDLE_TEST
+GH_SMOKEVIEW_TAG=BUNDLE_TEST
+
 export GH_REPO
 export GH_FDS_TAG
 export GH_SMOKEVIEW_TAG
