aabe5ecff7e2500dc3f777f5d48930f0761ac781
gforney <gforney@gmail.com>
2024-02-25 17:23:28 -0500

    bundle: set title in bundle script

--- Bundlebot/build/BUILD_bundle.sh
+++ Bundlebot/build/BUILD_bundle.sh
@@ -14,4 +14,6 @@ cd $CURDIR
 cd ../scripts
 ./run_bundlebot.sh -f -c -R release -F $BUNDLE_FDS_REVISION -X $BUNDLE_FDS_TAG -S $BUNDLE_SMV_REVISION -Y $BUNDLE_SMV_TAG -o firemodels $GHREPO
 cd $CURDIR
-./BUILD_settitle.sh
+
+TITLE="Bundle Test - $BUNDLE_FDS_TAG/$BUNDLE_FDS_REVISION - $BUNDLE_SMV_TAG/$BUNDLE_SMV_REVISION"
+gh release edit $GH_FDS_TAG  -t "$TITLE" -R github.com/$GH_OWNER/$GH_REPO
--- Bundlebot/build/BUILD_settitle.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source BUILD_config.sh
-
-TITLE="Bundle Test - $BUNDLE_FDS_TAG/$BUNDLE_FDS_REVISION - $BUNDLE_SMV_TAG/$BUNDLE_SMV_REVISION"
-gh release edit $GH_FDS_TAG  -t "$TITLE" -R github.com/$GH_OWNER/$GH_REPO
