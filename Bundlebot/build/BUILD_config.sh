c120f308dc6abb8c649a0e6a5feb9db9ee125411
gforney <gforney@gmail.com>
2024-02-25 05:33:17 -0500

    bundles: update fds and smv bundle revisions for 2/25 firebot pass

--- Bundlebot/build/BUILD_config.sh
+++ Bundlebot/build/BUILD_config.sh
@@ -3,13 +3,13 @@
 # It is run by the other BUILD scripts. 
 # You do not need to run it.
 
-# firebot pass 2/23/2024
-# FDS-6.8.0-1561-g50afa7f/master
-# SMV-6.8.0-2075-g38047e9/master
+# firebot pass 2/25/2024
+# FDS-6.8.0-1597-geacf8f6/master
+# SMV-6.8.0-2077-g009e372/master
 
-export BUNDLE_FDS_REVISION=50afa7f
+export BUNDLE_FDS_REVISION=eacf8f6
 export BUNDLE_FDS_TAG=FDS-6.9.0
-export BUNDLE_SMV_REVISION=38047e9
+export BUNDLE_SMV_REVISION=009e372
 export BUNDLE_SMV_TAG=SMV-6.9.0
 
 # lines below should not need to be changed
