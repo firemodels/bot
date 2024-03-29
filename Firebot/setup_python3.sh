478e92dc79e0a314b764a71a9496826e05d30d37
gforney <gforney@gmail.com>
2024-03-28 20:20:32 -0400

    firebot: check if python3 is installed

--- Firebot/setup_python3.sh
+++ Firebot/setup_python3.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 # setup environment for python 3 and run the hello_world.py to test the setup
 
-curdir=`pwd`
-cd ../..
-reporoot=`pwd`
+HAVEPYTHON3=`which python3 |& grep -v 'no python3'`
+if [ "$HAVEPYTHON3" != "" ]; then
+  echo "***error: python3 is not installed"
+else
+  curdir=`pwd`
+  cd ../..
+  reporoot=`pwd`
 
-cd $reporoot/fds/.github
-python3 -m venv fds_python_env
-source fds_python_env/bin/activate
-pip install -r requirements.txt
-cd $reporoot/fds/Utilities/Python
-python hello_world.py
-cd $curdir
+  cd $reporoot/fds/.github
+  python3 -m venv fds_python_env
+  source fds_python_env/bin/activate
+  pip install -r requirements.txt
+  cd $reporoot/fds/Utilities/Python
+  python hello_world.py
+  cd $curdir
+fi
