781a553a4265fee125dbd7943917a6a03bcac23b
gforney <gforney@gmail.com>
2024-04-18 11:23:53 -0400

    bundle scipts: fix typo in nightly bundle script

--- Bundlebot/nightly/BUILD_nightly.bat
+++ Bundlebot/nightly/BUILD_nightly.bat
@@ -3,7 +3,7 @@ set CURDIR=%CD%
 set develop=%1
 
 :: build and upload bundle
-if not exit %userprofile%\.bundle mkdir %userprofile%\.bundle
+if not exist %userprofile%\.bundle mkdir %userprofile%\.bundle
 call run_bundlebot -c %develop% > %userprofile%\.bundle\bundle_nightly.out 2>&1
 
 :: build but don't upload bundle (for testing)
