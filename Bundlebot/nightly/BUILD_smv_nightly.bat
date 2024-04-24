f7ae8682d092a0b4c347d0cb284613ca205a26b3
gforney <gforney@gmail.com>
2024-04-24 16:10:47 -0400

    bundle scripts: minor edits to nightly smokeview bundle script

--- Bundlebot/nightly/BUILD_smv_nightly.bat
+++ Bundlebot/nightly/BUILD_smv_nightly.bat
@@ -2,10 +2,9 @@
 setlocal
 set CURDIR=%CD%
 
-:: build and upload bundle
+:: build and upload a smokeview bundle
 if not exist %userprofile%\.bundle mkdir %userprofile%\.bundle
-::call make_smv_bundle > %userprofile%\.bundle\bundle_smv_nightly.out 2>&1
-call make_smv_bundle 
+call make_smv_bundle > %userprofile%\.bundle\bundle_smv_nightly.out 2>&1
 
 cd %CURDIR%
 echo complete
