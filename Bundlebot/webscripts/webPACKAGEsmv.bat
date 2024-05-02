0a6bbf7035498407358e02d1cd538e15b8009026
gforney <gforney@gmail.com>
2024-05-02 10:55:31 -0400

    bundle scripts: correct a script name

--- Bundlebot/webscripts/webPACKAGEsmv.bat
+++ Bundlebot/webscripts/webPACKAGEsmv.bat
@@ -57,7 +57,7 @@ if "%platform%" == "Linux" (
   echo.
   echo --- making 64 bit Linux Smokeview installer ---
   echo.
-  plink %plink_options% %linux_logon% %scriptdir%/scripts/make_bundle.sh %buildtype% %version% %linux_git_root%
+  plink %plink_options% %linux_logon% %scriptdir%/scripts/assemble_smvbundle.sh %buildtype% %version% %linux_git_root%
   goto eof
 )
 
@@ -67,7 +67,7 @@ if "%platform%" == "OSX" (
   echo.
   echo --- making 64 bit OSX Smokeview installer ---
   echo.
-  plink %plink_options% %osx_logon% %scriptdir%/scripts/make_bundle.sh %buildtype% %version% %linux_git_root%
+  plink %plink_options% %osx_logon% %scriptdir%/scripts/assemble_smvbundle.sh %buildtype% %version% %linux_git_root%
   goto eof
 )
 
