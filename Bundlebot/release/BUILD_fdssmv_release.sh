61f913f67bec5b3d62e2ccfaad09bda96ff1d0a2
gforney <gforney@gmail.com>
2024-05-02 16:58:18 -0400

    bundle scripts: correct name of bundle scriprt (run_bundlebot.sh -> BUILD_fdssmv_nightly.sh)

--- Bundlebot/release/BUILD_fdssmv_release.sh
+++ Bundlebot/release/BUILD_fdssmv_release.sh
@@ -13,7 +13,7 @@ cd ../../Scripts
 cd $CURDIR
 
 cd ../nightly
-./run_bundlebot.sh -f -c -R release -F $BUNDLE_FDS_REVISION -X $BUNDLE_FDS_TAG -S $BUNDLE_SMV_REVISION -Y $BUNDLE_SMV_TAG -o firemodels $GHREPO
+./BUILD_fdssmv_nightly.sh -R release -F $BUNDLE_FDS_REVISION -X $BUNDLE_FDS_TAG -S $BUNDLE_SMV_REVISION -Y $BUNDLE_SMV_TAG -o firemodels $GHREPO
 cd $CURDIR
 
 TITLE="Bundle Test - $BUNDLE_FDS_TAG/$BUNDLE_FDS_REVISION - $BUNDLE_SMV_TAG/$BUNDLE_SMV_REVISION"
