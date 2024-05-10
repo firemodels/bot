a28289607eb3b0c47dbc59ab38dde80f46bbe543
gforney <gforney@gmail.com>
2024-05-10 16:02:14 -0400

    revert smokeview 6.9.4 commits (will wait to update to 6.9.4)

--- Bundlebot/release/config.bat
+++ Bundlebot/release/config.bat
@@ -28,9 +28,9 @@ set BUNDLE_FIG_TAG=FIG-6.9.1
 set BUNDLE_OUT_REVISION=7593d3181
 set BUNDLE_OUT_TAG=OUT-6.9.1
 
-:: SMV-6.9.3-120-gdc902a673
-set BUNDLE_SMV_REVISION=dc902a673
-set BUNDLE_SMV_TAG=SMV-6.9.4
+:: SMV-6.9.2-67-gc9109b6c2
+set BUNDLE_SMV_REVISION=c9109b6c2
+set BUNDLE_SMV_TAG=SMV-6.9.3
 
 :: ----------------------------
 :: github environment variables
