10749147c8cd27d39db5530220ca3cfbc93233bd
gforney <gforney@gmail.com>
2024-05-28 22:26:48 -0400

    bundle scripts: upload smokeview releases to local repo

--- Bundlebot/release/BUILD_smv_release.bat
+++ Bundlebot/release/BUILD_smv_release.bat
@@ -10,7 +10,7 @@ call config
 set basename=%BUNDLE_SMV_TAG%_win
 set fullfilebase=%userprofile%\.bundle\uploads\%basename%
 
-set GH_SMV_TAG=SMOKEVIEW_TEST2
+set GH_SMV_TAG=SMOKEVIEW_TEST
 
 :: clone smv repo
 call clone_smv_repo  %BUNDLE_SMV_REVISION% release %BUNDLE_SMV_TAG%
@@ -23,8 +23,8 @@ call make_smv_bundle %BUNDLE_SMV_TAG%
 
 :: upload bundles
 echo uploading %fullfilebase%.sha1
-gh release upload %GH_SMV_TAG% %fullfilebase%.sha1 -R github.com/%GH_OWNER%/%GH_REPO% --clobber
+gh release upload %GH_SMV_TAG% %fullfilebase%.sha1 -R github.com/%GH_SMOKEVIEW_OWNER%/%GH_REPO% --clobber
 
 echo uploading %fullfilebase%.exe
-gh release upload %GH_SMV_TAG% %fullfilebase%.exe  -R github.com/%GH_OWNER%/%GH_REPO% --clobber
+gh release upload %GH_SMV_TAG% %fullfilebase%.exe  -R github.com/%GH_SMOKEVIEW_OWNER%/%GH_REPO% --clobber
 
