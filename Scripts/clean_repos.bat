aa32f6a550f862a0841ca113d324c75fdc19df93
gforney <gforney@gmail.com>
2024-08-21 13:28:47 -0400

    scripts: add batch file to clean repos

---
+++ Scripts/clean_repos.bat
@@ -0,0 +1,30 @@
+@echo off
+set CUR=%CD%
+set allrepos=bot cad cfast cor exp fds fig out radcal smv test_bundles
+
+cd ..\..
+set repo=%CD%
+cd %CUR%
+
+for %%x in ( %allrepos% ) do ( call :clean_repo %%x )
+
+goto eof
+
+:clean_repo
+  set reponame=%1
+  echo.
+  echo ----------------------------------
+  echo cleaning %reponame%
+  echo ----------------------------------
+  set repodir=%repo%\%reponame%
+  if not exist %repodir% (
+     exit /b
+  )
+  cd %repodir%
+  git remote prune origin 1> Nul 2> Nul
+  git checkout master     1> Nul 2> Nul
+  git clean -dxf          1> Nul 2> Nul
+  exit /b
+
+:eof
+cd %CUR%
