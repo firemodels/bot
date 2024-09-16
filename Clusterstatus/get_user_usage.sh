beb5a09a2c863d225a18a243783f0441f5abb809
gforney <gforney@gmail.com>
2024-09-15 20:17:46 -0400

    clusterstatus: fix problem with cluster status web page when no jobs are running

--- Clusterstatus/get_user_usage.sh
+++ Clusterstatus/get_user_usage.sh
@@ -6,16 +6,25 @@ infoout=info.out.$$
 #qstat -a | grep -v Q | awk 'NR>4' | awk '{print $2," ",$7}' | sort > $qstatout
 qstat -a | awk 'NR>5' | awk '{print $2," ",$7," ",$10}' | grep -v Q | grep -v C | awk '{print $1," ",$2}' | sort > $qstatout
 njobs=`cat $qstatout | wc -l`
-if [ $njobs -gt 0 ]; then
 awk '{a[$1] += $2} END{for (i in a) print i,a[i]}' $qstatout | sort > $infoout
 cat << EOF
 <table>
 <tr>
 <td valign=top>
 <table border=on>
+EOF
+if [ $njobs -gt 0 ]; then
+cat << EOF
 <tr><th>User</th><th>cores</th></tr>
 EOF
   cat $infoout | awk '{sum+=$2;print "<tr><td align=left>",$1,"</td>", "<td align=right>",$2,"</td></tr>"} END{print "<tr><th>Total</th><td align=right>",sum,"</td></tr>"}' 
+else
+cat << EOF
+<tr><th>User</th><th>cores</th></tr>
+<tr><td align=left>none</td><td align=right>0</td></tr>
+<tr><th>Total</td><td align=right>0</td></tr>
+EOF
+fi
 cat << EOF
 <tr></tr>
 <tr></tr>
@@ -23,6 +32,8 @@ cat << EOF
 <tr><th>$CB_HEAD</th><td align=right>$head_load</td></tr>
 <tr><th>$CB_LOGIN</th><td align=right>$login_load</td></tr>
 </table>
+EOF
+cat << EOF
 </td>
 <td></td>
 EOF
@@ -41,5 +52,4 @@ cat << EOF
 </table>
 EOF
 rm -f $infoout
-fi
 rm $qstatout
