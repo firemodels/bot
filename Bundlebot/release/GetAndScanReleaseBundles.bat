ff9503de8161b374215f0c9930401a59fe1ad3cd
gforney <gforney@gmail.com>
2024-12-04 11:44:47 -0500

    bundle scripts: add a script to download and scan release bundles

---
+++ Bundlebot/release/GetAndScanReleaseBundles.bat
@@ -0,0 +1,26 @@
+@echo off
+set TAG=FDS-6.9.1
+
+echo cleaning bundles directory
+set CURDIR=%CD%
+cd bundles
+git clean -dxf > Nul
+
+cd %CURDIR%
+
+echo downloading Linux and OSX bundles
+gh release download %TAG% -p FDS*.sh -D bundles  -R github.com/firemodels/fds
+
+echo downloading Windows bundle
+gh release download %TAG% -p FDS*.exe -D bundles  -R github.com/firemodels/fds
+
+echo downloading documents
+gh release download %TAG% -p FDS*.pdf -D bundles  -R github.com/firemodels/fds
+
+echo downloading sha1 files
+gh release download %TAG% -p FDS*.sha1 -D bundles  -R github.com/firemodels/fds
+
+echo.
+echo scanning nightly bundles in %BUNDLEDIR%
+set BUNDLEDIR=%CD%\bundles
+sentinelctl scan_folder -t -i %BUNDLEDIR%
