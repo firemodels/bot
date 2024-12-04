ab828a3be458998848f42fb4b34477b7f6468f67
gforney <gforney@gmail.com>
2024-12-04 13:39:31 -0500

    bundle scripts: don't scan .gz or .zip files

--- Bundlebot/nightly/GetAndScanNightlyBundles.bat
+++ Bundlebot/nightly/GetAndScanNightlyBundles.bat
@@ -9,12 +9,6 @@ echo downloading Linux and OSX bundles
 gh release download FDS_TEST -p FDS*.sh -D bundles  -R github.com/firemodels/test_bundles
 echo downloading Windows bundle
 gh release download FDS_TEST -p FDS*.exe -D bundles  -R github.com/firemodels/test_bundles
-echo downloading .tar.gz files
-gh release download FDS_TEST -p FDS*.tar.gz -D bundles  -R github.com/firemodels/test_bundles
-echo downloading .tar files
-gh release download FDS_TEST -p FDS*.tar -D bundles  -R github.com/firemodels/test_bundles
-echo downloading Windows .zip file
-gh release download FDS_TEST -p FDS*.zip -D bundles  -R github.com/firemodels/test_bundles
 echo downloading Windows .pdf files
 gh release download FDS_TEST -p FDS*.pdf -D bundles  -R github.com/firemodels/test_bundles
 echo downloading sha1 files
