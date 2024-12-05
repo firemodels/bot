2f0241dd6b5a6eed46602840d9ab49d5c5fd2550
gforney <gforney@gmail.com>
2024-12-05 09:44:55 -0500

    bundle scripts: clean bundles directory before downloading bundles (windows)

--- Bundlebot/nightly/GetNightlyBundles.bat
+++ Bundlebot/nightly/GetNightlyBundles.bat
@@ -1,6 +1,24 @@
-@echo on
+@echo off
+
+echo cleaning bundles directory
+cd bundles
+git clean -dxf > Nul
+cd ..
 
 echo downloading Linux and OSX bundles
-gh release download FDS_TEST -p FDS*.sh -D bundles  -R github.com/firemodels/test_bundles
+gh release download FDS_TEST -p FDS*.sh     -D bundles  -R github.com/firemodels/test_bundles
+
 echo downloading Windows bundle
-gh release download FDS_TEST -p FDS*.exe -D bundles  -R github.com/firemodels/test_bundles
+gh release download FDS_TEST -p FDS*.exe    -D bundles  -R github.com/firemodels/test_bundles
+
+echo downloading .pdf files
+gh release download FDS_TEST -p FDS*.pdf    -D bundles  -R github.com/firemodels/test_bundles
+
+echo downloading .tar.gz files
+gh release download FDS_TEST -p FDS*.tar.gz -D bundles  -R github.com/firemodels/test_bundles
+
+echo downloading .tar files
+gh release download FDS_TEST -p FDS*.tar    -D bundles  -R github.com/firemodels/test_bundles
+
+echo downloading sha1 files
+gh release download FDS_TEST -p FDS*.sha1   -D bundles  -R github.com/firemodels/test_bundles
