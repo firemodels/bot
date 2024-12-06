9789690b966b0fae328e035f909aaaed3f3367e4
gforney <gforney@gmail.com>
2024-12-06 10:23:35 -0500

    scripts: add a bash script for 'signing' bundle files

--- Bundlebot/nightly/SignNightlyBundles.sh
+++ Bundlebot/nightly/SignNightlyBundles.sh
@@ -3,9 +3,31 @@
 CURDIR=`pwd`
 cd bundles
 
-for file in *.sh
+#------------------------------
+SIGN_FILES()
+#------------------------------
+{
+EXT=$1
+
+echo
+echo ---------------------------
+echo signing $EXT files
+echo ---------------------------
+
+for file in *.$EXT
 do
+  echo
   echo signing $file
-  gpg --output $file.sig --detach-sig $file
+  rm -f $file.sig
+  gpg --output $file.sig --armor --detach-sig $file
+  echo
+  echo verifying $file
+  gpg --verify $file.sig $file
 done
+}
+
+SIGN_FILES sh
+SIGN_FILES pdf
+SIGN_FILES tar.gz
+SIGN_FILES tar
 
