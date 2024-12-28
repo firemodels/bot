6868da76823a820f874b0bff7db9925638b6d6e4
gforney <gforney@gmailcom>
2024-12-27 21:33:08 -0500

    scripts: minor updates to linux/osx newbranch.sh script

--- Scripts/newbranch.sh
+++ Scripts/newbranch.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-repo=$1
-branch=$2
+branch=$1
+repo=$2
 
-if [ $# -ne 2 ]; then
-  echo ***error: found $# arguments, was expecting 2
-  echo usage:
-  echo ./newbranch repo branch
+if [[ "$branch" == "" ]] && [[ "$repo" == "" ]]; then
+  echo "Usage: newbranch.sh branch [repo]"
+  echo " create a branch in repo repo."
+  echo ""
+  echo " branch - branch to be created"
+  echo " repo   - repo branch is created in (default: smv)"
   exit
 fi
 
@@ -16,13 +18,20 @@ GITROOT=`pwd`
 
 cd $CURDIR
 
+if [ "$repo" == "" ]; then
+  repo=smv
+fi
 repodir=$GITROOT/$repo
 if [ ! -d $repodir ]; then
-  echo ***error: repo $repodir does not exist
+  echo ***error: repo $repodir does not exist"
+  exit
+fi
+if [ "$branch" == "" ]; then
+  echo ***error: branch not specfied"
   exit
 fi
 
-echo "Do you wish to create the branch: $branch in repo: $repo ?"
+echo "Do you wish to create the branch: $branch in repo: $repo "
 echo "Press any key to continue or <CTRL> c to abort."
 read val
 
