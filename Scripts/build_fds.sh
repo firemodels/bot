7834250781c7580533f190407331fb62264c4b1d
gforney <gforney@gmail.com>
2025-01-04 21:03:52 -0500

    smokebot: try building FDS a 2nd time if the first time failed

--- Scripts/build_fds.sh
+++ Scripts/build_fds.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 OUT=$1
+EXE=$2
 
 make --makefile ../makefile clean &> /dev/null
 ./make_fds.sh &> $OUT
+if [ "$EXE" != "" ]; then
+  if [ ! -x $EXE ]; then
+    git clean -dxf >& /dev/null
+    echo "----------------------------------" >> $OUT 2>&1
+    echo first attempt at building fds failed >> $OUT 2>&1
+    ./make_fds.sh                             >> $OUT 2>&1
+  fi
+fi
 
 rm -f compiling
