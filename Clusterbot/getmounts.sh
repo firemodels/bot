07ca2c17430e5f137ef00ce92a260ad3fc7ca31c
gforney <gforney@gmail.com>
2025-01-27 20:30:56 -0500

    scripts: remove Clusterbot directory - moved to a gitlab repo

--- Clusterbot/getmounts.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-todir=$1
-
-file=file_mounts
-base=`basename $file`
-df | grep ':'  | grep -v Filesystem | awk '{print $1 $6}' | sort > $todir/${base}.`hostname -s`
-echo ${base}.`hostname -s`
