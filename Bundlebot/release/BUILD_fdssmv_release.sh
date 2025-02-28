e653d67bb0d0a72ce59abc509b358e20933e1593
gforney <gforney@gmail.com>
2025-02-28 16:04:27 -0500

    bundle scripts: minor edits

--- Bundlebot/release/BUILD_fdssmv_release.sh
+++ Bundlebot/release/BUILD_fdssmv_release.sh
@@ -5,8 +5,8 @@ export DISABLEPUSH=1
 export BUILDING_release=1
 
 # FDS-6.9.0-96-g889da6ae0
-export BUNDLE_FDS_REVISION=889da6ae0
-export BUNDLE_FDS_TAG=FDS-6.9.1test
+#export BUNDLE_FDS_REVISION=889da6ae0
+#export BUNDLE_FDS_TAG=FDS-6.9.1test
 
 OWNER=`whoami`
 
@@ -17,7 +17,7 @@ cd ../../Scripts
 cd $CURDIR
 
 cd ../nightly
-./BUILD_fdssmv_nightly.sh -R release -F $BUNDLE_FDS_REVISION -X $BUNDLE_FDS_TAG -S $BUNDLE_SMV_REVISION -Y $BUNDLE_SMV_TAG -o $OWNER -r test_bundles
+./BUILD_fdssmv_nightly.sh -f -R release -F $BUNDLE_FDS_REVISION -X $BUNDLE_FDS_TAG -S $BUNDLE_SMV_REVISION -Y $BUNDLE_SMV_TAG -o $OWNER -r test_bundles
 cd $CURDIR
 
 TITLE="Bundle Test - $BUNDLE_FDS_TAG/$BUNDLE_FDS_REVISION - $BUNDLE_SMV_TAG/$BUNDLE_SMV_REVISION"
