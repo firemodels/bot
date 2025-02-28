5ae7b300023d9ae138ee12e42eacab3b0737798e
gforney <gforney@gmail.com>
2025-02-28 11:01:27 -0500

    bundle: update config scripts using 6.10.0alpha

--- Bundlebot/release/config.sh
+++ Bundlebot/release/config.sh
@@ -4,24 +4,24 @@
 # ----------------------------
 # repo environment variables
 
-# BOT-6.9.1-421-ge9ae2ac3c
-export BUNDLE_BOT_REVISION=e9ae2ac3c
-export BUNDLE_BOT_TAG=BOT-6.9.2alpha
+# BOT-6.9.1-436-g351a6020e
+export BUNDLE_BOT_REVISION=351a6020e
+export BUNDLE_BOT_TAG=BOT-6.10.0alpha
 # CAD-6.9.1-6-g4675b4b
 export BUNDLE_CAD_REVISION=4675b4b
-export BUNDLE_CAD_TAG=CAD-6.9.2alpha
+export BUNDLE_CAD_TAG=CAD-6.10.0alpha
 # EXP-6.9.1-18-g69fc8c0c1
 export BUNDLE_EXP_REVISION=69fc8c0c1
-export BUNDLE_EXP_TAG=EXP-6.9.2alpha
-# FDS-6.9.1-1993-g8ffaf45a0
-export BUNDLE_FDS_REVISION=8ffaf45a0
-export BUNDLE_FDS_TAG=FDS-6.9.2alpha
+export BUNDLE_EXP_TAG=EXP-6.10.0alpha
+# FDS-6.9.1-2012-g771a14b73
+export BUNDLE_FDS_REVISION=771a14b73
+export BUNDLE_FDS_TAG=FDS-6.10.0alpha
 # FIG-6.9.1-87-g67786c7
 export BUNDLE_FIG_REVISION=67786c7
-export BUNDLE_FIG_TAG=FIG-6.9.2alpha
-# OUT-6.9.1-106-g507e4724c
-export BUNDLE_OUT_REVISION=507e4724c
-export BUNDLE_OUT_TAG=OUT-6.9.2alpha
+export BUNDLE_FIG_TAG=FIG-6.10.0alpha
+# OUT-6.9.1-108-gada0a73ac
+export BUNDLE_OUT_REVISION=ada0a73ac
+export BUNDLE_OUT_TAG=OUT-6.10.0alpha
 # SMV-6.9.6-191-g81dd82ee5
 export BUNDLE_SMV_REVISION=81dd82ee5
-export BUNDLE_SMV_TAG=SMV-6.9.7alpha
+export BUNDLE_SMV_TAG=SMV-6.10.0alpha
