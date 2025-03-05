7c4f546b6d7f4c4f52d5eee8385dc30a10979683
gforney <gforney@gmail.com>
2025-03-04 21:27:19 -0500

    bundle scripts: add options to build smokeview manuals scripts for specifying mailto address and github owner

--- Bundlebot/release/BUILD_smv_manuals.sh
+++ Bundlebot/release/BUILD_smv_manuals.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
+
+MAILTO=
+if [ "$BUNDLE_EMAIL" != "" ]; then
+  MAILTO="-m $BUNDLE_EMAIL"
+fi
+
+OWNER="-o firemodels"
+if [ "$BUNDLE_OWNER" != "" ]; then
+  OWNER="-o $BUNDLE_OWNER"
+fi
+
+#*** parse command line options
+
+while getopts 'm:o:' OPTION
+do
+case $OPTION  in
+  m)
+   MAILTO="-m $OPTARG"
+   ;;
+  o)
+   OWNER="-o $OPTARG"
+   ;;
+esac
+done
+shift $(($OPTIND-1))
+
 # this script runs smokebot to build smokeview manuals using revision and tags defined in config.sh
 source config.sh
-OWNER="-o firemodels"
-MAILTO=
 
 #echo ***updating repos
 CURDIR=`pwd`
@@ -12,11 +36,5 @@ echo "setting up repos"
 ./update_repos.sh -m
 cd $CURDIR
 
-if [ "$BUNDLE_EMAIL" != "" ]; then
-  MAILTO="-m $BUNDLE_EMAIL"
-fi
-if [ "$BUNDLE_OWNER" != "" ]; then
-  OWNER="-o $BUNDLE_OWNER"
-fi
 cd ../../Smokebot
 ./run_smokebot.sh -C -c -u -J -f -q firebot $MAILTO -x $BUNDLE_FDS_REVISION -X $BUNDLE_FDS_TAG -y $BUNDLE_SMV_REVISION -Y $BUNDLE_SMV_TAG $OWNER -r test_bundles -U -R release
