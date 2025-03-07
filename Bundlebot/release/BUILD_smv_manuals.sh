55578ad39749d3c3310ede748ad2cd6fa0f1a1c4
gforney <gforney@gmail.com>
2025-03-07 14:41:30 -0500

    bundle: add cleaning to smokeview build manuals script

--- Bundlebot/release/BUILD_smv_manuals.sh
+++ Bundlebot/release/BUILD_smv_manuals.sh
@@ -28,13 +28,24 @@ shift $(($OPTIND-1))
 # this script runs smokebot to build smokeview manuals using revision and tags defined in config.sh
 source config.sh
 
-#echo ***updating repos
 CURDIR=`pwd`
+
+echo ***clean files
+cd ../../Smokebot
+git clean -dxf >& /dev/null
+cd $CURDIR/output
+git clean -dxf >& /dev/null
+cd $CURDIR/../nightly
+git clean -dxf >& /dev/null
+
+cd $CURDIR
+
+
+#echo ***updating repos
 cd ../../Scripts
 echo "setting up repos"
 ./setup_repos.sh -A
-./update_repos.sh -m
-cd $CURDIR
+./update_repos.sh -w
 
-cd ../../Smokebot
+cd $CURDIR/../../Smokebot
 ./run_smokebot.sh -C -c -u -J -f -q firebot $MAILTO -x $BUNDLE_FDS_REVISION -X $BUNDLE_FDS_TAG -y $BUNDLE_SMV_REVISION -Y $BUNDLE_SMV_TAG $OWNER -r test_bundles -U -R release
