59185a4239d96024d902347d084a7207d62d14f9
gforney <gforney@gmail.com>
2025-04-09 15:44:58 -0400

    cfast bundle: update config scripts to include the exp repo

--- Bundlebot/cfast/release/config.bat
+++ Bundlebot/cfast/release/config.bat
@@ -1,10 +1,19 @@
 @echo off
+:: This scripts defines revisions and tags for a bundle.
+:: It is run by other BUILD scripts to define the environment.
+:: ----------------------------
+:: repo environment variables
 
 :: CFAST7.7.4-200-gad128be8a
 set BUNDLE_CFAST_REVISION=ad128be8a
 set BUNDLE_CFAST_TAG=CFAST-7.7.5
 
+:: EXP-6.10.0-0-g3c1046d02
+set BUNDLE_EXP_REVISION=3c1046d02
+set BUNDLE_EXP_TAG=EXP-6.10.1
+
+
 :: SMV-6.10.1-16-gbb2116a35
-set BUNDLE_SMV_REVISION=fe486ab05
+set BUNDLE_SMV_REVISION=bb2116a35
 set BUNDLE_SMV_TAG=SMV-6.10.1
 
