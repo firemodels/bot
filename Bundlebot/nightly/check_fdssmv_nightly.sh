a10cc784dec7221373c9f188a3ceb6a07bcde516
gforney <gforney@gmail.com>
2025-08-19 10:00:14 -0400

    bundle: add a script to check if FDS/smokeview nightly bundles were generated

---
+++ Bundlebot/nightly/check_fdssmv_nightly.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+#---------------------------------------------
+#                   usage
+#---------------------------------------------
+
+function usage {
+echo ""
+#echo "run_bundlebot.sh usage"
+echo ""
+echo "This script checks if nightly fds/smv bundles were generated"
+echo ""
+echo "Options:"
+echo "-h - display this message"
+
+if [ "$MAILTO" != "" ]; then
+  echo "-m mailto - email address [default: $MAILTO]"
+else
+  echo "-m mailto - email address"
+fi
+exit 0
+}
+
+while getopts 'hm:' OPTION
+do
+case $OPTION  in
+  h)
+   usage
+   ;;
+  m)
+   MAILTO="$OPTARG"
+   ;;
+esac
+done
+shift $(($OPTIND-1))
+
+
+uploads=uploads.txt
+errors=errors.txt
+gh release view FDS_TEST  -R github.com/firemodels/test_bundles | grep test_win | awk '{print $2}' >  $uploads
+gh release view FDS_TEST  -R github.com/firemodels/test_bundles | grep test_lnx | awk '{print $2}' >> $uploads
+gh release view FDS_TEST  -R github.com/firemodels/test_bundles | grep test_osx | awk '{print $2}' >> $uploads
+rm -f FDS_INFO.txt
+gh release download FDS_TEST -p FDS_INFO.txt     -D .  -R github.com/firemodels/test_bundles
+FDS_REVISION=`grep FDS_REVISION FDS_INFO.txt | awk '{print $2}'`
+SMV_REVISION=`grep SMV_REVISION FDS_INFO.txt | awk '{print $2}'`
+BASE=${FDS_REVISION}_${SMV_REVISION}
+FDSWIN=${BASE}_test_winx
+FDSLNX=${BASE}_test_lnx
+FDSOSX=${BASE}_test_osx
+rm -f $errors
+if [ `grep $FDSWIN.exe $uploads | wc -l` -eq 0 ]; then
+  echo  "***error: $FDSWIN.exe missing" >> $errors
+fi
+if [ `grep $FDSWIN.sha1 $uploads | wc -l` -eq 0 ]; then
+  echo  "***error: $FDSWIN.sha1 missing" >> $errors
+fi
+if [ `grep $FDSLNX.sh   $uploads  | grep -v sha1 | wc -l` -eq 0 ]; then
+  echo  "***error: $FDSLNX.sh missing" >> $errors
+fi
+if [ `grep $FDSLNX.sha1 $uploads  | wc -l` -eq 0 ]; then
+  echo  "***error: $FDSLNX.sha1 missing" >> $errors
+fi
+if [ `grep $FDSOSX.sh   $uploads  | grep -v sha1 | wc -l` -eq 0 ]; then
+  echo  "***error: $FDSOSX.sh missing" >> $errors
+fi
+if [ `grep $FDSOSX.sha1 $uploads  | wc -l` -eq 0 ]; then
+  echo  "***error: $FDSOSX.sha1 missing" >> $errors
+fi
+if [ -e $errors ]; then
+  cat $errors
+  if [ "$MAILTO" != "" ]; then
+    cat $errors | mail -s "***error: one or more nightlly FDS/Smokeview bundles were not generated" $MAILTO
+  fi
+else
+  echo All FDS/Smokeview bundles were generated
+fi
