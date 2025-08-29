b8d4ce4ca0a6bafee4bad3db4d74391003ace8a2
gforney <gforney@gmail.com>
2025-08-29 11:07:27 -0400

    scripts: add help and command line options to gitlog script

--- Scripts/gitlog.sh
+++ Scripts/gitlog.sh
@@ -1,18 +1,47 @@
 #!/bin/bash
-repo=$1
-month=$2
-year=$3
+function usage {
+echo "Create repos used by cfast, fds and/or smokview"
+echo ""
+echo "Options:"
+echo "-h      - display this message"
+echo "-m m    - month 1 to 12, default: $month"
+echo "-u user - user, default: $user"
+echo "-r repo - default: $repo"
+echo "-y y    - year, default: $year"
+exit
+}
 
-if [ "$year" == "" ]; then
-   year=$(date +%Y)
-fi
-if [ "$month" == "" ]; then
-   month=$(date +%m)
-fi
-if [ "$repo" == "" ]; then
-   repo=smv
-fi
-OWNER=`whoami`
+year=$(date +%Y)
+month=$(date +%m)
+repo=smv
+user=`whoami`
+showinput=
+
+while getopts 'hm:r:u:vy:' OPTION
+do
+case $OPTION  in
+  h)
+   usage
+   ;;
+  m)
+   month=$OPTARG;
+   ;;
+  r)
+   repo=$OPTARG;
+   ;;
+  u)
+   user=$OPTARG;
+   ;;
+  v)
+   showinput=1
+   ;;
+  y)
+   year=$OPTARG;
+   ;;
+esac
+done
+shift $(($OPTIND-1))
+
 
 fromdate=${year}-${month}-1
 todate=${year}-${month}-31
@@ -32,8 +61,12 @@ fi
 outfile=${repo}_${year}_${month}.log
 
 cd $repodir
-#echo "git --no-pager log --no-merges --abbrev-commit --since=$fromdate --until=$todate --pretty=oneline --date=short  >& $outfile"
-git --no-pager log --no-merges --abbrev-commit --since=$fromdate --until=$todate --pretty=format:"%ai %an %s"  | grep $OWNER
+if [ "$showinput" != "" ]; then
+  echo cd $repodir
+  echo "git --no-pager log --no-merges --abbrev-commit --since=$fromdate --until=$todate --pretty=format:\"%ai %an %s\"  | grep $user"
+else
+  git --no-pager log --no-merges --abbrev-commit --since=$fromdate --until=$todate --pretty=format:"%ai %an %s"  | grep $user
+fi
 
 
 
