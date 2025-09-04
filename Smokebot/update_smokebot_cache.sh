e922db602aece05ed97625ac35a3b1c88e955191
gforney <gforney@gmail.com>
2025-09-03 22:03:07 -0400

    smokebot: fix to update_smokebot_cache script

--- Smokebot/update_smokebot_cache.sh
+++ Smokebot/update_smokebot_cache.sh
@@ -3,9 +3,10 @@ echo Updating smokeview cache
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 
-CACHE_DIR=../../smokebot_cache
+cd ../..
+CACHE_DIR=smokebot_cache/Verification
 if [ ! -d $CACHE_DIR ]; then
-  mkdir $CACHE_DIR
+  mkdir -p $CACHE_DIR
 fi
 if [ ! -d $CACHE_DIR ]; then
   echo "***error: cache directory, $CACHE_DIR, failed to be created"
@@ -14,17 +15,11 @@ fi
 cd $CACHE_DIR
 CACHE_DIR=`pwd`
 
-cd ..
+cd ../..
 REPO_ROOT=`pwd`
 
-fdsrepo=$REPO_ROOT/fds
 smvrepo=$REPO_ROOT/smv
 ABORT=
-DIR=$fdsrepo/Build
-if [ ! -d $DIR ]; then
-  echo "***error: directory $DIR does not exist"
-  ABORT=1
-fi
 DIR=$smvrepo/Verification/Visualization
 if [ ! -d $DIR ]; then
   echo "***error: directory $DIR does not exist"
