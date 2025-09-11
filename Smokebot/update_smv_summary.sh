e492dbaf24fb6374a99e6854eaa95158f672b240
gforney <gforney@gmail.com>
2025-09-11 19:59:15 -0400

    compare: fix typo in update summary script

--- Smokebot/update_smv_summary.sh
+++ Smokebot/update_smv_summary.sh
@@ -4,5 +4,5 @@ SMV_REPO=../../smv
 cd $SMV_REPO/Manuals
 MANDIR=`pwd`
 cp $MANDIR/SMV_User_Guide/SCRIPT_FIGURES/*.png         $MANDIR/SMV_Summary/images/user
-cp $MANDIR/SMV_Technical_Guide/SCRIPT_FIGURES/*.png    $MANDIR/SMV_Summary/images/user
+cp $MANDIR/SMV_Technical_Reference_Guide/SCRIPT_FIGURES/*.png    $MANDIR/SMV_Summary/images/user
 cp $MANDIR/SMV_Verification_Guide/SCRIPT_FIGURES/*.png $MANDIR/SMV_Summary/images/verification
