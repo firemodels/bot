dd145f68bb9840ea8604e47b5a3b8d5fddc0cfa1
Chandan Paul <paulchandan19@gmail.com>
2025-11-13 16:40:12 -0500

    bot python: Have a common fds_python_setup scripts

--- Firebot/setup_python3.sh
+++ Firebot/setup_python3.sh
@@ -2,20 +2,11 @@
 # setup environment for python 3 and run the hello_world.py to test the setup
 # usage: source ./setup_python3.sh
 
-NOT_INSTALLED=`which python3 |& grep 'no python3'`
-if [ "$NOT_INSTALLED" != "" ]; then
-  echo "***Error: python3 is not installed"
-else
-  curdir=`pwd`
-  cd ../..
-  reporoot=`pwd`
+curdir=`pwd`
+cd ../..
+reporoot=`pwd`
 
-  cd $reporoot/fds/.github
-  python3 -m venv fds_python_env
-  source fds_python_env/bin/activate
-  pip install -r requirements.txt
-  export PYTHONPATH=$reporoot/fds/Utilities/Python:$PYTHONPATH
-  cd $reporoot/fds/Utilities/Python
-  python hello_world.py
-  cd $curdir
-fi
+cd $reporoot/fds/Utilities/Python
+source ./setup_python_env.sh
+python hello_world.py
+cd $curdir
