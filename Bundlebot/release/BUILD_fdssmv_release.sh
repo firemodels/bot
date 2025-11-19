67ced3c9cc21433151695ce7b9393b2a24463a01
gforney <gforney@gmail.com>
2025-11-19 09:14:54 -0500

    bundle script: don't update web repo when building a release, it will be cloned

--- Bundlebot/release/BUILD_fdssmv_release.sh
+++ Bundlebot/release/BUILD_fdssmv_release.sh
@@ -20,10 +20,6 @@ git clean -dxf  >& /dev/null
 cd $CURDIR/../nightly
 git clean -dxf  >& /dev/null
 
-echo ***cloning repos
-cd $CURDIR/../../Scripts
-./update_repos.sh -w
-
 cd $CURDIR/../nightly
 ./BUILD_fdssmv_nightly.sh -b -c -f -R release -F $BUNDLE_FDS_HASH -X $BUNDLE_FDS_TAG -S $BUNDLE_SMV_HASH -Y $BUNDLE_SMV_TAG -o $OWNER -r test_bundles
 
