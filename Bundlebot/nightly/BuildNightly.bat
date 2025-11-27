c3f6965893e70f85a9b7de119cacc911b8f368a5
gforney <gforney@gmail.com>
2025-11-27 17:41:16 -0500

    bundle scripts: mv run_bundlebot.bat to BuildNightly.bat

--- Bundlebot/nightly/BuildNightly.bat
+++ Bundlebot/nightly/BuildNightly.bat
@@ -1,14 +1,369 @@
 @echo off
-set CURDIR=%CD%
-set develop=%1
-
 set is_nightly=1
-
-:: build and upload bundle
 if not exist %userprofile%\.bundle mkdir %userprofile%\.bundle
-call run_bundlebot -c
+set clone=clone
+set CURDIR=%CD%
 
-:: build but don't upload bundle (for testing)
-:: call run_bundlebot -c -U
+set use_currentbot=
+
+set upload_bundle=1
+set use_currentbot=
+set FDS_BRANCH=master
+set SMV_BRANCH=master
+
+::set upload_bundle=
+::set use_currentbot=1
+::set FDS_BRANCH=master
+::set SMV_BRANCH=master
+
+set OWNER=%username%
+if "x%is_nightly%" == "x1" set OWNER=firemodels
+
+set clone=
+set FDS_HASH=
+set SMV_HASH=
+set FDS_TAG=
+set SMV_TAG=
+set BRANCH_NAME=nightly
+set logfile=%userprofile%\.bundle\logfile.txt
+set emailto=
+
+if EXIST .bundlebot goto endif1
+  echo ***error: run_bundlebot.bat must be run in bot/Bundlebot directory
+  exit /b 1
+:endif1
+
+::*** parse command line arguments
+call :getopts %*
+
+if "x%stopscript%" == "x" goto endif2
+  set stopscript=
+  exit /b 1
+:endif2
+
+set nightly=test
+set pub_dir=
+if NOT "x%BRANCH_NAME%" == "xrelease" goto skip_branch
+  set nightly=rls
+  set pub_dir=release
+:skip_branch
+
+:: make sure we are running in the master branch
+set BUNDLESCRIPTDIR=%CD%
+cd ..\..\..
+set REPOROOT=%CD%
+
+cd %REPOROOT%\bot
+set botrepo=%CD%
+
+if exist %REPOROOT%\webpages goto endif4
+  echo ***error: the webpages repo does not exist
+  cd %BUNDLESCRIPTDIR%
+  exit /b 1
+:endif4
+
+set email=%botrepo%\Scripts\email_insert.bat
+set emailexe=%userprofile%\bin\mailsend.exe
+if "x%emailto%" == "x" goto endif5
+  if exist %emailexe% goto endif5
+    echo ***warning: email program %emailexe% does not exist
+    set emailto=
+:endif5
+
+cd %REPOROOT%\webpages
+set webpagesrepo=%CD%
+
+cd ..
+set basedir=%CD%
+
+if "x%use_currentbot%" == "x1" goto skip1
+:: bring the bot repo up to date
+echo.
+echo ------------------------------------------------------
+echo ------------------------------------------------------
+echo updating bot repo
+echo.
+
+call :cd_repo %botrepo% master || exit /b 1
+git fetch origin master > Nul
+git merge origin/master > Nul
+:skip1
+
+:: bring the webpages repo up to date
+echo.
+echo ------------------------------------------------------
+echo ------------------------------------------------------
+echo updating web repo
+echo.
+
+call :cd_repo %webpagesrepo% nist-pages || exit /b 1
+git fetch origin nist-pages  > Nul
+git merge origin/nist-pages  > Nul
+
+cd %BUNDLESCRIPTDIR%
+
+:: create the bundle
+
+if x%is_nightly% == x1 goto else1
+:: hash and revisions obtained from config.bat (invoked in BuildRelease.bat)
+  set FDS_HASH_BUNDLER=%BUNDLE_FDS_HASH%
+  set SMV_HASH_BUNDLER=%BUNDLE_SMV_HASH%
+  set FDS_REVISION_BUNDLER=%BUNDLE_FDS_TAG%
+  set FDS_TAG=%BUNDLE_FDS_TAG%
+  set SMV_REVISION_BUNDLER=%BUNDLE_SMV_TAG%
+  set SMV_TAG=%BUNDLE_SMV_TAG%
+  goto endif1
+:else1
+:: hash and revisions obtained from latest firebot pass
+  call get_hash_revisions.bat || exit /b 1
+  set /p FDS_HASH_BUNDLER=<output\FDS_HASH
+  set /p SMV_HASH_BUNDLER=<output\SMV_HASH
+  set /p FDS_REVISION_BUNDLER=<output\FDS_REVISION
+  set /p SMV_REVISION_BUNDLER=<output\SMV_REVISION
+  erase output\FDS_HASH
+  erase output\SMV_HASH
+  erase output\FDS_REVISION
+  erase output\SMV_REVISION
+:endif1
+
+echo.                                                         > %logfile%
+echo ------------------------------------------------------  >> %logfile%
+echo ------------------------------------------------------  >> %logfile%
+echo Building bundle using:                                  >> %logfile%
+echo.                                                        >> %logfile%
+if "x%FDS_REVISION_BUNDLER%" == "x" goto skip_fdsrev
+  echo             FDS revision: %FDS_REVISION_BUNDLER%      >> %logfile%
+:skip_fdsrev
+
+if "x%FDS_HASH_BUNDLER%" == "x" goto skip_fdshash
+echo            FDS repo hash: %FDS_HASH_BUNDLER%            >> %logfile%
+:skip_fdshash
+
+if "x%FDS_TAG%" == "x" goto skip_fdstag
+echo             FDS repo tag: %FDS_TAG%                     >> %logfile%
+:skip_fdstag
+
+if "x%SMV_REVISION_BUNDLER%" == "x" goto skip_smvrev
+  echo             smv revision: %SMV_REVISION_BUNDLER%      >> %logfile%
+:skip_smvrev
+
+if "x%SMV_HASH_BUNDLER%" == "x" goto skip_smvhash
+echo            SMV repo hash: %SMV_HASH_BUNDLER%            >> %logfile%
+:skip_smvhash
+
+if "x%SMV_TAG%" == "x" goto skip_smvtag
+echo             SMV repo tag: %SMV_TAG%                     >> %logfile%
+:skip_smvtag
+
+if NOT "%emailto%" == "" (
+  echo                    email: %emailto%                   >> %logfile%
+)
+echo.                                                        >> %logfile%
+
+type %logfile%
+
+if "x%clone%" == "xclone" goto skip_warning
+  echo.
+  echo ---------------------------------------------------------------
+  echo ---------------------------------------------------------------
+  echo You are about to erase and then clone the fds and smv repos.
+  echo Press any key to continue or CTRL c to abort.
+  echo To avoid this warning, use the -c option on the command line
+  echo ---------------------------------------------------------------
+  echo ---------------------------------------------------------------
+  echo.
+  pause >Nul
+:skip_warning
+
+call clone_repos %FDS_HASH_BUNDLER% %SMV_HASH_BUNDLER% %FDS_TAG% %SMV_TAG% %BRANCH_NAME% %FDS_BRANCH% %SMV_BRANCH%  || exit /b 1
+
+echo.
+echo ------------------------------------------------------
+echo ------------------------------------------------------
+echo Building apps
+echo.
+
+cd %BUNDLESCRIPTDIR%
+call make_apps         || exit /b 1
+
+echo.
+echo ------------------------------------------------------
+echo ------------------------------------------------------
+echo Copying fds apps
+echo.
+cd %BUNDLESCRIPTDIR%
+call copy_apps fds bot || exit /b 1
+
+echo.
+echo ------------------------------------------------------
+echo ------------------------------------------------------
+echo Copying smv apps
+echo.
+
+cd %BUNDLESCRIPTDIR%
+call copy_apps smv bot || exit /b 1
+
+echo.
+echo ------------------------------------------------------
+echo ------------------------------------------------------
+echo Copying fds pubs
+echo.
+
+cd %BUNDLESCRIPTDIR%
+call copy_pubs firebot  %OWNER% || exit /b 1
+
+echo.
+echo ------------------------------------------------------
+echo ------------------------------------------------------
+echo Copying smv pubs
+echo.
+
+cd %BUNDLESCRIPTDIR%
+call copy_pubs smokebot %OWNER% || exit /b 1
+
+echo.
+echo ------------------------------------------------------
+echo ------------------------------------------------------
+echo making bundle
+echo.
+
+cd %BUNDLESCRIPTDIR%
+call make_bundle bot %FDS_REVISION_BUNDLER% %SMV_REVISION_BUNDLER% %nightly%
+
+cd %BUNDLESCRIPTDIR%
+
+if "x%upload_bundle%" == "x" goto skip_upload
+  echo.
+  echo ------------------------------------------------------
+  echo ------------------------------------------------------
+  echo uploading bundle
+  echo.
+
+  set filelist=%TEMP%\fds_smv_files_win.out
+  gh release view FDS_TEST -R github.com/%OWNER%/test_bundles | grep FDS | grep SMV | grep win | gawk "{print $2}" > %filelist%
+  for /F "tokens=*" %%A in (%filelist%) do gh release delete-asset FDS_TEST -R github.com/%OWNER%/test_bundles %%A -y
+  erase %filelist%
+
+  set /p basename=<%TEMP%\fds_smv_basename.txt
+
+  set fullfilebase=%userprofile%\.bundle\bundles\%basename%
+
+  echo gh release upload FDS_TEST %fullfilebase%.exe -R github.com/%OWNER%/test_bundles --clobber
+       gh release upload FDS_TEST %fullfilebase%.exe -R github.com/%OWNER%/test_bundles --clobber
+
+::  echo gh release upload FDS_TEST %fullfilebase%.zip -R github.com/%OWNER%/test_bundles --clobber
+::       gh release upload FDS_TEST %fullfilebase%.zip -R github.com/%OWNER%/test_bundles --clobber
+:skip_upload
+
+if "x%emailto%" == "x" goto endif6
+  call %email% %emailto% "PC bundle %FDS_REVISION_BUNDLER% %SMV_REVISION_BUNDLER% created on %COMPUTERNAME%" %logfile%
+:endif6
+
+goto eof
+
+
+::-----------------------------------------------------------------------
+:usage
+::-----------------------------------------------------------------------
+
+:usage
+echo.
+echo run_bundlebot usage
+echo.
+echo This script builds FDS and Smokeview apps and generates a bundle using either the
+echo specified fds and smv repo revisions or revisions from the latest firebot pass.
+echo.
+echo Options:
+echo -c - bundle without warning about cloning/erasing fds and smv repos 
+echo -h - display this message
+echo -m mailtto - send email to mailto
+echo -R name - branch name [default: %BRANCH_NAME%]
+echo -U - do not upload bundle
+exit /b 0
+
+::-----------------------------------------------------------------------
+:getopts
+::-----------------------------------------------------------------------
+ set stopscript=
+ if (%1)==() exit /b
+ set valid=0
+ set arg=%1
+ 
+ if "%1" EQU "-c" (
+   set clone=clone
+   set valid=1
+ )
+ if "%1" EQU "-h" (
+   call :usage
+   set stopscript=1
+   exit /b
+ )
+ if "%1" EQU "-m" (
+   set emailto=%2
+   set valid=1
+   shift
+ )
+ if "%1" EQU "-R" (
+   set BRANCH_NAME=%2
+   set valid=1
+   shift
+ )
+ if "%1" EQU "-U" (
+   set upload_bundle=
+   set valid=1
+ )
+ shift
+ if %valid% == 0 (
+   echo.
+   echo ***Error: the input argument %arg% is invalid
+   echo.
+   echo Usage:
+   call :usage
+   set stopscript=1
+   exit /b 1
+ )
+if not (%1)==() goto getopts
+exit /b 0
+
+:: -------------------------------------------------------------
+:chk_repo
+:: -------------------------------------------------------------
+
+set repodir=%1
+
+if NOT exist %repodir% (
+  echo ***error: repo directory %repodir% does not exist
+  echo  aborted
+  exit /b 1
+)
+exit /b 0
+
+
+:: -------------------------------------------------------------
+:cd_repo
+:: -------------------------------------------------------------
+
+set repodir=%1
+set repobranch=%2
+
+call :chk_repo %repodir% || exit /b 1
+
+cd %repodir%
+if "%repobranch%" == "" (
+  exit /b 0
+)
+git rev-parse --abbrev-ref HEAD>current_branch.txt
+set /p current_branch=<current_branch.txt
+erase current_branch.txt
+if "%repobranch%" NEQ "%current_branch%" (
+  echo ***error: in repo %repodir% found branch %current_branch%
+  echo            was expecting branch %repobranch%
+  echo  aborted
+  exit /b 1
+)
+exit /b 0
+
+:eof
+
 cd %CURDIR%
-echo complete
+exit /b 0
