48a089f622c82a66b536d2596e02714e5a47f070
gforney <gforney@gmail.com>
2025-11-27 21:45:05 -0500

    update config files for release bundles (testing)

--- Bundlebot/release/config.sh
+++ Bundlebot/release/config.sh
@@ -4,8 +4,8 @@
 # ----------------------------
 # repo environment variables
 
-# BOT-6.10.1-302-gd45412e6f-dirty
-export BUNDLE_BOT_HASH=d45412e6f
+# BOT-6.10.1-315-g6ba62bf46-dirty
+export BUNDLE_BOT_HASH=6ba62bf46
 export BUNDLE_BOT_TAG=BOT-6.10test
 
 # CAD-6.10.1-2-g7172120
@@ -16,8 +16,8 @@ export BUNDLE_CAD_TAG=CAD-6.10test
 export BUNDLE_EXP_HASH=6316af1dd
 export BUNDLE_EXP_TAG=EXP-6.10test
 
-# FDS-6.10.1-1614-gbd643754b4
-export BUNDLE_FDS_HASH=bd643754b4
+# FDS-6.10.1-1618-g5c3e6e20e6
+export BUNDLE_FDS_HASH=5c3e6e20e6
 export BUNDLE_FDS_TAG=FDS-6.10test
 
 # FIG-6.10.1-60-gf047b6e
