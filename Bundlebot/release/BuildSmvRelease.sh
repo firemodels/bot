47870ff20e9e9b8c90a5110a11025a6a72172d32
gforney <gforney@gmail.com>
2025-11-28 15:53:24 -0500

    bundle scripts: updates smokeview bundle scripts

--- Bundlebot/release/BuildSmvRelease.sh
+++ Bundlebot/release/BuildSmvRelease.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # build a release bundle using revision and tags defined in config.sh .
 source config.sh
-export BUILDING_release=1
 
 cd ../nightly
-./BuildSmvNightly.sh
+./BuildSmvNightly.sh -R -U
