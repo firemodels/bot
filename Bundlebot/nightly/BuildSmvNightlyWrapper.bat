73c8807e7a69336ae04e260686848ee1ac83717c
gforney <gforney@gmail.com>
2025-11-28 11:37:12 -0500

    bundle scripts: minor updates - update bot repo in smv nightly windows bundle script, add some output to fds/smv and smv nightly windows bundle scripts

--- Bundlebot/nightly/BuildSmvNightlyWrapper.bat
+++ Bundlebot/nightly/BuildSmvNightlyWrapper.bat
@@ -3,4 +3,15 @@ setlocal
 
 set outfile=%userprofile%\.bundle\bundle_smv_nightly.out
 
-call BuildSmvNightly > %outfile% 2>&1
+set CURDIRW=%CD%
+
+echo.                                                        >  %outfile% 2>&1
+echo ------------------------------------------------------  >> %outfile% 2>&1
+echo ------------------------------------------------------  >> %outfile% 2>&1
+echo Updating bot repo                                       >> %outfile% 2>&1
+cd ..\..\Scripts
+call update_repos -b -m > Nul 2>&1
+
+cd %CURDIRW%
+call BuildSmvNightly                                         >> %outfile% 2>&1
+cd %CURDIRW%
