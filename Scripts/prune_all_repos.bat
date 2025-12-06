c28e0e00c15cc146eb10009f2f8b10503e4d027c
gforney <gforney@gmail.com>
2025-12-05 20:17:28 -0500

    remove prune scripts

--- Scripts/prune_all_repos.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-for /D %%f in (%userprofile%\Firemodels*) do (
-  cd %%f\bot\scripts
-  echo -----------------------------------
-  echo %%f
-  echo -----------------------------------
-  call prune_repos
-)
