43830d2ca7360d5a6e2f49af7ca0d5c790093127
gforney <gforney@gmail.com>
2026-01-06 13:38:13 -0500

    smokebot: add support for ini and smv keywords to compare keyword script - interim commit

--- Smokebot/compare_keywords.sh
+++ Smokebot/compare_keywords.sh
@@ -6,16 +6,11 @@ if [ ! -e .smv_git ]; then
   exit 1
 fi
 
-OUTPUT=$1
-NAME_PREFIX=$2
-
-if [ "$OUTPUT" == "" ]; then
-  OUTPUT=output
-fi
-
-if [ "$NAME_PREFIX" != "" ]; then
-  NAME_PREFIX=${NAME_PREFIX}_
-fi
+OUTPUT=output
+KEYWORDTYPE=ssf
+#KEYWORDTYPE=ini
+#KEYWORDTYPE=smv
+NAME_PREFIX=${KEYWORDTYPE}_
 
 KEYWORDS_C=$OUTPUT/${NAME_PREFIX}keywords_c.txt
 KEYWORDS_TEX=$OUTPUT/${NAME_PREFIX}keywords_tex.txt
@@ -24,18 +19,36 @@ KEYWORDS_NODOC=$OUTPUT/${NAME_PREFIX}keywords_nodoc.txt
 KEYWORDS_NOSOURCE=$OUTPUT/${NAME_PREFIX}keywords_nosource.txt
 
 #remove files from last comparison
-rm -f $OUTPUT/*keywords*txt
+rm -f $OUTPUT/${KEYWORDTYPE}*keywords*txt
 
-# FDS UG directory
+# SMV UG directory
 tex_dir=../../smv/Manuals/SMV_User_Guide/
-# fds source directory
-c_dir=../../smv/Source/smokeview
+# smv source directory
+source_dir=../../smv/Source/smokeview
+# shared source directory
+shared_dir=../../smv/Source/shared
 
-# generate list of script keywords found in IOscript.c
-grep InitKeyword $c_dir/IOscript.c | awk -F'"' '{print $2}' | awk -F'"' '{print $1}' |  grep -v '^[[:space:]]*$' | sort -u > $KEYWORDS_C
+if [ "$KEYWORDTYPE" == "ssf" ]; then
+  # generate list of script keywords found in IOscript.c
+  grep InitKeyword $source_dir/IOscript.c | awk -F'"' '{print $2}' | awk -F'"' '{print $1}' |  grep -v '^[[:space:]]*$' | sort -u > $KEYWORDS_C
 
-# generate list of script keywords found in SMV_User_Guide.tex
-grep hitemssf $tex_dir/*.tex | awk -F'{' '{print $2}' | awk -F '}' '{print $1}' | sort -u > $KEYWORDS_TEX
+  # generate list of script keywords found in SMV_User_Guide.tex
+  grep hitemssf $tex_dir/*.tex | awk -F'{' '{print $2}' | awk -F '}' '{print $1}' | sort -u > $KEYWORDS_TEX
+fi
+if [ "$KEYWORDTYPE" == "smv" ]; then
+  # generate list of script keywords found in IOscript.c
+  grep MatchSMV $shared_dir/readsmvfile.c | awk -F'"' '{print $2}' | awk -F'"' '{print $1}' |  grep -v '^[[:space:]]*$' | sort -u > $KEYWORDS_C
+
+  # generate list of script keywords found in SMV_User_Guide.tex
+  grep hitemsmv $tex_dir/*.tex | awk -F'{' '{print $2}' | awk -F '}' '{print $1}' | sort -u > $KEYWORDS_TEX
+fi
+if [ "$KEYWORDTYPE" == "ini" ]; then
+  # generate list of script keywords found in IOscript.c
+  grep MatchINI $source_dir/readsmv.c | awk -F'"' '{print $2}' | awk -F'"' '{print $1}' |  grep -v '^[[:space:]]*$' | sort -u > $KEYWORDS_C
+
+  # generate list of script keywords found in SMV_User_Guide.tex
+  grep hitemini $tex_dir/*.tex | awk -F'{' '{print $2}' | awk -F '}' '{print $1}' | sort -u > $KEYWORDS_TEX
+fi
 
 #compute difference between tex and c /keywords
 git diff --no-index $KEYWORDS_C $KEYWORDS_TEX                                  > $KEYWORDS_DIFF
@@ -49,14 +62,14 @@ echo "unimplemented script keywords: $nlines_nosource"                         >
 grep ^+ $KEYWORDS_DIFF | sed 's/^+//g' | grep -v \\+\\+                       >> $KEYWORDS_NOSOURCE
 
 if [ "$nlines_nodoc" == "0" ]; then
-  echo "$nlines_nodoc undocumented script keywords"
+  echo "$nlines_nodoc undocumented $KEYWORDTYPE script keywords"
 else
-  echo "$nlines_nodoc undocumented script keywords: $KEYWORDS_NODOC"
+  echo "$nlines_nodoc undocumented $KEYWORDTYPE script keywords: $KEYWORDS_NODOC"
   cat $KEYWORDS_NODOC
 fi
 if [ "$nlines_nosource" == "0" ]; then
-  echo "$nlines_nosource unimplemented script keywords"
+  echo "$nlines_nosource unimplemented $KEYWORDTYPE script keywords"
 else
-  echo "$nlines_nosource unimplemented script keywords: $KEYWORDS_NOSOURCE"
+  echo "$nlines_nosource unimplemented $KEYWORDTYPE script keywords: $KEYWORDS_NOSOURCE"
 fi
 
