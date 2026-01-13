0b0ce36a058e3bda9563e2b7fd5df688f55e3026
gforney <gforney@gmail.com>
2026-01-13 14:40:59 -0500

    bundle scripts: fix to conversion script

--- Bundlebot/nightly/make_bundle.sh
+++ Bundlebot/nightly/make_bundle.sh
@@ -416,7 +416,7 @@ if [ $clam_status -eq 1 ]; then
   echo "--- scanning archive for viruses/malware ---"
   echo "" 
   clamscan -r $UPLOAD_DIR/$bundlebase > $scanlog 2>&1
-  sed 's/.*FDS/FDS/' $scanlog > $vscanlog
+  sed 's/.*FDS-/FDS-/' $scanlog > $vscanlog
   ninfected=`grep 'Infected files' $vscanlog | awk -F: '{print $2}'`
   if [ "$ninfected" == "" ]; then
     ninfected=0
