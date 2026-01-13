2ec5dd19e53a8139fb2a84e13a5524283ad191d8
gforney <gforney@gmail.com>
2026-01-13 16:14:37 -0500

    bundle scripts: correct scan log path name

--- Bundlebot/nightly/make_bundle.sh
+++ Bundlebot/nightly/make_bundle.sh
@@ -54,8 +54,8 @@ else
   bundlebase=${fds_version}_${smv_version}_${FDSREPODATE}${NIGHTLY}lnx
 fi
 custombase=${fds_version}_${smv_version}
-scanlog=$SCRIPTDIR/output/$bundlebase_log.txt
-vscanlog=$SCRIPTDIR/output/$bundlebase_vlog.txt
+scanlog=$SCRIPTDIR/output/${bundlebase}_log.txt
+vscanlog=$SCRIPTDIR/output/${bundlebase}_vlog.txt
 
 # create upload directory if it doesn't exist
 
