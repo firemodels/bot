87f2a2d180ab432669d3923fe084d2acc892ab3f
gforney <gforney@gmail.com>
2026-01-14 21:27:35 -0500

    bundle scripts: minor edits to windows csv to html conversion script

--- Bundlebot/nightly/csv2html.bat
+++ Bundlebot/nightly/csv2html.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal EnableExtensions
 
-REM ---- Input handling ----
+:: ---- Input handling ----
 if "%~1"=="" (
     echo Usage: %~nx0 inputfile
     exit /b 1
@@ -12,10 +12,10 @@ set "INPUTTEMP=%INPUT%.%RANDOM%"
 set "TITLE=%~n1"
 set "OUTPUT=%~n1_manifest.html"
 
-REM ---- sed replacement (same as Bash) ----
+:: ---- sed replacement (same as Bash) ----
 sed "s/: OK/OK/g" "%INPUT%" > "%INPUTTEMP%"
 
-REM ---- HTML header ----
+:: ---- HTML header ----
 (
 echo ^<html^>^<head^>^<title^>%TITLE% Manifest^</title^>^</head^>
 echo ^<body^>
@@ -24,8 +24,8 @@ echo ^<table border=on^>
 echo ^<tr^>^<th^>file^</th^>^<th^>sha256 hash^</th^>^<th^>virus status^</th^>^</tr^>
 ) > "%OUTPUT%"
 
-REM ---- awk table rows (until SCAN SUMMARY) ----
-awk -F, "{
+:: ---- awk table rows (until SCAN SUMMARY) ----
+gawk -F, "{
     if ($0 ~ /SCAN SUMMARY/) exit
     printf \"<tr>\"
     for (i=1; i<=NF; i++) printf \"<td>%s</td>\", $i
@@ -35,13 +35,13 @@ awk -F, "{
 REM ---- start preformatted section ----
 echo ^<pre^> >> "%OUTPUT%"
 
-REM ---- awk SCAN SUMMARY section ----
-awk "{
+:: ---- awk SCAN SUMMARY section ----
+gawk "{
     if ($0 ~ /SCAN SUMMARY/) start=1
     if (start) print
 }" "%INPUTTEMP%" >> "%OUTPUT%"
 
-REM ---- HTML footer ----
+:: ---- HTML footer ----
 (
 echo ^</pre^>
 echo ^</table^>
@@ -49,7 +49,7 @@ echo ^</body^>
 echo ^</html^>
 ) >> "%OUTPUT%"
 
-REM ---- Cleanup ----
+:: ---- Cleanup ----
 del "%INPUTTEMP%" >nul 2>&1
 
 echo Manifest created: %OUTPUT%
