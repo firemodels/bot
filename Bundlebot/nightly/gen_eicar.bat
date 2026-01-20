71d7fc638160653cd3030451f710798f1eea5d54
gforney <gforney@gmail.com>
2026-01-20 15:56:34 -0500

    update eicar generating script

--- Bundlebot/nightly/gen_eicar.bat
+++ Bundlebot/nightly/gen_eicar.bat
@@ -1,3 +1,6 @@
 echo off
 set OUTPUT=%1
-<nul set /p=X5O!P%%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*>%OUTPUT%
+set TEST=%2
+:: if TEST is null then OUTPUT will contain the eicar test string
+:: if TEST contains a string then it will not (ie will not be quarantined by a virus scanner)
+<nul set /p=%TEST%X5O^!P%%@AP[4\PZX54^(P^^^)7CC^)7^}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE^!$H+H^*%TEST%>%OUTPUT%
--- Bundlebot/nightly/gen_eicar.sh
+++ Bundlebot/nightly/gen_eicar.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 OUTPUT=$1
+TEST=$2
+# if TEST is null then OUTPUT will contain the eicar test string
+# if TEST contains a string then it will not (ie will not be quarantined by a virus scanner)
 cat << EOF > $OUTPUT
-X5O!P%@AP[4\PZX54(P^)7CC)7}\$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!\$H+H*
+${TEST}X5O!P%@AP[4\PZX54(P^)7CC)7}\$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!\$H+H*${TEST}
 EOF
