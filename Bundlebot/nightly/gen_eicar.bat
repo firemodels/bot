59733caaa2969285f613a6a3c9d736cb2d9e54b7
gforney <gforney@gmail.com>
2026-01-20 16:08:33 -0500

    bundle scripts: require an output file when generaing an eicar test file

--- Bundlebot/nightly/gen_eicar.bat
+++ Bundlebot/nightly/gen_eicar.bat
@@ -1,6 +1,9 @@
 echo off
 set OUTPUT=%1
 set TEST=%2
+if x%OUTPUT% == x echo ***error: specify an output file
+if x%OUTPUT% == x exit /b
+
 :: if TEST is null then OUTPUT will contain the eicar test string
 :: if TEST contains a string then it will not (ie will not be quarantined by a virus scanner)
-<nul set /p=%TEST%X5O^!P%%@AP[4\PZX54^(P^^^)7CC^)7^}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE^!$H+H^*%TEST%>%OUTPUT%
+<nul set /p=%TEST%X5O^!P%%@AP[4\PZX54^(P^^^)7CC^)7^}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE^!$H+H^*%TEST%>stdout
