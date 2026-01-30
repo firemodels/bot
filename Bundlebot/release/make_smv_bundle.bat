03c3c16ebcaa74868103826aa09ead9d8f479333
gforney <gforney@gmail.com>
2026-01-30 09:06:08 -0500

    bundle scripts: fix a couple of typos in windows bundle script: path to gawk, path to manifest

--- Bundlebot/release/make_smv_bundle.bat
+++ Bundlebot/release/make_smv_bundle.bat
@@ -28,6 +28,7 @@ set windbuild=%reporoot%\smv\Build\wind2fds\intel_win
 set sh2bat=%reporoot%\smv\Build\sh2bat\intel_win
 set gettime=%reporoot%\smv\Build\get_time\intel_win
 set repoexes=%userprofile%\.bundle\BUNDLE\WINDOWS\repoexes
+set gawk=%reporoot%\bot\scripts\bin\gawk.exe
 
 set smvdir=%zipbase%\%SMVEDITION%
 
@@ -138,7 +139,7 @@ if %ERRORLEVEL% == 1 goto elsescan
   call %CSV2HTML% %vscanlog%
   if NOT exist %htmllog% echo ***error: %htmllog% does not exist
   if NOT exist %htmllog% goto skiphtml
-  CALL :COPY %htmllog% %out_doc%\SmvManifest.html
+  CALL :COPY %htmllog% %logdir%\SmvManifest.html
   CALL :COPY %htmllog% %bundles%\%zipbase%_manifest.html
   :skiphtml
   
