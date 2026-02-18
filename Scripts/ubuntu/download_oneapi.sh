455ec955a95796eb50a544ad1784df150e17f684
gforney <gforney@gmail.com>
2026-02-18 13:09:59 -0500

    update ubuntu setup scripts

--- Scripts/ubuntu/download_oneapi.sh
+++ Scripts/ubuntu/download_oneapi.sh
@@ -9,10 +9,10 @@ echo downloading oneapi compilers
 wget https://registrationcenter-download.intel.com/akdlm/IRC_NAS/6caa93ca-e10a-4cc5-b210-68f385feea9e/intel-oneapi-base-toolkit-2025.3.1.36_offline.sh
 wget https://registrationcenter-download.intel.com/akdlm/IRC_NAS/f59a79aa-4a6e-46e8-a6c2-be04bb13f274/intel-oneapi-hpc-toolkit-2025.3.1.55_offline.sh
 
-echo install oneapi compilers using
-sudo sh ./intel-oneapi-base-toolkit-2025.3.1.36_offline.sh -a --silent --cli --eula accept -a --silent --cli --eula accept
-sudo sh ./intel-oneapi-hpc-toolkit-2025.3.1.55_offline.sh -a --silent --cli --eula accept -a --silent --cli --eula accept
+#echo install oneapi compilers using
+#sudo sh ./intel-oneapi-base-toolkit-2025.3.1.36_offline.sh -a --silent --cli --eula accept -a --silent --cli --eula accept
+#sudo sh ./intel-oneapi-hpc-toolkit-2025.3.1.55_offline.sh -a --silent --cli --eula accept -a --silent --cli --eula accept
 
-echo add essential packages
-sudo apt update
-sudo apt -y install cmake pkg-config build-essential
+#echo add essential packages
+#sudo apt update
+#sudo apt -y install cmake pkg-config build-essential
