2a2f79b0f6eb9565db152b43f81e1ce39208bddb
gforney <gforney@gmail.com>
2026-04-24 22:27:14 -0400

    scripts: add button to build apps to web gui

---
+++ Bundlebot/webscripts/webBuildVerApps.bat
@@ -0,0 +1,30 @@
+@echo off
+
+:: batch file to build test or release smokeview on Windows, Linux or OSX platforms
+
+:: setup environment variables (defining where repository resides etc) 
+
+set envfile="%userprofile%"\fds_smv_env.bat
+IF EXIST %envfile% GOTO endif_envexist
+echo ***Fatal error.  The environment setup file %envfile% does not exist. 
+echo Create a file named %envfile% and use smv/scripts/fds_smv_env_template.bat
+echo as an example.
+echo.
+echo Aborting now...
+pause>NUL
+goto:eof
+
+:endif_envexist
+
+call %envfile%
+
+cd %git_root%\smv\Build\smokeview\intel_win
+start "build fds" cmd /c "call make_smokeview"
+
+cd %git_root%\smv\Build\fds2fed\intel_win
+start "build fds2fed" cmd /c "call make_fds2fed"
+
+
+cd %git_root%\fds\Build\impi_intel_win
+start "build fds" cmd /c "call make_fds"
+
