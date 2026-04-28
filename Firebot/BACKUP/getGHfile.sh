22ed6ee4dc23f894c3754a15e62b53a44ea18911
gforney <gforney@gmail.com>
2026-04-27 23:32:20 -0400

    firebot: remove unused scripts

--- Firebot/BACKUP/getGHfile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-FROMFILE=$1
-
-rm -f $FROMFILE
-gh release download FDS_TEST -p $FROMFILE -R github.com/firemodels/test_bundles --clobber
-if [ ! -e $FROMFILE ]; then
-  echo ***error: $FROMFILE failed to download using:
-  echo "         gh release download FDS_TEST -p $FROMFILE -R github.com/firemodels/test_bundles --clobber"
-fi
--- Firebot/BACKUP/test_python.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-./setup_python.sh
