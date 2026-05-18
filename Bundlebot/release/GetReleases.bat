91302571fa8da38eb11ea186c7165d4c7cd355ff
gforney <gforney@gmail.com>
2026-05-18 08:58:34 -0400

    bundle: updates to GetRelease windows batch file

--- Bundlebot/release/GetReleases.bat
+++ Bundlebot/release/GetReleases.bat
@@ -1,6 +1,10 @@
 @echo on
 setlocal
 
+set OWNER=%1
+
+if x%OWNER% == x set OWNER=%username%
+
 set CURDIR=%CD%
 call config
 
@@ -14,24 +18,24 @@ cd %CURDIR%
 
 set BUNDLE_BASE=%BUNDLE_FDS_TAG%_%BUNDLE_SMV_TAG%_
 
-call :downloadfile FDS_Config_Management_Plan.pdf
-call :downloadfile FDS_Technical_Reference_Guide.pdf
-call :downloadfile FDS_User_Guide.pdf
-call :downloadfile FDS_Validation_Guide.pdf
-call :downloadfile FDS_Verification_Guide.pdf
+call :downloadfile FDS_TEST FDS_Config_Management_Plan.pdf
+call :downloadfile FDS_TEST FDS_Technical_Reference_Guide.pdf
+call :downloadfile FDS_TEST FDS_User_Guide.pdf
+call :downloadfile FDS_TEST FDS_Validation_Guide.pdf
+call :downloadfile FDS_TEST FDS_Verification_Guide.pdf
 
-call :downloadfile SMV_User_Guide.pdf
-call :downloadfile SMV_Verification_Guide.pdf
-call :downloadfile SMV_Technical_Reference_Guide.pdf
+call :downloadfile SMOKEVIEW_TEST SMV_User_Guide.pdf
+call :downloadfile SMOKEVIEW_TEST SMV_Verification_Guide.pdf
+call :downloadfile SMOKEVIEW_TEST SMV_Technical_Reference_Guide.pdf
 
-call :downloadfile %BUNDLE_BASE%lnx.sh
-call :downloadfile %BUNDLE_BASE%lnx.tar.gz
+call :downloadfile FDS_TEST %BUNDLE_BASE%lnx.sh
+call :downloadfile FDS_TEST %BUNDLE_BASE%lnx_manifest.html
 
-call :downloadfile %BUNDLE_BASE%osx.sh
-call :downloadfile %BUNDLE_BASE%osx.tar.gz
+call :downloadfile FDS_TEST %BUNDLE_BASE%osx.sh
+call :downloadfile FDS_TEST %BUNDLE_BASE%osx_manifest.html
 
-call :downloadfile %BUNDLE_BASE%win.exe
-call :downloadfile %BUNDLE_BASE%win.zip
+call :downloadfile FDS_TEST %BUNDLE_BASE%win.exe
+call :downloadfile FDS_TEST %BUNDLE_BASE%win_manifest.html
 echo ***files downloaded to %BUNDLEDIR%
 cd %CURDIR%
 goto eof
@@ -39,9 +43,10 @@ goto eof
 ::----------------------------------------------------------
 :downloadfile
 ::----------------------------------------------------------
-set ffile=%1
+set RELEASE=%1
+set ffile=%2
   echo downloading %ffile%
-  gh release download FDS_TEST -p %ffile% -D %BUNDLEDIR%  -R github.com/%username%/test_bundles
+  gh release download %RELEASE% -p %ffile% -D %BUNDLEDIR%  -R github.com/%OWNER%/test_bundles
   exit /b 0
 
 :eof
